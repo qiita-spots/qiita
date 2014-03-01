84294aeaed2b1b9c5cd215fa9d56e1190c8fb911
Jose Navas <josenavasmolina@gmail.com>
2014-02-28 17:24:46 -0700

    Set up base code structure

--- qiita_db/__init__.py
+++ qiita_db/__init__.py
@@ -9,7 +9,7 @@ __maintainer__ = "Jose Antonio Navas Molina"
 __email__ = "josenavasmolina@gmail.edu"
 __status__ = "Development"
 
-from .backends.sql import (SQLUser, SQLAnalysis, SQLStudy, SQLSample, SQLJob,
+from .backends.sql import (SQLUser, SQLAnalysis, SQLStudy, SQLJob,
                            SQLMetadataMap)
 from .backends.fs import FSUser, FSAnalysis, FSStudy, FSSample, FSJob
 from qiita_db.config import qiita_db_config
@@ -19,7 +19,6 @@ if qiita_db_config.backend == "SQL":
     User = SQLUser
     Analysis = SQLAnalysis
     Study = SQLStudy
-    Sample = SQLSample
     Job = SQLJob
     MetadataMap = SQLMetadataMap
 elif qiita_db_config.backend == "FS":
