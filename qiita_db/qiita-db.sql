90d7a9b99eb6237d599e1b5efa2253fe0290390d
Joshua Shorenstein <squirrelo@gmail.com>
2014-04-15 15:58:52 -0600

    Fix duplicate ids

--- qiita_db/qiita-db.sql
+++ qiita_db/qiita-db.sql
@@ -389,7 +389,6 @@ CREATE TABLE qiita.qiita_user (
 	address              varchar  ,
 	phone                varchar  ,
 	salt                 varchar  NOT NULL,
-	user_verify_code     varchar  NOT NULL,
 	pass_reset_code      varchar  ,
 	pass_reset_timestamp timestamp  ,
 	CONSTRAINT pk_user PRIMARY KEY ( email ),
@@ -401,8 +400,6 @@ CREATE INDEX idx_user ON qiita.qiita_user ( user_level_id );
 COMMENT ON TABLE qiita.qiita_user IS 'Holds all user information';
 
 COMMENT ON COLUMN qiita.qiita_user.user_level_id IS 'user level';
-
-COMMENT ON COLUMN qiita.qiita_user.user_verify_code IS 'Code for initial user email verification';
 
 COMMENT ON COLUMN qiita.qiita_user.pass_reset_code IS 'Randomly generated code for password reset';
 
@@ -486,7 +483,7 @@ CREATE TABLE qiita.study_sample_columns (
 	CONSTRAINT fk_study_mapping_columns_study FOREIGN KEY ( study_id ) REFERENCES qiita.study( study_id )    
  );
 
-CREATE INDEX idx_study_mapping_columns ON qiita.study_sample_columns ( study_id );
+CREATE INDEX idx_study_mapping_columns_study_id ON qiita.study_sample_columns ( study_id );
 
 CREATE TABLE qiita.study_users ( 
 	study_id             bigint  NOT NULL,
@@ -515,9 +512,9 @@ CREATE TABLE qiita.analysis (
 	CONSTRAINT fk_analysis_analysis_status FOREIGN KEY ( analysis_status_id ) REFERENCES qiita.analysis_status( analysis_status_id )    
  );
 
-CREATE INDEX idx_analysis ON qiita.analysis ( email );
+CREATE INDEX idx_analysis_email ON qiita.analysis ( email );
 
-CREATE INDEX idx_analysis ON qiita.analysis ( analysis_status_id );
+CREATE INDEX idx_analysis_status_id ON qiita.analysis ( analysis_status_id );
 
 COMMENT ON TABLE qiita.analysis IS 'hHolds analysis information';
 
