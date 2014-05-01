b926cf3876f1301af55935d3f4d471efd51049e9
Joshua Shorenstein <squirrelo@gmail.com>
2014-05-01 16:44:19 -0600

    inital study create function

--- qiita_db/study.py
+++ qiita_db/study.py
@@ -19,8 +19,18 @@ Classes
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
+from datetime import date
+
 from .base import QiitaStatusObject
-from .exceptions import QiitaDBNotImplementedError
+from .exceptions import QiitaDBNotImplementedError, QiitaDBExecutionError
+from .sql_connection import SQLConnectionHandler
+
+
+REQUIRED_KEYS = set(("timeseries_type_id", "lab_person_id", "mixs_compliant",
+                     "metadata_complete", "number_samples_collected",
+                     "number_samples_promised", "portal_type",
+                     "principal_investigator_id", "study_title", "study_alias",
+                     "study_description", "study_abstract"))
 
 
 class Study(QiitaStatusObject):
@@ -43,15 +53,72 @@ class Study(QiitaStatusObject):
     """
 
     @staticmethod
-    def create(owner):
+    def create(owner, info, investigation=None):
         """Creates a new study on the database
 
         Parameters
         ----------
         owner : str
             the user id of the study' owner
+        info: dict
+            the information attached to the study
+        investigation: dict
+            if the study is part of an investigation, the information needed to
+            create the investigation or add study to investigation
+
+        Raises
+        ------
+        QiitaDBExecutionError
+            All required keys not passed or non-db columns in info dictionary
+
+        Notes
+        -----
+        If investigation_id passed in investigation database, will assume that
+        study is part of that investigation. Otherwise need to pass the
+        following keys in investigation: "name", "description",
+        "contact_person_id"
         """
-        raise QiitaDBNotImplementedError()
+        # make sure required keys are in the info dict
+        if len(REQUIRED_KEYS.difference(set(info))) > 0:
+            raise QiitaDBExecutionError("Required keys missing: %s" %
+                                        REQUIRED_KEYS.difference(set(info)))
+
+        conn_handler = SQLConnectionHandler()
+        # make sure dictionary only has keys for available columns in db
+        sql = ("select column_name from information_schema.columns where "
+               "table_name='study'")
+        cols = set(conn_handler.fetchone(sql))
+        if len(set(info).difference(cols)) > 0:
+            raise QiitaDBExecutionError("Non-database keys found: %s" %
+                                        set(info).difference(cols))
+
+        # Insert study into database
+        sql = ("INSERT INTO qiita.study (email,study_status_id,first_contact,"
+               "%s) VALUES (%s) RETURNING study_id" % (','.join(info.keys()),
+                                                       '%s' * (len(info)+3)))
+        data = [owner, 1, date.today().strftime("%B %d, %Y")]
+        # make sure data in same order as sql column names
+        for col in info.keys():
+            data.append(info[col])
+        study_id = conn_handler.fetchone(sql, data)[0]
+
+        # Insert investigation information if necessary
+        if investigation:
+            if "investigation_id" in investigation:
+                # investigation already exists
+                inv_id = investigation["investigation_id"]
+            else:
+                # investigation does not exist in db so create it and add study
+                sql = ("INSERT INTO qiita.investigation(name, description,"
+                       "contact_person_id) VALUES (%s,%s,%s) RETURNING "
+                       "investigation_id")
+                data = (investigation["name"], investigation["description"],
+                        investigation["contact_person_id"])
+                inv_id = conn_handler.fetchone(sql, data)[0]
+            # add study to investigation
+            sql = ("INSERT INTO qiita.investigation_study (investigation_id, "
+                   "study_id) VALUES (%s, %s)")
+            conn_handler.execute(sql, (inv_id, study_id))
 
     @staticmethod
     def delete(id_):
