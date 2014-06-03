16c7d5c857a2dc66e0922b42cbc552598a1dab86
Jose Navas <josenavasmolina@gmail.com>
2014-06-03 17:16:18 -0600

    Dropiing pyqi support so we can support Py3.4.

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(name='qiita',
       scripts=glob('scripts/*'),
       extras_require={'test': ["nose >= 0.10.1", "pep8"],
                       'doc': ["Sphinx >= 1.2.2", "sphinx-bootstrap-theme"]},
-      install_requires=['psycopg2', 'pyqi == 0.3.2', 'click == 1.0'],
+      install_requires=['psycopg2', 'click == 1.0'],
       classifiers=classifiers
       )
