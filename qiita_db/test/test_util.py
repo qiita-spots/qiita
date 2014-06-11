e0d526012ae7bb56ec85da157170b882c2c97ecc
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-11 11:48:47 -0600

    tests for new util functions

---
+++ qiita_db/test/test_util.py
@@ -0,0 +1,50 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+
+from qiita_core.util import qiita_test_checker
+from qiita_db.util import check_table_cols, check_required_columns
+from qiita_db.sql_connection import SQLConnectionHandler
+from qiita_db.exceptions import QiitaDBColumnError
+
+
+@qiita_test_checker()
+class DBUtilTests(TestCase):
+    def setUp(self):
+        self.conn_handler = SQLConnectionHandler()
+        self.table = 'study'
+        self.required = [
+            'number_samples_promised', 'study_title', 'mixs_compliant',
+            'metadata_complete', 'study_description', 'first_contact',
+            'reprocess', 'study_status_id', 'portal_type_id',
+            'timeseries_type_id', 'study_alias', 'study_abstract',
+            'principal_investigator_id', 'email', 'number_samples_collected']
+
+    def test_check_required_columns(self):
+        # Doesn't do anything if correct info passed, only errors if wrong info
+        check_required_columns(self.conn_handler, self.required, self.table)
+
+    def test_check_required_columns_fail(self):
+        self.required.remove('study_title')
+        with self.assertRaises(QiitaDBColumnError):
+            check_required_columns(self.conn_handler, self.required,
+                                   self.table)
+
+    def test_check_table_cols(self):
+        # Doesn't do anything if correct info passed, only errors if wrong info
+        check_table_cols(self.conn_handler, self.required, self.table)
+
+    def test_check_table_cols_fail(self):
+        self.required.append('BADTHINGNOINHERE')
+        with self.assertRaises(QiitaDBColumnError):
+            check_table_cols(self.conn_handler, self.required,
+                             self.table)
+
+if __name__ == "__main__":
+    main()
