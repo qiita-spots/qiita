7cfdbd5b135edb172dcd1649892b1ce46922142e
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-12 12:47:50 -0600

    break out insert_filepaths

--- qiita_db/util.py
+++ qiita_db/util.py
@@ -20,6 +20,7 @@ Methods
     exists_dynamic_table
     get_db_files_base_dir
     compute_checksum
+    insert_filepaths
 
 """
 # -----------------------------------------------------------------------------
@@ -31,10 +32,14 @@ Methods
 # -----------------------------------------------------------------------------
 
 from __future__ import division
+from future.builtins import zip
 from random import choice
 from string import ascii_letters, digits, punctuation
 from binascii import crc32
 from bcrypt import hashpw, gensalt
+from functools import partial
+from os.path import join, basename
+from shutil import copy
 
 from .exceptions import QiitaDBColumnError
 from qiita_db.sql_connection import SQLConnectionHandler
@@ -278,3 +283,58 @@ def compute_checksum(filepath):
     # We need the & 0xffffffff in order to get the same numeric value across
     # all python versions and platforms
     return crc & 0xffffffff
+
+
+def insert_filepaths(filepaths, obj_id, table, filepath_table, conn_handler):
+        r"""Inserts `filepaths` in the DB connected with `conn_handler`. Since
+        the files live outside the database, the directory in which the files
+        lives is controlled by the database, so it copies the filepaths from
+        its original location to the controlled directory.
+
+        Parameters
+        ----------
+        filepaths : iterable of tuples (str, int)
+            The list of paths to the raw files and its filepath type identifier
+        obj_id : int
+            Id of the object calling the functions
+        table : str
+            Table that holds the file data
+        filepath_table : str
+            Table that holds the filepath information
+        conn_handler : SQLConnectionHandler
+            The connection handler object connected to the DB
+
+        Returns
+        -------
+        list
+            The filepath_id in the database for each added filepath
+        """
+        # Get the base directory in which the type of data is stored
+        base_data_dir = join(get_db_files_base_dir(), table)
+        # Generate the new fileapths. Format: DataId_OriginalName
+        # Keeping the original name is useful for checking if the RawData
+        # alrady exists on the DB
+        db_path = partial(join, base_data_dir)
+        new_filepaths = [
+            (db_path("%s_%s" % (obj_id, basename(path))), id)
+            for path, id in filepaths]
+        # Copy the original files to the controlled DB directory
+        for old_fp, new_fp in zip(filepaths, new_filepaths):
+            copy(old_fp[0], new_fp[0])
+
+        paths_w_checksum = [(path, id, compute_checksum(path))
+                            for path, id in new_filepaths]
+
+        # Create the list of SQL values to add
+        values = ["('%s', %s, '%s', %s)" % (scrub_data(path), id, checksum, 1)
+                  for path, id, checksum in paths_w_checksum]
+        # Insert all the filepaths at once and get the filepath_id back
+        ids = conn_handler.execute_fetchall(
+            "INSERT INTO qiita.{0} (filepath, filepath_type_id, checksum, "
+            "checksum_algorithm_id) VALUES {1} "
+            "RETURNING filepath_id".format(filepath_table,
+                                           ', '.join(values)))
+
+        # we will receive a list of lists with a single element on it (the id),
+        # transform it to a list of ids
+        return [id[0] for id in ids]
