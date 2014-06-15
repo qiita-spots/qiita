5d7e2276fdd2e16b705b1fda82758e99892605e1
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-15 12:50:54 -0600

    more test cleanup fix for travis

--- qiita_db/test/test_job.py
+++ qiita_db/test/test_job.py
@@ -147,7 +147,7 @@ class JobTest(TestCase):
 
     def test_add_results_tar(self):
         # make test directory to tar, inclluding internal file
-        basedir = "/tmp/tar_folder"
+        basedir = join(get_work_base_dir(), "tar_folder")
         self._delete_dir = [basedir]
         self._delete_path = [join(get_db_files_base_dir(), "job",
                              "1_tar_folder.tar")]
