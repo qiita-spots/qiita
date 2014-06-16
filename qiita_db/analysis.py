5ba90d687ecfadcd91eaa75ba36fc89fefc5d6a7
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-15 20:38:49 -0600

    fleshout using docstrings

--- qiita_db/analysis.py
+++ qiita_db/analysis.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-from __future__ import division
-
 """
 Objects for dealing with Qiita analyses
 
@@ -18,6 +15,7 @@ Classes
 #
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
+from __future__ import division
 
 from .base import QiitaStatusObject
 from .exceptions import QiitaDBNotImplementedError
@@ -29,90 +27,130 @@ class Analysis(QiitaStatusObject):
 
     Attributes
     ----------
+    name
+    description
     biom_table
     jobs
-    info
+    pmid
+    parent
+    children
 
     Methods
     -------
-    add_jobs(jobs)
-        Adds a list of jobs to the analysis
-
-    remove_jobs(jobs)
-        Removes a list of jobs from the analysis
-
+    add_jobs
     """
 
     _table = "analysis"
 
-    @staticmethod
-    def create(owner):
+    @classmethod
+    def create(owner, title, description, sample_processed_ids, parent=None):
         """Creates a new analysis on the database
 
         Parameters
         ----------
         owner : str
-            the user id of the analysis' owner
+            The user id of the analysis' owner
+        title : str
+            Title of the analysis
+        description : str
+            Description of the analysis
+        sample_processed_ids : list of tuples
+            samples and the processed data id they come from in form
+            (sample_id, processed_data_id)
+        parent : Analysis object, optional
+            The analysis this one was forked from
         """
         raise QiitaDBNotImplementedError()
 
-    @staticmethod
-    def delete(id_):
-        """Deletes the analysis `id` from the database
+    # ---- Properties ----
+    @property
+    def name(self):
+        """The name of the analysis
+
+        Returns
+        -------
+        str
+            Name of the Analysis
+        """
+        raise QiitaDBNotImplementedError()
+
+    @property
+    def description(self):
+        """Returns the description of the analysis"""
+        raise QiitaDBNotImplementedError()
+
+    @description.setter
+    def description(self, description):
+        """Changes the description of the analysis
 
         Parameters
         ----------
-        id_ :
-            The analysis identifier
+        description : str
+            New description for the analysis
+
+        Raises
+        ------
+        QiitaDBStatusError
+            Analysis is public
         """
         raise QiitaDBNotImplementedError()
 
     @property
     def biom_table(self):
-        """The biom table of the analysis"""
-        raise QiitaDBNotImplementedError()
+        """The biom table of the analysis
 
-    @biom_table.setter
-    def biom_table(self, biom_table):
-        """Updates the biom table used in the analysis
-
-        Parameters
-        ----------
-            biom_table : biom.table.Table
+        Returns
+        -------
+        int
+            ProcessedData id of the biom table
         """
         raise QiitaDBNotImplementedError()
 
     @property
     def jobs(self):
-        """A list of jobs included in the analysis"""
+        """A list of jobs included in the analysis
+
+        Returns
+        -------
+        list of ints
+            Job ids for jobs in analysis
+        """
         raise QiitaDBNotImplementedError()
 
     @property
-    def info(self):
-        """Dict with any other information attached to the analysis"""
+    def pmid(self):
+        """Returns pmid attached to the analysis
+
+        Returns
+        -------
+        str
+        """
         raise QiitaDBNotImplementedError()
 
-    @info.setter
-    def info(self, info):
-        """Updates the information attached to the analysis
+    @pmid.setter
+    def pmid(self, pmid):
+        """adds pmid to the analysis
 
         Parameters
         ----------
-            info : dict
+        pmid: str
+            pmid to set for study
+
+        Raises
+        ------
+        QiitaDBStatusError
+            Analysis is public
+
+        Notes
+        -----
+        An analysis should only ever have one PMID attached to it.
         """
         raise QiitaDBNotImplementedError()
+
+    # ---- Functions ----
 
     def add_jobs(self, jobs):
         """Adds a list of jobs to the analysis
-
-        Parameters
-        ----------
-            jobs : list of Job objects
-        """
-        raise QiitaDBNotImplementedError()
-
-    def remove_jobs(self, jobs):
-        """Removes a list of jobs from the analysis
 
         Parameters
         ----------
