98f8c61de95ddac1cb6f1fc8660a78dbbb9d310c
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-17 13:00:54 -0600

    initial base of web frontend

---
+++ qiita_pet/handlers/base_handlers.py
@@ -0,0 +1,46 @@
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    def get_current_user(self):
+        '''Overrides default method of returning user curently connected'''
+        user = self.get_secure_cookie("user")
+        if user is None:
+            self.clear_cookie("user")
+            return ''
+        else:
+            return user.strip('" ')
+
+    def write_error(self, status_code, **kwargs):
+        '''Overrides the error page created by Tornado'''
+        from traceback import format_exception
+        if self.settings.get("debug") and "exc_info" in kwargs:
+            exc_info = kwargs["exc_info"]
+            trace_info = ''.join(["%s<br />" % line for line in
+                                 format_exception(*exc_info)])
+            request_info = ''.join(["<strong>%s</strong>: %s<br />" %
+                                   (k, self.request.__dict__[k]) for k in
+                                    self.request.__dict__.keys()])
+            error = exc_info[1]
+
+            self.render('error.html', error=error, trace_info=trace_info,
+                        request_info=request_info,
+                        user=self.get_current_user())
+
+
+class MainHandler(BaseHandler):
+    '''Index page'''
+    def get(self):
+        username = self.get_current_user()
+        completedanalyses = []
+        self.render("index.html", user=username, analyses=completedanalyses)
+
+
+class MockupHandler(BaseHandler):
+    def get(self):
+        self.render("mockup.html", user=self.get_current_user())
+
+
+class NoPageHandler(BaseHandler):
+    def get(self):
+        self.render("404.html", user=self.get_current_user())
---
+++ qiita_pet/webserver.py
@@ -0,0 +1,64 @@
+#login code modified from https://gist.github.com/guillaumevincent/4771570
+
+import tornado.auth
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+from tornado.options import define, options
+from os.path import dirname, join
+from base64 import b64encode
+from uuid import uuid4
+
+from qiita_pet.handlers.base_handlers import (MainHandler, MockupHandler,
+                                              NoPageHandler)
+from qiita_pet.handlers.auth_handlers import (AuthCreateHandler,
+                                              AuthLoginHandler,
+                                              AuthLogoutHandler,
+                                              AuthVerifyHandler)
+
+define("port", default=8888, help="run on the given port", type=int)
+
+DIRNAME = dirname(__file__)
+STATIC_PATH = join(DIRNAME, "static")
+TEMPLATE_PATH = join(DIRNAME, "templates")  # base folder for webpages
+RES_PATH = join(DIRNAME, "results")
+COOKIE_SECRET = b64encode(uuid4().bytes + uuid4().bytes)
+DEBUG = False
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/auth/login/", AuthLoginHandler),
+            (r"/auth/logout/", AuthLogoutHandler),
+            (r"/auth/create/", AuthCreateHandler),
+            (r"/results/(.*)", tornado.web.StaticFileHandler,
+             {"path": RES_PATH}),
+            (r"/static/(.*)", tornado.web.StaticFileHandler,
+             {"path": STATIC_PATH}),
+            (r"/mockup/", MockupHandler),
+            # 404 PAGE MUST BE LAST IN THIS LIST!
+            (r".*", NoPageHandler)
+        ]
+        settings = {
+            "template_path": TEMPLATE_PATH,
+            "debug": DEBUG,
+            "cookie_secret": COOKIE_SECRET,
+            "login_url": "/auth/login/"
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    print "Tornado started on port", options.port
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
