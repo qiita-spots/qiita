5ffad777756a60d0abddec1ff001cd671d760970
Jose Navas <josenavasmolina@gmail.com>
2014-06-17 20:51:30 -0600

    Adding drop_test_env command

--- qiita_db/environment_manager.py
+++ qiita_db/environment_manager.py
@@ -80,9 +80,41 @@ def clean_test_environment(user, password, host):
     # Connect to the postgres server
     conn = connect(user=user, host=host, password=password,
                    database='qiita_test')
+    # Get the cursor
     cur = conn.cursor()
+    # Drop the qiita schema
     cur.execute("DROP SCHEMA qiita CASCADE")
+    # Commit the changes
     conn.commit()
+    # Close cursor and connections
+    cur.close()
+    conn.close()
+
+
+def drop_test_environment(user, password, host):
+    r"""Drops the test environment.
+
+    If the `settings` table is modified, the test database environment should
+    be rebuilt. This command allows to drop the old one.
+
+    Parameters
+    ----------
+    user : str
+        The postgres user to connect to the server
+    password : str
+        The password of the user
+    host : str
+        The host where the postgres server is running
+    """
+    # Connect to the postgres server
+    conn = connect(user=user, host=host, password=password)
+    # Set the isolation level to AUTOCOMMIT so we can execute a
+    # drop database sql query
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    # Drop the database
+    cur = conn.cursor()
+    cur.execute('DROP DATABASE qiita_test')
+    # Close cursor and connection
     cur.close()
     conn.close()
 
