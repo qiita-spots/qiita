43ab39824c30b13bdcef4e4be79f18874b4e3e57
Daniel McDonald <mcdonadt@colorado.edu>
2014-06-20 12:11:44 -0600

    More ipython bits, added config manager to qiita_env

--- qiita_core/configuration_manager.py
+++ qiita_core/configuration_manager.py
@@ -6,6 +6,7 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
+from functools import partial
 from os.path import join, dirname, abspath
 from os import environ
 
@@ -43,6 +44,18 @@ class ConfigurationManager(object):
         The host where the database lives
     port : int
         The port used to connect to the postgres database in the previous host
+    ipyc_demo : str
+        The file path to the IPython demo cluster profile
+    ipyc_demo_n : int
+        The size of the demo cluster
+    ipyc_reserved : str
+        The file path to the IPython reserved cluster profile
+    ipyc_reserved_n : int
+        The size of the reserved cluster
+    ipyc_general : str
+        The file path to the IPython general cluster profile
+    ipyc_general_n : int
+        The size of the general cluster
     """
     def __init__(self):
         # If conf_fp is None, we default to the test configuration file
@@ -58,8 +71,8 @@ class ConfigurationManager(object):
             config.readfp(conf_file)
 
         _expected_sections = set(['main', 'ipython', 'redis', 'postgres'])
-        if set(config.sections) != _expected_sections:
-            missing = _expected_sections - set(config.sections)
+        if set(config.sections()) != _expected_sections:
+            missing = _expected_sections - set(config.sections())
             raise MissingSectionHeaderError("Missing: %r" % missing)
 
         self._get_main(config)
@@ -68,7 +81,7 @@ class ConfigurationManager(object):
         self._get_ipython(config)
 
     def _get_main(self, config):
-        # Get the configuration of the main section
+        """Get the configuration of the main section"""
         self.test_environment = config.getboolean('main', 'TEST_ENVIRONMENT')
         try:
             self.base_data_dir = config.get('main', 'BASE_DATA_DIR')
@@ -80,7 +93,7 @@ class ConfigurationManager(object):
                 raise e
 
     def _get_postgres(self, config):
-        # Get the configuration of the postgres section
+        """Get the configuration of the postgres section"""
         self.user = config.get('postgres', 'USER')
         try:
             self.password = config.get('postgres', 'PASSWORD')
@@ -94,7 +107,23 @@ class ConfigurationManager(object):
         self.port = config.getint('postgres', 'PORT')
 
     def _get_redis(self, config):
+        """Get the configuration of the redis section"""
         pass
 
     def _get_ipython(self, config):
-        pass
+        """Get the configuration of the ipython section"""
+        from IPython.utils.path import locate_profile
+        sec_get = partial(config.get, 'ipython')
+        sec_getint = partial(config.getint, 'ipython')
+
+        demo_profile = sec_get('DEMO_CLUSTER')
+        reserved_profile = sec_get('RESERVED_CLUSTER')
+        general_profile = sec_get('GENERAL_CLUSTER')
+
+        self.ipyc_demo = locate_profile(demo_profile)
+        self.ipyc_reserved = locate_profile(reserved_profile)
+        self.ipyc_general = locate_profile(general_profile)
+
+        self.ipyc_demo_n = sec_getint('DEMO_CLUSTER_SIZE')
+        self.ipyc_reserved_n = sec_getint('RESERVED_CLUSTER_SIZE')
+        self.ipyc_general_n = sec_getint('GENERAL_CLUSTER_SIZE')
