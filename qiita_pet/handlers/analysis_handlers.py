85fdc0e0403947541e9b4e29607fb496840baf7e
Joshua Shorenstein <squirrelo@gmail.com>
2014-06-21 17:48:45 -0600

    fix option name

--- qiita_pet/handlers/analysis_handlers.py
+++ qiita_pet/handlers/analysis_handlers.py
@@ -136,8 +136,8 @@ class AnalysisWaitHandler(BaseHandler):
                 opts["--tree_fp"] = join(get_db_files_base_dir(), "reference",
                                          "gg_97_otus_4feb2011.tre")
             else:
-                opts["--tree_fp"] = join(get_db_files_base_dir(), "reference",
-                                         "params_qiime.txt")
+                opts["--parameter_fp"] = join(get_db_files_base_dir(),
+                                              "reference", "params_qiime.txt")
             Job.create(data_type, command, opts, analysis)
             commands.append("%s: %s" % (data_type, command))
         user = self.get_current_user()
