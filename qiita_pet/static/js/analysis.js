e17b9ea11c6ff51163b632dd98f21bb781c0c6b8
Joshua Shorenstein <squirrelo@gmail.com>
2014-07-24 11:02:39 -0600

    show/hide for selected div

--- qiita_pet/static/js/analysis.js
+++ qiita_pet/static/js/analysis.js
@@ -1,19 +1,21 @@
-function toggle_row(sid, select) {
-  if(select) {
-    var row_style = document.getElementById("selrow" + sid).style;
-    var toggle = document.getElementById("seltoggle" + sid);
+function displaySelected() {
+  var linktext = document.getElementById("shselected").innerHTML;
+  var seldiv = document.getElementById("selected");
+  var sampdiv = document.getElementById("availstudies");
+  var buttondiv = document.getElementById("seperator");
+  if(seldiv.style.display == "none") {
+    sampdiv.style.bottom = "30%";
+    buttondiv.style.bottom = "25%";
+    seldiv.style.height = "25%";
+    seldiv.style.display = "";
+    linktext = "Hide selected samples";
   }
   else {
-    var row_style = document.getElementById("row" + sid).style;
-    var toggle = document.getElementById("toggle" + sid);
-  }
-  if(row_style.display == "none") {
-    row_style.display = '';
-    toggle.innerHTML = "(-)";
-  }
-  else {
-    row_style.display = "none";
-    toggle.innerHTML = "(+)";
+    sampdiv.style.bottom = "";
+    buttondiv.style.bottom = "0px";
+    seldiv.style.height = "0px";
+    seldiv.style.display = "none";
+    linktext = "Show selected samples";
   }
 } 
 
