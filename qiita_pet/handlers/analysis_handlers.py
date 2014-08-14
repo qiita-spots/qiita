57becf6d52f5ef63ee86d666cce488851efc5522
Joshua Shorenstein <squirrelo@gmail.com>
2014-08-14 16:31:37 -0600

    remove redundant options setting

--- qiita_pet/handlers/analysis_handlers.py
+++ qiita_pet/handlers/analysis_handlers.py
@@ -272,7 +272,6 @@ class AnalysisWaitHandler(BaseHandler):
                 opts["--parameter_fp"] = join(get_db_files_base_dir(),
                                               "reference", "params_qiime.txt")
             job = Job.create(data_type, command, opts, analysis)
-            job.options = opts
             commands.append("%s: %s" % (data_type, command))
         user = self.current_user
         self.render("analysis_waiting.html", user=user, aid=analysis_id,
