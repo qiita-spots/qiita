8ab1cede13cc78eaee6155a403644967cf899c50
Joshua Shorenstein <squirrelo@gmail.com>
2014-08-15 14:47:18 -0600

    change populate_test_db.sql to reflect new columns

--- qiita_db/support_files/populate_test_db.sql
+++ qiita_db/support_files/populate_test_db.sql
@@ -339,10 +339,10 @@ INSERT INTO qiita.analysis (email, name, description, analysis_status_id, pmid) 
 INSERT INTO qiita.analysis_job (analysis_id, job_id) VALUES (1, 1), (1, 2), (2, 3);
 
 -- Attach filepath to analysis
-INSERT INTO qiita.analysis_filepath (analysis_id, filepath_id) VALUES (1, 7);
+INSERT INTO qiita.analysis_filepath (analysis_id, filepath_id, data_type_id, filepath_type_id) VALUES (1, 7, 2, 6);
 
 -- Attach samples to analysis
-INSERT INTO qiita.analysis_sample (analysis_id, processed_data_id, sample_id) VALUES (1,1,'SKB8.640193'), (1,1,'SKD8.640184'), (1,1,'SKB7.640196'), (1,1,'SKM9.640192'), (1,1,'SKM4.640180'), (2,1,'SKB8.640193'), (2,1,'SKD8.640184'), (2,1,'SKB7.640196');
+INSERT INTO qiita.analysis_sample (analysis_id, processed_data_id, sample_id, study_id) VALUES (1,1,'SKB8.640193',1), (1,1,'SKD8.640184',1), (1,1,'SKB7.640196',1), (1,1,'SKM9.640192',1), (1,1,'SKM4.640180',1), (2,1,'SKB8.640193',1), (2,1,'SKD8.640184',1), (2,1,'SKB7.640196',1);
 
 --Share analysis with shared user
 INSERT INTO qiita.analysis_users (analysis_id, email) VALUES (1, 'shared@foo.bar');
