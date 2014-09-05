a1d8286e17dcb9cd44f02177f1051b2a3b222662
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-09-05 14:15:04 -0600

    ENH: Add suggestions as they were requested in CR

--- qiita_ware/test/test_util.py
+++ qiita_ware/test/test_util.py
@@ -15,7 +15,7 @@ import pandas as pd
 
 from unittest import TestCase, main
 
-from qiita_db.metadata_template import PrepTemplate
+from qiita_db.metadata_template import SampleTemplate, PrepTemplate
 from qiita_ware.util import (per_sample_sequences, template_to_dict,
                              metadata_stats_from_sample_and_prep_templates,
                              metadata_map_from_sample_and_prep_templates)
@@ -76,12 +76,14 @@ class UtilTests(TestCase):
         self.assertEqual(sorted(obs), exp)
 
     def test_metadata_stats_from_sample_and_prep_templates(self):
-        obs = metadata_stats_from_sample_and_prep_templates(1, 1)
+        obs = metadata_stats_from_sample_and_prep_templates(SampleTemplate(1),
+                                                            PrepTemplate(1))
         self.assertEqual(obs, SUMMARY_STATS)
 
 
     def test_metadata_map_from_sample_and_prep_templates(self):
-        obs = metadata_map_from_sample_and_prep_templates(1, 1)
+        obs = metadata_map_from_sample_and_prep_templates(SampleTemplate(1),
+                                                          PrepTemplate(1))
 
         # We don't test the specific values as this would blow up the size
         # of this file as the amount of lines would go to ~1000
@@ -159,80 +161,80 @@ sequences = [
 
 SUMMARY_STATS = \
 {'altitude': [('0.0', 27)],
- 'anonymized_name': [('SKD9', 1),
+ 'anonymized_name': [('SKB1', 1),
+                     ('SKB2', 1),
+                     ('SKB3', 1),
+                     ('SKB4', 1),
                      ('SKB5', 1),
+                     ('SKB6', 1),
+                     ('SKB7', 1),
+                     ('SKB8', 1),
                      ('SKB9', 1),
-                     ('SKD2', 1),
                      ('SKD1', 1),
-                     ('SKD6', 1),
-                     ('SKD7', 1),
+                     ('SKD2', 1),
+                     ('SKD3', 1),
                      ('SKD4', 1),
                      ('SKD5', 1),
-                     ('SKB1', 1),
-                     ('SKD3', 1),
-                     ('SKB2', 1),
-                     ('SKB4', 1),
-                     ('SKB6', 1),
-                     ('SKM8', 1),
-                     ('SKB7', 1),
-                     ('SKB3', 1),
+                     ('SKD6', 1),
+                     ('SKD7', 1),
                      ('SKD8', 1),
-                     ('SKM5', 1),
-                     ('SKM4', 1),
-                     ('SKM7', 1),
-                     ('SKM6', 1),
+                     ('SKD9', 1),
                      ('SKM1', 1),
-                     ('SKM3', 1),
                      ('SKM2', 1),
-                     ('SKM9', 1),
-                     ('SKB8', 1)],
+                     ('SKM3', 1),
+                     ('SKM4', 1),
+                     ('SKM5', 1),
+                     ('SKM6', 1),
+                     ('SKM7', 1),
+                     ('SKM8', 1),
+                     ('SKM9', 1)],
  'assigned_from_geo': [('n', 27)],
- 'barcodesequence': [('ACGCACATACAA', 1),
-                     ('TAGCGCGAACTT', 1),
-                     ('CGAGGTTCTGAT', 1),
-                     ('AACTCCTGTGGA', 1),
-                     ('ATATCGCGATGA', 1),
-                     ('GCGGACTATTCA', 1),
+ 'barcodesequence': [('AACTCCTGTGGA', 1),
                      ('ACCTCAGTCAAG', 1),
+                     ('ACGCACATACAA', 1),
                      ('AGCAGGCACGAA', 1),
-                     ('CCTCGATGCAGT', 1),
-                     ('CGCCGGTAATCT', 1),
-                     ('GTCCGCAAGTTA', 1),
-                     ('CGTAGAGCTCTC', 1),
-                     ('TCGACCAAACAC', 1),
-                     ('CCACCCAGTAAC', 1),
-                     ('TAATGGTCGTAG', 1),
-                     ('TGCTACAGACGT', 1),
-                     ('TGAGTGGTCTGT', 1),
                      ('AGCGCTCACATC', 1),
-                     ('CGGCCTAAGTTC', 1),
+                     ('ATATCGCGATGA', 1),
                      ('ATGGCCTGACTA', 1),
-                     ('CGTGCACAATTG', 1),
-                     ('TTGCACCGTCGA', 1),
-                     ('CCGATGCCTTGA', 1),
-                     ('CCTCTGAGAGCT', 1),
                      ('CATACACGCACC', 1),
+                     ('CCACCCAGTAAC', 1),
+                     ('CCGATGCCTTGA', 1),
+                     ('CCTCGATGCAGT', 1),
+                     ('CCTCTGAGAGCT', 1),
+                     ('CGAGGTTCTGAT', 1),
+                     ('CGCCGGTAATCT', 1),
+                     ('CGGCCTAAGTTC', 1),
+                     ('CGTAGAGCTCTC', 1),
+                     ('CGTGCACAATTG', 1),
+                     ('GATAGCACTCGT', 1),
+                     ('GCGGACTATTCA', 1),
+                     ('GTCCGCAAGTTA', 1),
+                     ('TAATGGTCGTAG', 1),
+                     ('TAGCGCGAACTT', 1),
+                     ('TCGACCAAACAC', 1),
+                     ('TGAGTGGTCTGT', 1),
+                     ('TGCTACAGACGT', 1),
                      ('TGGTTATGGCAC', 1),
-                     ('GATAGCACTCGT', 1)],
+                     ('TTGCACCGTCGA', 1)],
  'center_name': [('ANL', 27)],
  'center_project_name': [('None', 27)],
  'collection_timestamp': [('2011-11-11 13:00:00', 27)],
- 'common_name': [('root metagenome', 9),
-                 ('rhizosphere metagenome', 9),
+ 'common_name': [('rhizosphere metagenome', 9),
+                 ('root metagenome', 9),
                  ('soil metagenome', 9)],
  'country': [('GAZ:United States of America', 27)],
  'data_type_id': [('2', 27)],
  'depth': [('0.15', 27)],
  'description': [('Cannabis Soil Microbiome', 27)],
  'description_duplicate': [('Bucu Rhizo', 3),
-                           ('Diesel Root', 3),
-                           ('Burmese bulk', 3),
-                           ('Burmese Rhizo', 3),
-                           ('Bucu bulk', 3),
                            ('Bucu Roots', 3),
+                           ('Bucu bulk', 3),
+                           ('Burmese Rhizo', 3),
+                           ('Burmese bulk', 3),
                            ('Burmese root', 3),
-                           ('Diesel bulk', 3),
-                           ('Diesel Rhizo', 3)],
+                           ('Diesel Rhizo', 3),
+                           ('Diesel Root', 3),
+                           ('Diesel bulk', 3)],
  'ebi_study_accession': [('None', 27)],
  'ebi_submission_accession': [('None', 27)],
  'elevation': [('114.0', 27)],
@@ -242,54 +244,63 @@ SUMMARY_STATS = \
  'env_feature': [('ENVO:plant-associated habitat', 27)],
  'experiment_center': [('ANL', 27)],
  'experiment_design_description': [('micro biome of soil and rhizosphere of '
-                                    'cannabis plants from CA', 27)],
+                                    'cannabis plants from CA',
+                                    27)],
  'experiment_title': [('Cannabis Soil Microbiome', 27)],
  'has_extracted_data': [('True', 27)],
  'has_physical_specimen': [('True', 27)],
- 'host_subject_id': [('1001:M9', 1),
-                     ('1001:D6', 1),
-                     ('1001:B8', 1),
-                     ('1001:B5', 1),
-                     ('1001:B4', 1),
-                     ('1001:B7', 1),
-                     ('1001:B6', 1),
-                     ('1001:B1', 1),
-                     ('1001:B3', 1),
+ 'host_subject_id': [('1001:B1', 1),
                      ('1001:B2', 1),
-                     ('1001:D9', 1),
-                     ('1001:D8', 1),
-                     ('1001:D7', 1),
-                     ('1001:D5', 1),
-                     ('1001:M8', 1),
-                     ('1001:D4', 1),
-                     ('1001:D3', 1),
-                     ('1001:D2', 1),
+                     ('1001:B3', 1),
+                     ('1001:B4', 1),
+                     ('1001:B5', 1),
+                     ('1001:B6', 1),
+                     ('1001:B7', 1),
+                     ('1001:B8', 1),
+                     ('1001:B9', 1),
                      ('1001:D1', 1),
+                     ('1001:D2', 1),
+                     ('1001:D3', 1),
+                     ('1001:D4', 1),
+                     ('1001:D5', 1),
+                     ('1001:D6', 1),
+                     ('1001:D7', 1),
+                     ('1001:D8', 1),
+                     ('1001:D9', 1),
+                     ('1001:M1', 1),
+                     ('1001:M2', 1),
+                     ('1001:M3', 1),
                      ('1001:M4', 1),
                      ('1001:M5', 1),
                      ('1001:M6', 1),
                      ('1001:M7', 1),
-                     ('1001:M1', 1),
-                     ('1001:M2', 1),
-                     ('1001:M3', 1),
-                     ('1001:B9', 1)],
+                     ('1001:M8', 1),
+                     ('1001:M9', 1)],
  'host_taxid': [('3483', 27)],
  'illumina_technology': [('MiSeq', 27)],
  'latitude': [('33.193611', 27)],
  'library_construction_protocol': [('This analysis was done as in Caporaso et '
-     'al 2011 Genome research. The PCR primers (F515/R806) were developed agai'
-     'nst the V4 region of the 16S rRNA (both bacteria and archaea), which we '
-     'determined would yield optimal community clustering with reads of this l'
-     'ength using a procedure similar to that of ref. 15. [For reference, this'
-     ' primer pair amplifies the region 533_786 in the Escherichia coli strain'
-     ' 83972 sequence (greengenes accession no. prokMSA_id:470367).] The rever'
-     'se PCR primer is barcoded with a 12-base error-correcting Golay code to '
-     'facilitate multiplexing of up to 1,500 samples per lane, and both PCR pr'
-     'imers contain sequencer adapter regions.', 27)],
+                                    'al 2011 Genome research. The PCR primers '
+                                    '(F515/R806) were developed against the '
+                                    'V4 region of the 16S rRNA (both bacteria '
+                                    'and archaea), which we determined would '
+                                    'yield optimal community clustering with '
+                                    'reads of this length using a procedure '
+                                    'similar to that of ref. 15. [For '
+                                    'reference, this primer pair amplifies '
+                                    'the region 533_786 in the Escherichia '
+                                    'coli strain 83972 sequence (greengenes '
+                                    'accession no. prokMSA_id:470367).] The '
+                                    'reverse PCR primer is barcoded with a 12'
+                                    '-base error-correcting Golay code to '
+                                    'facilitate multiplexing of up to 1,500 '
+                                    'samples per lane, and both PCR primers '
+                                    'contain sequencer adapter regions.',
+                                    27)],
  'linkerprimersequence': [('GTGCCAGCMGCCGCGGTAA', 27)],
  'longitude': [('-117.241111', 27)],
  'pcr_primers': [('FWD:GTGCCAGCMGCCGCGGTAA; REV:GGACTACHVGGGTWTCTAAT', 27)],
- 'ph': [('6.82', 10), ('6.8', 9), ('6.94', 8)],
+ 'ph': [('6.8', 9), ('6.82', 10), ('6.94', 8)],
  'physical_location': [('ANL', 27)],
  'platform': [('Illumina', 27)],
  'required_sample_info_status_id': [('4', 27)],
@@ -305,14 +316,15 @@ SUMMARY_STATS = \
  'study_center': [('CCME', 27)],
  'target_gene': [('16S rRNA', 27)],
  'target_subfragment': [('V4', 27)],
- 'taxon_id': [('1118232', 9), ('410658', 9), ('939928', 9)],
+ 'taxon_id': [('410658', 9), ('939928', 9), ('1118232', 9)],
  'temp': [('15.0', 27)],
- 'texture': [('66 sand, 16.3 silt, 17.7 clay', 9),
+ 'texture': [('63.1 sand, 17.7 silt, 19.2 clay', 9),
              ('64.6 sand, 17.6 silt, 17.8 clay', 9),
-             ('63.1 sand, 17.7 silt, 19.2 clay', 9)],
- 'tot_nitro': [('1.51', 9), ('1.3', 9), ('1.41', 9)],
+             ('66 sand, 16.3 silt, 17.7 clay', 9)],
+ 'tot_nitro': [('1.3', 9), ('1.41', 9), ('1.51', 9)],
  'tot_org_carb': [('3.31', 9), ('4.32', 9), ('5.0', 9)],
- 'water_content_soil': [('0.178', 9), ('0.101', 9), ('0.164', 9)]}
+ 'water_content_soil': [('0.101', 9), ('0.164', 9), ('0.178', 9)]}
+
 
 if __name__ == '__main__':
     main()
