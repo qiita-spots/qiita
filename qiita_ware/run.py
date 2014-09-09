17b7fdcdf53bb01b76dc4ea3e39db964dc70f9cc
Joshua Shorenstein <squirrelo@gmail.com>
2014-09-08 20:41:54 -0600

    add parallel ipython support to qiita_ware

--- qiita_ware/run.py
+++ qiita_ware/run.py
@@ -2,13 +2,15 @@
 from __future__ import division
 from json import dumps
 from os.path import join
+from sys import stderr
 
 from redis import Redis
+import networkx as nx
 
 from qiita_db.job import Job
 from qiita_db.util import get_db_files_base_dir
-
-from qiita_ware.cluster import qiita_compute, system_call
+from qiita_ware.wrapper import ParallelWrapper
+from qiita_ware.context import system_call
 from qiita_ware.exceptions import ComputeError
 
 
@@ -20,10 +22,7 @@ from qiita_ware.exceptions import ComputeError
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-r_server = Redis()
-
-
-def _job_comm_wrapper(user, job, analysis_id, r_server=r_server):
+def _job_comm_wrapper(user, analysis_id, job):
     """Wraps the job command execution to allow redis communication"""
     name, command = job.command
     options = job.options
@@ -38,9 +37,9 @@ def _job_comm_wrapper(user, job, analysis_id, r_server=r_server):
     c_fmt = str("%s %s" % (command, o_fmt))
 
     # send running message to user wait page
+    r_server = Redis()
     job.status = 'running'
     msg["msg"] = "Running"
-    print "!!!!!!!!!!!!!!!!!!!!!!! RUNNING %s!" % name
     r_server.rpush(user + ":messages", dumps(msg))
     r_server.publish(user, dumps(msg))
 
@@ -50,88 +49,112 @@ def _job_comm_wrapper(user, job, analysis_id, r_server=r_server):
     except Exception as e:
         job.status = 'error'
         msg["msg"] = "ERROR"
-        print "!!!!!!!!!!!!!!!!!!!!!!! ERROR %s!" % name
         r_server.rpush(user + ":messages", dumps(msg))
         r_server.publish(user, dumps(msg))
-        raise RuntimeError("Failed compute on job id %d: %s\n%s" %
-                           (job_id, e, c_fmt))
+        print("Failed compute on job id %d:\n%s\n%s" % (job.id, c_fmt, str(e)))
+        return
 
     msg["msg"] = "Completed"
-    print "!!!!!!!!!!!!!!!!!!!!!!! COMPLETED %s!" % name
+    r_server = Redis()
     r_server.rpush(user + ":messages", dumps(msg))
     r_server.publish(user, dumps(msg))
     # FIX THIS Should not be hard coded
-    job.add_results([(options["--output_dir"], "directory")])
+    job.add_results([(job.options["--output_dir"], "directory")])
     job.status = 'completed'
 
 
-def _run_wrapper(user, analysis, commands, rarefaction_depth):
-    """Main function controlling an analysis run"""
+def _build_analysis_files(analysis, r_depth=None):
+    """Creates the biom tables and mapping file, then adds to jobs"""
     # create the biom tables and add jobs to the analysis
-    print "!!!!!!!!!!!!!! WRAPPER START"
-    analysis.build_files(rarefaction_depth)
-    print "!!!!!!!!!!!!!! FILES BUILT"
+    analysis.build_files(r_depth)
     mapping_file = analysis.mapping_file
     biom_tables = analysis.biom_tables
-    for data_type, command in commands:
-        opts = {
-            "--otu_table_fp": biom_tables[data_type],
-            "--mapping_fp": mapping_file
-        }
-        # HARD CODED HACKY THING FOR DEMO, FIX  Issue #164
-        if command == "Beta Diversity" and data_type in {'16S', '18S'}:
-            opts["--tree_fp"] = join(get_db_files_base_dir(), "reference",
-                                     "gg_97_otus_4feb2011.tre")
-        elif command == "Beta Diversity":
-            opts["--parameter_fp"] = join(get_db_files_base_dir(),
-                                          "reference", "params_qiime.txt")
-        job = Job.create(data_type, command, opts, analysis)
 
-    # fire off jobs asynchronously, holding onto AsyncResult objects
-    jobstats = []
+    # add files to existing jobs
     for job_id in analysis.jobs:
         job = Job(job_id)
         if job.status == 'queued':
-            jobstats.append(qiita_compute.submit_async(
-                _job_comm_wrapper, user, job, analysis.id))
+            opts = {
+                "--otu_table_fp": biom_tables[job.datatype],
+                "--mapping_fp": mapping_file
+            }
+            job_opts = job.options
+            job_opts.update(opts)
+            job.options = job_opts
 
-    # wait until all jobs are done
+
+def _finish_analysis(user, analysis):
+    # check job exit statuses for analysis result status
     all_good = True
-    for job in jobstats:
-        job.wait()
-        if not job.successful():
+    for job_id in analysis.jobs:
+        if Job(job_id).status == "error":
             all_good = False
+            break
 
-    # send websockets message that we are done running all jobs
-    msg["msg"] = "allcomplete"
-    msg["command"] = ""
-    r_server.rpush(user + ":messages", dumps(msg))
-    r_server.publish(user, dumps(msg))
     # set final analysis status
     if all_good:
         analysis.status = "completed"
     else:
         analysis.status = "error"
 
+    # send websockets message that we are done running all jobs
+    msg = {
+        "msg": "allcomplete",
+        "analysis": analysis.id
+    }
+    r_server = Redis()
+    r_server.rpush(user + ":messages", dumps(msg))
+    r_server.publish(user, dumps(msg))
 
-def run_analysis(user, analysis, commands, rarefaction_depth=None):
-    """Build the jobs for an analysis and run them all asynchronously
 
-    user : User object
-        user making the analysis
-    analysis : analysis object
-        the analysis to be run
-    commands : list of tuples
-        list of commands to add in the form [(command name, sys command)]
-    rarefaction_depth : int, optional
-        the rarefaction depth for biom table creation, if needed
+class RunAnalysis(ParallelWrapper):
+    def __init__(self):
+        super(RunAnalysis, self).__init__(block=False)
 
-    Notes
-    -----
-    This is a wrapper that just sets analysis to running and fires off the
-    actual work in an async ipython call. This stops the function from blocking
-    and offloads all the work to ipython.
-    """
-    analysis.status = "running"
-    qiita_compute.submit_async(
-        _run_wrapper, user, analysis, commands, rarefaction_depth)
+    def _construct_job_graph(self, user, analysis, commands, comm_opts=None,
+                             rarefaction_depth=None):
+        self._logger = stderr
+        # Add jobs to analysis
+        if comm_opts is None:
+            comm_opts = {}
+        for data_type, command in commands:
+            # get opts set by user, else make it empty dict
+            opts = comm_opts.get(command, {})
+            # Add commands to analysis as jobs
+            # HARD CODED HACKY THING FOR DEMO, FIX  Issue #164
+            if command == "Beta Diversity" and data_type in {'16S', '18S'}:
+                opts["--tree_fp"] = join(get_db_files_base_dir(), "reference",
+                                         "gg_97_otus_4feb2011.tre")
+            elif command == "Beta Diversity":
+                opts["--parameter_fp"] = join(get_db_files_base_dir(),
+                                              "reference", "params_qiime.txt")
+            Job.create(data_type, command, opts, analysis,
+                       return_existing=True)
+
+        # Create the files for the jobs
+        files_node_name = "%d_ANALYSISFILES" % analysis.id
+        self._job_graph.add_node(files_node_name,
+                                 job=(_build_analysis_files,
+                                      analysis, rarefaction_depth),
+                                 requires_deps=False)
+        # Add the jobs
+        job_nodes = []
+        for job_id in analysis.jobs:
+            job = Job(job_id)
+            node_name = "%d_JOB_%d" % (analysis.id, job.id)
+            job_nodes.append(node_name)
+            self._job_graph.add_node(node_name,
+                                     job=(_job_comm_wrapper, user, analysis.id,
+                                          job),
+                                     requires_deps=False)
+            # Adding the dependency edges to the graph
+            self._job_graph.add_edge(files_node_name, node_name)
+
+        # Finalize the analysis
+        node_name = "FINISH_ANALYSIS_%d" % analysis.id
+        self._job_graph.add_node(node_name,
+                                 job=(_finish_analysis, user, analysis),
+                                 requires_deps=False)
+        # Adding the dependency edges to the graph
+        for job_node_name in job_nodes:
+            self._job_graph.add_edge(job_node_name, node_name)
---
+++ qiita_ware/wrapper.py
@@ -0,0 +1,160 @@
+from __future__ import division
+
+__author__ = "Jose Antonio Navas Molina"
+__copyright__ = "Copyright 2011, The QIIME project"
+__credits__ = ["Jose Antonio Navas Molina"]
+__license__ = "GPL"
+__version__ = "1.8.0-dev"
+__maintainer__ = "Jose Navas Antonio Molina"
+# taken from pull request #1616 in qiime, with permission from Jose
+
+from shutil import rmtree
+from os import remove
+
+import networkx as nx
+
+from .context import context
+
+
+class ParallelWrapper(object):
+    """Base class for any parallel code"""
+    def __init__(self, retain_temp_files=False, block=True):
+        # Set if we have to delete the temporary files or keep them
+        self._retain_temp_files = retain_temp_files
+        # Set if we have to wait until the job is done or we can submit
+        # the waiter as another job
+        self._block = block
+        # self._job_graph: A networkx DAG holding the job workflow. Should be
+        # defined when calling the subclass' _construct_job_graph method. Each
+        # node should have two properties:
+        #     - "job": a tuple with the actual job to execute in the node
+        #     - "requires_deps": a boolean indicating if a dictionary with the
+        #       results of the previous jobs (keyed by node name) is passed to
+        #       the job using the kwargs argument with name 'dep_results'
+        self._job_graph = nx.DiGraph()
+        # self._logger: A WorkflowLogger object. Should be defined when
+        # calling the subclass' _construct_job_graph method
+        self._logger = None
+        # Clean up variables
+        self._filepaths_to_remove = []
+        self._dirpaths_to_remove = []
+
+    def _construct_job_graph(self, *args, **kwargs):
+        """Constructs the workflow graph with the jobs to execute
+
+        Raises
+        ------
+        NotImplementedError
+            If not overwritten in a subclass
+        """
+        raise NotImplementedError("This method should be overwritten by the "
+                                  "subclass")
+
+    def _validate_execution_order(self, results):
+        """Makes sure that the execution order represented in _job_graph has
+        been respected
+
+        Parameters
+        ----------
+        results : dict of {Node: AsyncResult}
+            The AsyncResult objects of the executed jobs
+        """
+        # Adapted from
+        # http://ipython.org/ipython-doc/dev/parallel/dag_dependencies.html
+        self._logger.write("Validating execution order... ")
+        for node in self._job_graph:
+            started = results[node].metadata.started
+            if started is None:
+                self._logger.write("Job %s: starting time not available"
+                                   % node)
+                continue
+            for parent in self._job_graph.predecessors(node):
+                finished = results[parent].metadata.completed
+                if finished is None:
+                    self._logger.write("Job %s: finish time not available"
+                                       % parent)
+                    continue
+                if started < finished:
+                    self._logger.write(
+                        "Job order not respected: %s should have happened "
+                        "after %s\n" % (node, parent))
+        self._logger.write("Done\n")
+
+    def _validate_job_status(self, results):
+        """Validates that all jobs executed finished correctly
+
+        Parameters
+        ----------
+        results : dict of {Node: AsyncResult}
+            The AsyncResult objects of the executed jobs
+        """
+        self._logger.write("\nValidating job status:\n")
+        for node, ar in results.items():
+            self._logger.write("\nJob %s: " % node)
+            if ar.successful():
+                self._logger.write("Success\n")
+            else:
+                self._logger.write("Error\n")
+                try:
+                    job_result = ar.get()
+                except Exception, e:
+                    job_result = e
+                self._logger.write("\tJob results: %s\n"
+                                   "\tPython output: %s\n"
+                                   "\tStandard output: %s\n"
+                                   "\tStandard error: %s\n"
+                                   % (job_result, ar.pyout, ar.stdout,
+                                      ar.stderr))
+
+    def _clean_up_paths(self):
+        """Removes the temporary paths"""
+        if not self._retain_temp_files:
+            self._logger.write("\nCleaning up temporary files")
+            for fp in self._filepaths_to_remove:
+                remove(fp)
+            for dp in self._dirpaths_to_remove:
+                rmtree(dp)
+
+    def _job_blocker(self, results):
+        # Block until all jobs are done
+        self._logger.write("\nWaiting for all jobs to finish... ")
+        context.wait(results.values())
+        self._logger.write("Done\n")
+        self._validate_job_status(results)
+        self._validate_execution_order(results)
+        self._clean_up_paths()
+        self._logger.close()
+
+    def __call__(self, *args, **kwargs):
+        self._construct_job_graph(*args, **kwargs)
+
+        if self._job_graph is None or self._logger is None:
+            raise RuntimeError(
+                "Job graph and/or logger not instantiated in the subclass")
+
+        # We need to submit the jobs to ipython in topological order, so we can
+        # actually define the dependencies between jobs. Adapted from
+        # http://ipython.org/ipython-doc/dev/parallel/dag_dependencies.html
+        results = {}
+        for node in nx.topological_sort(self._job_graph):
+            # Get the list of predecessor jobs
+            deps = [results[n] for n in self._job_graph.predecessors(node)]
+            # Get the tuple with the job to run
+            job = self._job_graph.node[node]['job']
+            # We can now submit the job taking into account the dependencies
+            self._logger.write("Submitting job %s: %s... " % (node, job))
+            if self._job_graph.node[node]['requires_deps']:
+                # The job requires the results of the previous jobs, get them
+                # and add them to a dict keyed by dependency node name
+                deps_dict = {n: results[n].get()
+                             for n in self._job_graph.predecessors(node)}
+                results[node] = context.submit_async_deps(
+                    deps, *job, dep_results=deps_dict)
+            else:
+                results[node] = context.submit_async_deps(deps, *job)
+            self._logger.write("Done\n")
+
+        # if self._block:
+        #     self._job_blocker(results)
+        # else:
+        #     context.submit_async(self._job_blocker, results)
