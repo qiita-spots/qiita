e047622ab9867e337ae6a7f547b7f6a641057e5f
Antonio Gonzalez <antgonza@gmail.com>
2014-09-23 16:17:20 -0600

    fixing pep8 errors

--- qiita_db/test/test_analysis.py
+++ qiita_db/test/test_analysis.py
@@ -57,7 +57,7 @@ class TestAnalysis(TestCase):
                "FROM qiita.analysis WHERE analysis_id = 3")
         obs = self.conn_handler.execute_fetchall(sql)
         self.assertEqual(obs[0][:-1], [3, 'admin@foo.bar', 'newAnalysis',
-                                'A New Analysis', 1, None])
+                                       'A New Analysis', 1, None])
         self.assertTrue(time1 < float(obs[0][-1]))
 
     def test_create_parent(self):
@@ -72,7 +72,7 @@ class TestAnalysis(TestCase):
                "FROM qiita.analysis WHERE analysis_id = 3")
         obs = self.conn_handler.execute_fetchall(sql)
         self.assertEqual(obs[0][:-1], [3, 'admin@foo.bar', 'newAnalysis',
-                                'A New Analysis', 1, None])
+                                       'A New Analysis', 1, None])
         self.assertTrue(time1 < float(obs[0][-1]))
 
         sql = "SELECT * FROM qiita.analysis_chain WHERE child_id = 3"
