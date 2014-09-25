27aa7311f706e35e263590811e1554a37e1dcf4f
Daniel McDonald <mcdonadt@colorado.edu>
2014-09-25 13:39:05 -0600

    TST/BUG: typo in lib selection, and test for generate_library_description

--- qiita_ware/test/test_ebi.py
+++ qiita_ware/test/test_ebi.py
@@ -151,11 +151,16 @@ class TestEBISubmission(TestCase):
                                        'fakepath', 'experiment description',
                                        'library protocol')
 
-    def test__generate_library_descriptor(self):
-        # raise NotImplementedError()
-        pass
+    def test_generate_library_descriptor(self):
+        e = EBISubmission('2', 'Study Title', 'Study Abstract', 'metagenome')
+        elm = ET.Element('design', {'foo': 'bar'})
 
-    def test__generate_spot_descriptor(self):
+        e._generate_library_descriptor(elm, 'sample', 10, 'libconsprot')
+        exp = ''.join([l.strip() for l in GENLIBDESC.splitlines()])
+        obs = ET.tostring(elm)
+        self.assertEqual(obs, exp)
+
+    def test_generate_spot_descriptor(self):
         # raise NotImplementedError()
         pass
 
@@ -379,6 +384,20 @@ ADDDICTTEST = """<TESTING foo="bar">
     </foo>
 </TESTING>
 """
+
+GENLIBDESC = """<design foo="bar">
+    <LIBRARY_DESCRIPTOR>
+        <LIBRARY_NAME>sample:10</LIBRARY_NAME>
+        <LIBRARY_STRATEGY>POOLCLONE</LIBRARY_STRATEGY>
+        <LIBRARY_SOURCE>METAGENOMIC</LIBRARY_SOURCE>
+        <LIBRARY_SELECTION>unspecified</LIBRARY_SELECTION>
+        <LIBRARY_LAYOUT><SINGLE /></LIBRARY_LAYOUT>
+        <LIBRARY_CONSTRUCTION_PROTOCOL>libconsprot
+        </LIBRARY_CONSTRUCTION_PROTOCOL>
+    </LIBRARY_DESCRIPTOR>
+</design>
+"""
+
 EXP_SAMPLE_TEMPLATE = (
     "sample_name\tcollection_timestamp\tdescription\thas_extracted_data\t"
     "has_physical_specimen\thost_subject_id\tlatitude\tlongitude\t"
