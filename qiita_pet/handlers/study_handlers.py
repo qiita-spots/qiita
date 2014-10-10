6204b1cdfa34167150d5105a6ccc502807023218
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-10-10 15:47:20 -0600

    STY: Fix PEP8 annoyances

--- qiita_pet/handlers/study_handlers.py
+++ qiita_pet/handlers/study_handlers.py
@@ -230,6 +230,7 @@ class CreateStudyHandler(BaseHandler):
         # TODO: change this redirect to something more sensible
         self.redirect('/')
 
+
 class MetadataSummaryHandler(BaseHandler):
     @authenticated
     def get(self, arguments):
