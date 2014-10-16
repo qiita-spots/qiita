55a59b61e1d2355d90500f825aa5da4f61944b3e
Joshua Shorenstein <squirrelo@gmail.com>
2014-10-16 17:13:37 -0600

    control access to the error page

--- qiita_pet/handlers/logger_handlers.py
+++ qiita_pet/handlers/logger_handlers.py
@@ -4,17 +4,25 @@ from tornado.web import authenticated
 
 from .base_handlers import BaseHandler
 from qiita_db.logger import LogEntry
+from qiita_db.user import User
+from tornado.web import HTTPError
 
 
 class LogEntryViewerHandler(BaseHandler):
+    def _check_access(self):
+        if User(self.current_user).level not in {'admin', 'dev'}:
+            raise HTTPError(405, "User %s doesn't have sufficient privileges "
+                            "to view error page" % self.current_user)
     @authenticated
     def get(self):
+        self._check_access()
         logentries = LogEntry.newest_records()
         self.render("error_log.html", logentries=logentries,
                     user=self.current_user)
 
     @authenticated
     def post(self):
+        self._check_access()
         numentries = int(self.get_argument("numrecords"))
         if numentries < 0:
             numentries = 100
