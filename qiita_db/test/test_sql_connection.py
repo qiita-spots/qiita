f58d82173b7defbed651a1eaec2c318e7bc17911
Joshua Shorenstein <squirrelo@gmail.com>
2014-10-28 15:38:19 -0600

    add giant queue test

--- qiita_db/test/test_sql_connection.py
+++ qiita_db/test/test_sql_connection.py
@@ -31,22 +31,49 @@ class TestConnHandler(TestCase):
 
     def test_queue_fail(self):
         """Fail if no results data exists for substitution"""
+        self.conn_handler = SQLConnectionHandler()
+        self.conn_handler.create_queue("toy_queue")
+        self.conn_handler.add_to_queue(
+            "toy_queue",
+            "INSERT INTO qiita.qiita_user (email, name, password) VALUES "
+            "(%s, %s, %s)", ['somebody@foo.bar', 'Toy', 'pass'])
+        self.conn_handler.add_to_queue(
+            "toy_queue", "UPDATE qiita.qiita_user SET user_level_id = 1 "
+            "WHERE email = %s and password = %s", [{0}, {1}])
+
         with self.assertRaises(QiitaDBExecutionError):
-            self.conn_handler = SQLConnectionHandler()
-            self.conn_handler.create_queue("toy_queue")
-            self.conn_handler.add_to_queue(
-                "toy_queue",
-                "INSERT INTO qiita.qiita_user (email, name, password) VALUES "
-                "(%s, %s, %s)", ['somebody@foo.bar', 'Toy', 'pass'])
-            self.conn_handler.add_to_queue(
-                "toy_queue", "UPDATE qiita.qiita_user SET user_level_id = 1 "
-                "WHERE email = %s and password = %s", [{0}, {1}])
             self.conn_handler.execute_queue("toy_queue")
+
         # make sure roll back correctly
         obs = self.conn_handler.execute_fetchall(
             "SELECT * from qiita.qiita_user WHERE email = %s",
             ['somebody@foo.bar'])
         self.assertEqual(obs, [])
 
+    def test_huge_queue(self):
+        self.conn_handler = SQLConnectionHandler()
+        self.conn_handler.create_queue("toy_queue")
+        # add tons of inserts to queue
+        for x in range(120):
+            self.conn_handler.add_to_queue(
+                "toy_queue",
+                "INSERT INTO qiita.qiita_user (email, name, password) VALUES "
+                "(%s, %s, %s)", ['%dsomebody@foo.bar' % x, 'Toy', 'pass'])
+        # add failing insert as final item in queue
+        self.conn_handler.add_to_queue(
+            "toy_queue",
+            "INSERT INTO qiita.qiita_BADTABLE (email, name, password) VALUES "
+            "(%s, %s, %s)", ['%dsomebody@foo.bar' % x, 'Toy', 'pass'])
+        self.conn_handler.add_to_queue(
+            "toy_queue", "UPDATE qiita.qiita_user SET user_level_id = 1 "
+            "WHERE email = %s and password = %s", [{0}, {1}])
+        with self.assertRaises(QiitaDBExecutionError):
+            self.conn_handler.execute_queue("toy_queue")
+
+        # make sure roll back correctly
+        obs = self.conn_handler.execute_fetchall(
+            "SELECT * from qiita.qiita_user WHERE email LIKE "
+            "'%somebody@foo.bar%'")
+        self.assertEqual(obs, [])
 if __name__ == "__main__":
     main()
