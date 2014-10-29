d817d0ae5cd1d713f265af5902fd889e0c75c730
Jose Navas <josenavasmolina@gmail.com>
2014-10-29 12:05:28 -0600

    Fixing small issues due to changes on populate.sql

--- qiita_db/support_files/populate_test_db.sql
+++ qiita_db/support_files/populate_test_db.sql
@@ -59,7 +59,7 @@ INSERT INTO qiita.study_experimental_factor (study_id, efo_id) VALUES (1, 1);
 INSERT INTO qiita.filepath (filepath, filepath_type_id, checksum, checksum_algorithm_id) VALUES ('raw_data/1_s_G1_L001_sequences.fastq.gz', 1, '852952723', 1), ('raw_data/1_s_G1_L001_sequences_barcodes.fastq.gz', 3, '852952723', 1), ('raw_data/2_sequences.fastq.gz', 1, '852952723', 1), ('raw_data/2_sequences_barcodes.fastq.gz', 3, '852952723', 1);
 
 -- Insert the raw data information for study 1
-INSERT INTO qiita.raw_data (filetype_id, investigation_type, preprocessing_status) VALUES (2, 'Metagenomics', 'success'), (2, 'Whole Genome Sequencing', 'not_preprocessed');
+INSERT INTO qiita.raw_data (filetype_id, investigation_type, preprocessing_status) VALUES (3, 'Metagenomics', 'success'), (2, 'Whole Genome Sequencing', 'not_preprocessed');
 
 -- Insert (link) the raw data with the raw filepaths
 INSERT INTO qiita.raw_filepath (raw_data_id, filepath_id) VALUES (1, 1), (1, 2), (2, 3), (2, 4);
