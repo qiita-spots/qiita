3a4e38bed9d471b870f739a77c9ce606712a4710
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-10-31 10:31:36 -0600

    Add webserver entry for stats page

--- qiita_pet/webserver.py
+++ qiita_pet/webserver.py
@@ -29,6 +29,7 @@ from qiita_pet.handlers.websocket_handlers import MessageHandler
 from qiita_pet.handlers.upload import UploadFileHandler
 from qiita_pet.handlers.compute import ComputeCompleteHandler
 from qiita_pet.handlers.preprocessing_handlers import PreprocessHandler
+from qiita_pet.handlers.stats import StatsHandler
 from qiita_db.util import get_db_files_base_dir
 
 define("port", default=8888, help="run on the given port", type=int)
@@ -73,6 +74,7 @@ class Application(tornado.web.Application):
             (r"/study/description/(.*)", StudyDescriptionHandler),
             (r"/upload/", UploadFileHandler),
             (r"/check_study/", CreateStudyAJAX),
+            (r"/stats/", StatsHandler),
             # 404 PAGE MUST BE LAST IN THIS LIST!
             (r".*", NoPageHandler)
         ]
