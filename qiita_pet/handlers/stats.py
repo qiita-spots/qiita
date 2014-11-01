0ff2886385c792bb551fa78fdf871225e928b0f8
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-11-01 11:08:29 -0600

    Remove time data from map
    Rendering this number of geotemporal data points would require more advanced
    techniques (e.g., WebGL) that are not a priority right now.
    
    Fixes #288

--- qiita_pet/handlers/stats.py
+++ qiita_pet/handlers/stats.py
@@ -17,36 +17,32 @@ class StatsHandler(BaseHandler):
         # check if the key exists in redis
         lats = r_server.lrange('stats:sample_lats', 0, -1)
         longs = r_server.lrange('stats:sample_longs', 0, -1)
-        times = r_server.lrange('stats:sample_times', 0, -1)
-        if not (lats or longs or times):
+        if not (lats or longs):
             # if we don't have them, then fetch from disk and add to the
             # redis server with a 1-hour expiration
             conn = SQLConnectionHandler()
-            sql = """select latitude, longitude, collection_timestamp
+            sql = """select latitude, longitude
                      from qiita.required_sample_info"""
-            lat_long_times = conn.execute_fetchall(sql)
+            lat_longs = conn.execute_fetchall(sql)
             with r_server.pipeline() as pipe:
-                for latitude, longitude, time in lat_long_times:
+                for latitude, longitude in lat_longs:
                     # storing as a simple data structure, hopefully this doesn't
                     # burn us later
                     pipe.rpush('stats:sample_lats', latitude)
                     pipe.rpush('stats:sample_longs', longitude)
-                    pipe.rpush('stats:sample_times', time)
 
                 # set the key to expire in an hour, so that we limit the number of
                 # times we have to go to the database to a reasonable amount
                 r_server.expire('stats:sample_lats', 3600)
                 r_server.expire('stats:sample_longs', 3600)
-                r_server.expire('stats:sample_times', 3600)
 
                 pipe.execute()
         else:
             # If we do have them, put the redis results into the same structure
             # that would come back from the database
-            times = [datetime.strptime(t, '%Y-%m-%d %H:%M:%S') for t in times]
             longs = [float(x) for x in longs]
             lats = [float(x) for x in lats]
-            lat_long_times = zip(lats, longs, times)
+            lat_longs = zip(lats, longs)
 
         # Get the number of studies
         num_studies = get_count('qiita.study')
@@ -57,11 +53,11 @@ class StatsHandler(BaseHandler):
         # Get the number of users
         num_users = get_count('qiita.qiita_user')
 
-        callback([num_studies, num_samples, num_users, lat_long_times])
+        callback([num_studies, num_samples, num_users, lat_longs])
 
     @coroutine
     def get(self):
-        num_studies, num_samples, num_users, lat_long_times = \
+        num_studies, num_samples, num_users, lat_longs = \
             yield Task(self._get_stats)
 
         # Pull a random public study from the database
@@ -69,6 +65,6 @@ class StatsHandler(BaseHandler):
 
         self.render('stats.html', user=self.current_user,
                     num_studies=num_studies, num_samples=num_samples,
-                    num_users=num_users, lat_long_times=lat_long_times,
+                    num_users=num_users, lat_longs=lat_longs,
                     random_study_info=study.info,
                     random_study_title=study.title, random_study_id=study.id)
