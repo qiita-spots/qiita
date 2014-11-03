6215af341ef1e4f19e6941f4b54d20e3a483aacf
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-11-03 14:38:39 -0700

    Do study access check in handler

--- qiita_pet/handlers/study_handlers.py
+++ qiita_pet/handlers/study_handlers.py
@@ -559,10 +559,10 @@ class MetadataSummaryHandler(BaseHandler):
             tid = int(self.get_argument('sample_template'))
             template = SampleTemplate(tid)
 
-        study = Study(study_id)
+        study = Study(template.study_id)
 
         # check whether or not the user has access to the requested information
-        if not template.has_access(User(self.current_user)):
+        if not study.has_access(User(self.current_user)):
             raise HTTPError(403, "You do not have access to access this "
                                  "information.")
 
