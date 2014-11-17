a56939b44f24d3f05e0f99c9b950cfa45a8833c3
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-11-17 12:55:53 -0700

    Roll together dispatchable EBI submit into CLI cmd
    The code executed by invoking the CLI call now does almost the same exact thing
    as the code executed when invoking the command through the web interface

--- qiita_ware/commands.py
+++ qiita_ware/commands.py
@@ -9,61 +9,119 @@
 from os.path import join, isdir
 from os import makedirs
 from functools import partial
+from tempfile import mkdtemp
+from gzip import open as gzopen
 
 from qiita_db.study import Study
 from qiita_db.data import PreprocessedData
+from qiita_db.metadata_template import PrepTemplate, SampleTemplate
+from qiita_core.qiita_settings import qiita_config
+
 from qiita_ware.ebi import EBISubmission
+from qiita_ware.demux import to_per_sample_ascii
+from qiita_ware.exceptions import ComputeError
+from qiita_ware.util import open_file
+from qiita_db.util import convert_to_id
+from qiita_db.ontology import Ontology
 
 
 ebi_actions = ['ADD', 'VALIDATE', 'MODIFY']
 
 
-def submit_EBI_from_files(preprocessed_data_id, sample_template, prep_template,
-                          fastq_dir_fp, output_dir_fp, investigation_type,
-                          action, send, new_investigation_type=None):
-    """EBI submission from files
+def submit_EBI(preprocessed_data_id, action, send, fastq_dir_fp=None):
+    """Submit a preprocessed data to EBI
 
     Parameters
     ----------
     preprocessed_data_id : int
         The preprocesssed data id
-    sample_template : File
-        The file handler of the sample template file
-    prep_template : File
-        The file handler of the prep template file
-    fastq_dir_fp : str
-        The fastq filepath
-    output_dir_fp : str
-        The output directory
-    investigation_type : str
-        The investigation type string
     action : %s
         The action to perform with this data
     send : bool
         True to actually send the files
-    new_investigation_type : str, optional
-        If investigation_type is `'Other'` then a value describing this new
-        investigation type should be specified. 'mimarks-survey' or
-        'metagenomics' are commonly used.
+    fastq_dir_fp : str, optional
+        The fastq filepath
     """
-
     preprocessed_data = PreprocessedData(preprocessed_data_id)
     preprocessed_data_id_str = str(preprocessed_data_id)
     study = Study(preprocessed_data.study)
+    sample_template = SampleTemplate(study.sample_template)
+    prep_template = PrepTemplate(preprocessed_data.prep_template)
 
-    # Get study-specific output directory and set filepaths
-    get_output_fp = partial(join, output_dir_fp)
+    investigation_type = None
+    new_investigation_type = None
+
+    status = preprocessed_data.submitted_to_insdc_status()
+    if status in ('submitting', 'success'):
+        raise ValueError("Cannot resubmit! Current status is: %s" % status)
+
+    if send:
+        # If we intend actually to send the files, then change the status in
+        # the database
+        preprocessed_data.update_insdc_status('submitting')
+
+    # we need to figure out whether the investigation type is a known one
+    # or if we have to submit a "new_investigation_type" to EBI
+    current_type = prep_template.investigation_type
+    ena_ontology = Ontology(convert_to_id('ENA', 'ontology'))
+    if current_type in ena_ontology.terms:
+        investigation_type = current_type
+    elif current_type in ena_ontology.user_defined_terms:
+        investigation_type = 'Other'
+        new_investigation_type = current_type
+    else:
+        # This should never happen
+        raise ValueError("Unrecognized investigation type: '%s'. This term "
+                         "is neither one of the official terms nor one of the "
+                         "user-defined terms in the ENA ontology")
+
+    if fastq_dir_fp:
+        # If the user specifies a FASTQ directory, use it
+
+        # Set demux_samples to None so that MetadataTemplate.to_file will put
+        # all samples in the template files
+        demux_samples = None
+    else:
+        # If the user does not specify a FASTQ directory, create one and
+        # re-serialize the per-sample FASTQs from the demux file
+        fastq_dir_fp = mkdtemp(prefix=qiita_config.working_dir)
+        demux = [path for path, ftype in preprocessed_data.get_filepaths()
+                 if ftype == 'preprocessed_demux'][0]
+
+        # Keep track of which files were actually in the demux file so that we
+        # can write those rows to the prep and samples templates
+        demux_samples = set()
+
+        with open_file(demux) as demux_fh:
+            for samp, iterator in to_per_sample_ascii(demux_fh,
+                                                      list(sample_template)):
+                demux_samples.add(samp)
+                sample_fp = join(fastq_dir_fp, "%s.fastq.gz" % samp)
+                with gzopen(sample_fp, 'w') as fh:
+                    for record in iterator:
+                        fh.write(record)
+
+    output_dir = fastq_dir_fp + '_submission'
+
+    samp_fp = join(fastq_dir_fp, 'sample_metadata.txt')
+    prep_fp = join(fastq_dir_fp, 'prep_metadata.txt')
+
+    sample_template.to_file(samp_fp, demux_samples)
+    prep_template.to_file(prep_fp, demux_samples)
+
+    # Get specific output directory and set filepaths
+    get_output_fp = partial(join, output_dir)
     study_fp = get_output_fp('study.xml')
     sample_fp = get_output_fp('sample.xml')
     experiment_fp = get_output_fp('experiment.xml')
     run_fp = get_output_fp('run.xml')
     submission_fp = get_output_fp('submission.xml')
 
-    if not isdir(output_dir_fp):
-        makedirs(output_dir_fp)
+    if not isdir(output_dir):
+        makedirs(output_dir)
     else:
-        raise ValueError('The output folder already exists: %s' %
-                         output_dir_fp)
+        raise IOError('The output folder already exists: %s' %
+                      output_dir)
 
     submission = EBISubmission.from_templates_and_per_sample_fastqs(
         preprocessed_data_id_str, study.title, study.info['study_abstract'],
@@ -77,10 +135,18 @@ def submit_EBI_from_files(preprocessed_data_id, sample_template, prep_template,
     if send:
         submission.send_sequences()
         study_accession, submission_accession = submission.send_xml()
+
+        if study_accession is None or submission_accession is None:
+            preprocessed_data.update_insdc_status('failed')
+
+            raise ComputeError("EBI Submission failed!")
+        else:
+            preprocessed_data.update_insdc_status('success', study_accession,
+                                                  submission_accession)
     else:
         study_accession, submission_accession = None, None
 
     return study_accession, submission_accession
 
 
-submit_EBI_from_files.__doc__ %= ebi_actions
+submit_EBI.__doc__ %= ebi_actions
--- qiita_ware/dispatchable.py
+++ qiita_ware/dispatchable.py
@@ -1,22 +1,13 @@
-from os.path import join
-from tempfile import mkdtemp
-from gzip import open as gzopen
 from traceback import format_exception_only
 from sys import exc_info
 
 from .processing_pipeline import StudyPreprocessor
 from .analysis_pipeline import RunAnalysis
-from qiita_core.qiita_settings import qiita_config
-from qiita_ware.commands import submit_EBI_from_files
-from qiita_ware.demux import to_per_sample_ascii
-from qiita_ware.exceptions import ComputeError
-from qiita_ware.util import open_file
-from qiita_db.util import convert_to_id
+from qiita_ware.commands import submit_EBI
 from qiita_db.study import Study
 from qiita_db.analysis import Analysis
-from qiita_db.metadata_template import SampleTemplate, PrepTemplate
+from qiita_db.metadata_template import PrepTemplate
 from qiita_db.data import RawData
-from qiita_db.ontology import Ontology
 
 
 def preprocessor(study_id, prep_template_id, param_id, param_constructor):
@@ -38,63 +29,9 @@ def preprocessor(study_id, prep_template_id, param_id, param_constructor):
 
 def submit_to_ebi(preprocessed_data_id, submission_type):
     """Submit a study to EBI"""
-    from qiita_db.data import PreprocessedData
-
-    preprocessed_data = PreprocessedData(preprocessed_data_id)
-    pt = PrepTemplate(preprocessed_data.prep_template)
-    st = SampleTemplate(preprocessed_data.study)
-
-    investigation_type = None
-    new_investigation_type = None
-
-    state = preprocessed_data.submitted_to_insdc_status()
-    if state in ('submitting', 'success'):
-        raise ValueError("Cannot resubmit! Current state is: %s" % state)
-
-    # we need to figure out whehter the investigation type is a known one
-    # or if we have to submit a "new_investigation_type" to EBI
-    current_type = pt.investigation_type
-    if current_type not in Ontology(convert_to_id('ENA', 'ontology')).terms:
-        investigation_type = 'Other'
-        new_investigation_type = current_type
-    else:
-        investigation_type = current_type
-
-    demux = [path for path, ftype in preprocessed_data.get_filepaths()
-             if ftype == 'preprocessed_demux'][0]
-
-    tmp_dir = mkdtemp(prefix=qiita_config.working_dir)
-    output_dir = tmp_dir + '_submission'
-
-    samp_fp = join(tmp_dir, 'sample_metadata.txt')
-    prep_fp = join(tmp_dir, 'prep_metadata.txt')
-
-    st.to_file(samp_fp)
-    pt.to_file(prep_fp)
-
-    with open_file(demux) as demux_fh:
-        for samp, iterator in to_per_sample_ascii(demux_fh, list(st)):
-            with gzopen(join(tmp_dir, "%s.fastq.gz" % samp), 'w') as fh:
-                for record in iterator:
-                    fh.write(record)
-
-    preprocessed_data.update_insdc_status('submitting')
-    study_acc, submission_acc = submit_EBI_from_files(preprocessed_data_id,
-                                                      open(samp_fp),
-                                                      open(prep_fp), tmp_dir,
-                                                      output_dir,
-                                                      investigation_type,
-                                                      submission_type, True,
-                                                      new_investigation_type)
-
-    if study_acc is None or submission_acc is None:
-        preprocessed_data.update_insdc_status('failed')
-
-        # this will set the job status as failed
-        raise ComputeError("EBI Submission failed!")
-    else:
-        preprocessed_data.update_insdc_status('success', study_acc,
-                                              submission_acc)
+    study_acc, submission_acc = submit_EBI(preprocessed_data_id,
+                                           submission_type,
+                                           True)
 
     return study_acc, submission_acc
 
