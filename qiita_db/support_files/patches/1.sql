74c33abe1291a9d46293881324f8f769d5dab593
Jose Navas <josenavasmolina@gmail.com>
2014-11-17 15:27:04 -0700

    Removing the id from the env packages table as name is a natural PK

--- qiita_db/support_files/patches/1.sql
+++ qiita_db/support_files/patches/1.sql
@@ -2,34 +2,33 @@
 -- This patch adds to the database the structure needed for supporting environmental packages
 
 CREATE TABLE qiita.environmental_package ( 
-	environmental_package_id bigserial  NOT NULL,
-	name                 varchar  NOT NULL,
+	environmental_package_name varchar  NOT NULL,
 	metadata_table       varchar  NOT NULL,
-	CONSTRAINT pk_environmental_package PRIMARY KEY ( environmental_package_id )
+	CONSTRAINT pk_environmental_package PRIMARY KEY ( environmental_package_name )
  ) ;
 
-COMMENT ON COLUMN qiita.environmental_package.name IS 'The name of the environmental package';
+COMMENT ON COLUMN qiita.environmental_package.environmental_package_name IS 'The name of the environmental package';
 
 COMMENT ON COLUMN qiita.environmental_package.metadata_table IS 'Contains the name of the table that contains the pre-defined metadata columns for the environmental package';
 
 CREATE TABLE qiita.study_environmental_package ( 
 	study_id             bigint  NOT NULL,
-	environmental_package_id bigint  NOT NULL,
-	CONSTRAINT pk_study_environmental_package PRIMARY KEY ( study_id, environmental_package_id )
+	environmental_package_name varchar  NOT NULL,
+	CONSTRAINT pk_study_environmental_package PRIMARY KEY ( study_id, environmental_package_name )
  ) ;
 
 CREATE INDEX idx_study_environmental_package ON qiita.study_environmental_package ( study_id ) ;
 
-CREATE INDEX idx_study_environmental_package_0 ON qiita.study_environmental_package ( environmental_package_id ) ;
+CREATE INDEX idx_study_environmental_package_0 ON qiita.study_environmental_package ( environmental_package_name ) ;
 
-COMMENT ON TABLE qiita.study_environmental_package IS 'Holds the 1 to many relationship between the study and the environmental packages';
+COMMENT ON TABLE qiita.study_environmental_package IS 'Holds the 1 to many relationship between the study and the environmental_package';
 
 ALTER TABLE qiita.study_environmental_package ADD CONSTRAINT fk_study_environmental_package FOREIGN KEY ( study_id ) REFERENCES qiita.study( study_id )    ;
 
-ALTER TABLE qiita.study_environmental_package ADD CONSTRAINT fk_study_environmental_package_0 FOREIGN KEY ( environmental_package_id ) REFERENCES qiita.environmental_package( environmental_package_id )    ;
+ALTER TABLE qiita.study_environmental_package ADD CONSTRAINT fk_study_environmental_package_0 FOREIGN KEY ( environmental_package_name ) REFERENCES qiita.environmental_package( environmental_package_name )    ;
 
 -- We insert the environmental packages that we know
-INSERT INTO qiita.environmental_package (name, metadata_table) VALUES
+INSERT INTO qiita.environmental_package (environmental_package_name, metadata_table) VALUES
 	('air', 'ep_air'),
 	('built environment', 'ep_built_environment'),
 	('host-associated', 'ep_host_associated'),
