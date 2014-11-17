5602cae08950efef8706d2e6411b372c0c3cf8fe
Jose Navas <josenavasmolina@gmail.com>
2014-11-17 12:39:59 -0700

    Modifiying the structure of the database for env packages, so each env package will have it's own table

--- qiita_db/support_files/patches/1.sql
+++ qiita_db/support_files/patches/1.sql
@@ -1,35 +1,17 @@
--- Nov 14, 2014
--- This patch adds to the database the tables needed for supporting environmental packages
+-- Nov 17, 2014
+-- This patch adds to the database the structure needed for supporting environmental packages
 
--- The environmental package table
 CREATE TABLE qiita.environmental_package ( 
 	environmental_package_id bigserial  NOT NULL,
 	name                 varchar  NOT NULL,
+	metadata_table       varchar  NOT NULL,
 	CONSTRAINT pk_environmental_package PRIMARY KEY ( environmental_package_id )
  ) ;
 
-COMMENT ON TABLE qiita.environmental_package IS 'Holds the information for each environmental package';
+COMMENT ON COLUMN qiita.environmental_package.name IS 'The name of the environmental package';
 
-COMMENT ON COLUMN qiita.environmental_package.name IS 'Holds the name of the environmental package';
+COMMENT ON COLUMN qiita.environmental_package.metadata_table IS 'Contains the name of the table that contains the pre-defined metadata columns for the environmental package';
 
--- Which columns are required for each environmental package
-CREATE TABLE qiita.environmental_package_columns ( 
-	environmental_package_id bigint  NOT NULL,
-	column_name          varchar  NOT NULL,
-	column_type          varchar  NOT NULL,
-	CONSTRAINT pk_environmental_package_columns PRIMARY KEY ( environmental_package_id, column_name, column_type )
- ) ;
-
-CREATE INDEX idx_environmental_package_columns ON qiita.environmental_package_columns ( environmental_package_id ) ;
-
-COMMENT ON TABLE qiita.environmental_package_columns IS 'Holds the required columns for the environmental packages';
-
-COMMENT ON COLUMN qiita.environmental_package_columns.column_name IS 'Holds the name of the required column';
-
-COMMENT ON COLUMN qiita.environmental_package_columns.column_type IS 'Holds the type of the required column';
-
-
--- The 1 to N relationship between the study and the environmental packages
 CREATE TABLE qiita.study_environmental_package ( 
 	study_id             bigint  NOT NULL,
 	environmental_package_id bigint  NOT NULL,
@@ -40,13 +22,29 @@ CREATE INDEX idx_study_environmental_package ON qiita.study_environmental_packag
 
 CREATE INDEX idx_study_environmental_package_0 ON qiita.study_environmental_package ( environmental_package_id ) ;
 
-COMMENT ON TABLE qiita.study_environmental_package IS 'Holds the 1 to many relationship between the study and the environmental package.';
-
-ALTER TABLE qiita.environmental_package_columns ADD CONSTRAINT fk_environmental_package_columns FOREIGN KEY ( environmental_package_id ) REFERENCES qiita.environmental_package( environmental_package_id )    ;
+COMMENT ON TABLE qiita.study_environmental_package IS 'Holds the 1 to many relationship between the study and the environmental packages';
 
 ALTER TABLE qiita.study_environmental_package ADD CONSTRAINT fk_study_environmental_package FOREIGN KEY ( study_id ) REFERENCES qiita.study( study_id )    ;
 
 ALTER TABLE qiita.study_environmental_package ADD CONSTRAINT fk_study_environmental_package_0 FOREIGN KEY ( environmental_package_id ) REFERENCES qiita.environmental_package( environmental_package_id )    ;
 
 -- We insert the environmental packages that we know
-INSERT INTO qiita.environmental_package (name) VALUES ('air'), ('built environment'), ('host-associated'), ('human-amniotic-fluid'), ('human-associated'), ('human-blood'), ('human-gut'), ('human-oral'), ('human-skin'), ('human-urine'), ('human-vaginal'), ('microbial mat/biofilm'), ('miscellaneous natural or artificial environment'), ('plant-associated'), ('sediment'), ('soil'), ('wastewater/sludge'), ('water');
+INSERT INTO qiita.environmental_package (name, metadata_table) VALUES
+	('air', 'ep_air'),
+	('built environment', 'ep_built_environment'),
+	('host-associated', 'ep_host_associated'),
+	('human-amniotic-fluid', 'ep_human_amniotic_fluid'),
+	('human-associated', 'ep_human_associated'),
+	('human-blood', 'ep_human_blood'),
+	('human-gut', 'ep_human_gut'),
+	('human-oral', 'ep_human_oral'),
+	('human-skin', 'ep_human_skin'),
+	('human-urine', 'ep_human_urine'),
+	('human-vaginal', 'ep_human_vaginal'),
+	('microbial mat/biofilm', 'ep_microbial_mat_biofilm'),
+	('miscellaneous natural or artificial environment', 'ep_misc_artif'),
+	('plant-associated', 'ep_plant_associated'),
+	('sediment', 'ep_sediment'),
+	('soil', 'ep_soil'),
+	('wastewater/sludge', 'ep_wastewater_sludge'),
+	('water', 'ep_water');
