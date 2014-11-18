4bf123484bb61bc2baf000e269a5b927fbdcce00
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-11-18 12:30:12 -0700

    Add headers to download rather than display file

--- qiita_pet/handlers/download.py
+++ qiita_pet/handlers/download.py
@@ -2,10 +2,15 @@ from tornado.web import authenticated
 
 from .base_handlers import BaseHandler
 
+
 class DownloadHandler(BaseHandler):
     @authenticated
     def get(self, filepath_id):
+        # Check access to file
+        self.set_header('Content-Description', 'File Transfer')
+        self.set_header('Content-Type', 'application/octet-stream')
+        self.set_header('Content-Transfer-Encoding', 'binary')
+        self.set_header('Expires',  '0')
         self.set_header('X-Accel-Redirect',
-                        '/protected/preprocessed_data/' +
-                        filepath_id)
+                        '/protected/preprocessed_data/' + filepath_id)
         self.finish()
