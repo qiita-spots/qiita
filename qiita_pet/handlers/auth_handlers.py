edb7cb5bd2010acf78238810382083157aad55db
Joshua Shorenstein <squirrelo@gmail.com>
2014-12-02 14:52:07 -0700

    Rediret to mainpage if login from auth pages. Fix #697

--- qiita_pet/handlers/auth_handlers.py
+++ qiita_pet/handlers/auth_handlers.py
@@ -82,7 +82,10 @@ class AuthLoginHandler(BaseHandler):
         passwd = self.get_argument("password", "")
         nextpage = self.get_argument("next", None)
         if nextpage is None:
-            nextpage = self.request.headers['Referer']
+            if "auth" not in self.request.headers['Referer']:
+                nextpage = self.request.headers['Referer']
+            else:
+                nextpage = "/"
 
         msg = ""
         # check the user level
