6a922336e0a74d237be5434764312c43774f7bc6
Jose Navas <josenavasmolina@gmail.com>
2014-12-04 14:23:37 -0700

    Simplifying template parameters for the sample template tab

--- qiita_pet/uimodules/sample_template_tab.py
+++ qiita_pet/uimodules/sample_template_tab.py
@@ -15,27 +15,30 @@ from qiita_db.util import get_files_from_uploads_folders
 
 class SampleTemplateTab(UIModule):
     def render(self, study):
-        study_status = study.status
-        user = User(self.current_user)
-        user_level = user.level
+        # Retrieve the files from the uploads folder, so the user can choose
+        # the sample template of the study
         files = get_files_from_uploads_folders(str(study.id))
 
-        ste = SampleTemplate.exists(study.id)
-        if ste:
+        # If the sample template exists, retrieve all its filepaths
+        if SampleTemplate.exists(study.id):
             sample_templates = SampleTemplate(study.id).get_filepaths()
         else:
+            # If the sample template does not exist, just pass an empty list
             sample_templates = []
 
         # Check if the request came from a local source
         is_local_request = ('localhost' in self.request.headers['host'] or
                             '127.0.0.1' in self.request.headers['host'])
 
+        # The user can choose the sample template only if the study is
+        # sandboxed or the current user is an admin
+        show_select_sample = (study.status == 'sandbox'
+                              or User(self.current_user).level == 'admin')
+
         return self.render_string(
             "sample_template_tab.html",
-            study_status=study_status,
-            user_level=user_level,
+            show_select_sample=show_select_sample,
             files=files,
             study_id=study.id,
             sample_templates=sample_templates,
-            is_local_request=is_local_request,
-            ste=ste)
+            is_local_request=is_local_request)
