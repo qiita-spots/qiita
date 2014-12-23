05552015a15cc968abfb3580cd89070460b2f4ff
Daniel McDonald <mcdonadt@colorado.edu>
2014-12-23 09:54:29 -0700

    TST: updating numbers

--- qiita_db/test/test_setup.py
+++ qiita_db/test/test_setup.py
@@ -41,19 +41,19 @@ class SetupTest(TestCase):
         self.assertEqual(get_count("qiita.study_status"), 4)
 
     def test_filepath(self):
-        self.assertEqual(get_count("qiita.filepath"), 16)
+        self.assertEqual(get_count("qiita.filepath"), 18)
 
     def test_filepath_type(self):
         self.assertEqual(get_count("qiita.filepath_type"), 19)
 
     def test_raw_data(self):
-        self.assertEqual(get_count("qiita.raw_data"), 2)
+        self.assertEqual(get_count("qiita.raw_data"), 4)
 
     def test_raw_filepath(self):
-        self.assertEqual(get_count("qiita.raw_filepath"), 6)
+        self.assertEqual(get_count("qiita.raw_filepath"), 4)
 
     def test_study_raw_data(self):
-        self.assertEqual(get_count("qiita.study_raw_data"), 2)
+        self.assertEqual(get_count("qiita.study_raw_data"), 4)
 
     def test_required_sample_info(self):
         self.assertEqual(get_count("qiita.required_sample_info"), 27)
