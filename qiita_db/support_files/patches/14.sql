60979f74b653cce9a6981f3205bf973dce38f803
Joshua Shorenstein <squirrelo@gmail.com>
2015-02-11 15:17:53 -0800

    update patches

--- qiita_db/support_files/patches/14.sql
+++ qiita_db/support_files/patches/14.sql
@@ -1,2 +1,4 @@
+
+--Feb 11, 2015
 --Placeholder for python to run
 select * from settings;
--- qiita_db/support_files/patches/python_patches/14.py
+++ qiita_db/support_files/patches/python_patches/14.py
@@ -1,15 +1,26 @@
-from qiita_db.sql_connection import SQLConnectionHandler
-from os.path import basename
-
+# Feb 11, 2015
 # This changes all analysis files to be relative path instead of absolute
+
+from os.path import basename, dirname
+
+from qiita_db.util import get_mountpoint
+from qiita_db.sql_connection import SQLConnectionHandler
 
 conn_handler = SQLConnectionHandler()
 
 filepaths = conn_handler.execute_fetchall(
     'SELECT f.* from qiita.filepath f JOIN qiita.analysis_filepath afp ON '
     'f.filepath_id = afp.filepath_id')
+
+# retrieve relative filepaths as dictionary for matching
+mountpoints = {m[1]: m[0] for m in get_mountpoint(
+    'analysis', conn_handler=conn_handler, retrieve_all=True)}
+
 for filepath in filepaths:
     filename = basename(filepath['filepath'])
+    # find the ID of the analysis filepath used
+    mp_id = mountpoints[dirname(filepath['filepath'])]
     conn_handler.execute(
-        'UPDATE qiita.filepath SET filepath = %s WHERE filepath_id = %s',
-        [filename, filepath['filepath_id']])
+        'UPDATE qiita.filepath SET filepath = %s, data_directory_id = %s WHERE'
+        ' filepath_id = %s',
+        [filename, mp_id, filepath['filepath_id']])
