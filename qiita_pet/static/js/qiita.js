fcb4003fa7449016ee5c40eb764c4f3ae5cb8c23
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-03-11 11:20:30 -0700

    BUG: Add missing qiita.js file

---
+++ qiita_pet/static/js/qiita.js
@@ -0,0 +1,23 @@
+/*
+ * bootstrapAlert will add a Bootstrap alert message to the body of the current
+ * page.
+ *
+ * @param message: Message to display
+ * @param severit: One of 'danger', 'info', 'warning' or 'success'.
+ *
+ */
+function bootstrapAlert(message, severity){
+
+  severity = typeof severity !== 'undefined' ? severity : 'danger';
+
+  var alertDiv = $('<div>', { 'class': 'alert alert-'+severity, 'role': 'alert'});
+
+  alertDiv.append('<a href="#" class="close" data-dismiss="alert">&times;</a>');
+  alertDiv.append('<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>');
+
+  // prepend a space to separate the message from the '!' icon
+  alertDiv.append(' '+message);
+
+  $('body').prepend(alertDiv);
+
+}
