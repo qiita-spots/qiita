b6735056f133088e9a3e6b533d99ba5d54c721e6
Joshua Shorenstein <squirrelo@gmail.com>
2015-03-25 21:23:09 -0700

    address comments

--- qiita_ware/search.py
+++ qiita_ware/search.py
@@ -1,4 +1,5 @@
 from collections import Counter, defaultdict
+from itertools import izip
 
 from future.utils import viewvalues, viewitems
 
@@ -8,12 +9,14 @@ from qiita_db.data import ProcessedData
 
 def count_metadata(results, meta_cols):
     """Counts the metadata found in a search, and returns these counts
+
     Parameters
     ----------
     results : dict of lists of list
         results in the format returned by the qiita_db search obj
     meta_cols : list
         metadata column names searched for, as returned by qiita_db search obj
+
     Returns
     -------
     fullcount : dict of dicts
@@ -39,26 +42,29 @@ def count_metadata(results, meta_cols):
     # Now get metadata counts for each study, removing sample ids as before
     studycount = {}
     for study_id in results:
-        studycount[study_id] = {}
+        hold = {}
         # zip all samples for a given study so that each metadata column found
         # is its own list
         meta_vals = zip(*(sample for sample in results[study_id]))
         for pos, cat in enumerate(meta_cols):
             # use Counter object to count all metadata values for a column
             # pos+1 so we skip the sample names list
-            studycount[study_id][cat] = Counter(meta_vals[pos + 1])
+            hold[cat] = Counter(meta_vals[pos + 1])
+        studycount[study_id] = hold
 
     return fullcount, studycount
 
 
 def filter_by_processed_data(results, datatypes=None):
     """Filters results to what is available in each processed data
+
     Parameters
     ----------
     results : dict of lists of list
         results in the format returned by the qiita_db search obj
     datatypes : list of str, optional
         Datatypes to selectively return. Default all datatypes available
+
     Returns
     -------
     study_proc_ids : dict of dicts of lists
@@ -85,12 +91,12 @@ def filter_by_processed_data(results, datatypes=None):
             if datatypes is not None and datatype not in datatypes:
                 continue
             samps_available = proc_data.samples
-            proc_data_samples[proc_data_id] = [s[0] for s in study_samples
-                                               if s[0] in samps_available]
-            if len(proc_data_samples[proc_data_id]) == 0:
+            hold = [s[0] for s in study_samples if s[0] in samps_available]
+            if len(hold) == 0:
                 # all samples filtered so remove it as a result
                 del(proc_data_samples[proc_data_id])
             else:
+                proc_data_samples[proc_data_id] = hold
                 # add the processed data to the list for the study
                 study_proc_ids[study_id][datatype].append(proc_data_id)
     return study_proc_ids, proc_data_samples, samples_meta
