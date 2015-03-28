5cc71a04b1131cb4d2fa98789ea0e3c2b2e569c6
Jose Navas <josenavasmolina@gmail.com>
2015-03-28 16:09:35 -0700

    New metadata template code layout

---
+++ qiita_db/metadata_template/__init__.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+__version__ = "0.0.1-dev"
+
+from .sample_template import SampleTemplate
+from .prep_template import PrepTemplate
+
+__all__ = ['SampleTemplate', 'PrepTemplate']
