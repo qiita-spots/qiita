f0c3f8052921ddd23f460acc8f659ddf0a638fa4
Joshua Shorenstein <squirrelo@gmail.com>
2015-03-31 10:29:18 -0700

    add default analyses using only SQL

--- qiita_db/support_files/patches/20.sql
+++ qiita_db/support_files/patches/20.sql
@@ -1,3 +1,12 @@
 -- March 28, 2015
 -- Add default analyses for all existing users
-SELECT 42;
+DO $do$
+DECLARE 
+	eml varchar;
+BEGIN
+FOR eml IN
+	SELECT email FROM qiita.qiita_user
+LOOP
+	INSERT INTO qiita.analysis(email, name, description, dflt, analysis_status_id) VALUES (eml, eml || '-dflt', 'dflt', true, 1);
+END LOOP;
+END $do$;
