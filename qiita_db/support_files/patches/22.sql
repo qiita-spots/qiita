d17b6710b2baf82e04f681f74ed86dac657a90cb
Joshua Shorenstein <squirrelo@gmail.com>
2015-04-23 10:35:38 -0700

    update patch 22

--- qiita_db/support_files/patches/22.sql
+++ qiita_db/support_files/patches/22.sql
@@ -1,3 +1,15 @@
 -- April 16, 2015
--- Add primary key to analysis_sample table
-ALTER TABLE qiita.analysis_sample ADD CONSTRAINT pk_analysis_sample PRIMARY KEY ( analysis_id, processed_data_id, sample_id );
+-- Add primary key to analysis_sample table, first deleting the duplicates
+-- http://stackoverflow.com/a/9862688
+DO $do$
+BEGIN
+    CREATE TEMP TABLE temp_table
+    ON COMMIT drop AS
+        SELECT analysis_id, processed_data_id, sample_id,
+        ROW_NUMBER() OVER(PARTITION BY analysis_id, processed_data_id, sample_id ORDER BY (SELECT 0)) AS rn
+        FROM qiita.analysis_sample;
+    DELETE FROM qiita.analysis_sample;
+    INSERT INTO qiita.analysis_sample (analysis_id, processed_data_id, sample_id) SELECT analysis_id, processed_data_id, sample_id FROM temp_table WHERE rn = 1;
+
+    ALTER TABLE qiita.analysis_sample ADD CONSTRAINT pk_analysis_sample PRIMARY KEY ( analysis_id, processed_data_id, sample_id );
+END $do$
