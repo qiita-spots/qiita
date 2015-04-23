961c75f83748307d684c4799b579e316eb8ebd4d
Joshua Shorenstein <squirrelo@gmail.com>
2015-04-23 13:40:46 -0700

    use group by to simplify

--- qiita_db/support_files/patches/22.sql
+++ qiita_db/support_files/patches/22.sql
@@ -5,11 +5,10 @@ DO $do$
 BEGIN
     CREATE TEMP TABLE temp_table
     ON COMMIT drop AS
-        SELECT analysis_id, processed_data_id, sample_id,
-        ROW_NUMBER() OVER(PARTITION BY analysis_id, processed_data_id, sample_id ORDER BY (SELECT 0)) AS rn
-        FROM qiita.analysis_sample;
+        SELECT analysis_id, processed_data_id, sample_id
+        FROM qiita.analysis_sample GROUP BY analysis_id, processed_data_id, sample_id;
     DELETE FROM qiita.analysis_sample;
-    INSERT INTO qiita.analysis_sample (analysis_id, processed_data_id, sample_id) SELECT analysis_id, processed_data_id, sample_id FROM temp_table WHERE rn = 1;
+    INSERT INTO qiita.analysis_sample (analysis_id, processed_data_id, sample_id) SELECT analysis_id, processed_data_id, sample_id FROM temp_table;
 
     ALTER TABLE qiita.analysis_sample ADD CONSTRAINT pk_analysis_sample PRIMARY KEY ( analysis_id, processed_data_id, sample_id );
 END $do$
