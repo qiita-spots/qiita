5eb78ef206809d37347840acf72ba08593d4a2f8
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-04-30 15:59:16 -0700

    REL: Bump version to 0.1.0-dev & update CHANGELOG

--- qiita_ware/__init__.py
+++ qiita_ware/__init__.py
@@ -6,4 +6,4 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-__version__ = "0.1.0"
+__version__ = "0.1.0-dev"
