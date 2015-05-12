0ca9bac6917db5d867d3cef5219f3cf35db9312f
Joshua Shorenstein <squirrelo@gmail.com>
2015-05-12 14:30:27 -0700

    add logging tables for prep and sample template edits

--- qiita_db/support_files/patches/25.sql
+++ qiita_db/support_files/patches/25.sql
@@ -1,3 +1,29 @@
 -- May 12, 2015
 -- Add in analysis status marking that undelying data has changed.
+-- Add logging tables for changes to sample and prep templates
 INSERT INTO qiita.analysis_status (status) VALUES ('altered_data');
+
+-- Add logging tables for template edits
+CREATE TABLE qiita.sample_template_edit (
+	sample_template_id   bigint  NOT NULL,
+	change               varchar  NOT NULL,
+	timestamp            timestamp DEFAULT current_timestamp NOT NULL,
+	CONSTRAINT idx_sample_template_edit PRIMARY KEY ( sample_template_id, change )
+ );
+
+CREATE INDEX idx_sample_template_edit_0 ON qiita.sample_template_edit ( sample_template_id );
+
+COMMENT ON TABLE qiita.sample_template_edit IS 'Holds information on edits made to sample templates';
+
+ALTER TABLE qiita.sample_template_edit ADD CONSTRAINT fk_sample_template_edit FOREIGN KEY ( sample_template_id ) REFERENCES qiita.study( study_id );
+
+CREATE TABLE qiita.prep_template_edit (
+	prep_template_id     bigint  NOT NULL,
+	change               varchar  NOT NULL,
+	timestamp            timestamp DEFAULT current_timestamp NOT NULL,
+	CONSTRAINT idx_prep_template_edit_0 PRIMARY KEY ( prep_template_id, change )
+ );
+
+CREATE INDEX idx_prep_template_edit ON qiita.prep_template_edit ( prep_template_id );
+
+ALTER TABLE qiita.prep_template_edit ADD CONSTRAINT fk_prep_template_edit FOREIGN KEY ( prep_template_id ) REFERENCES qiita.prep_template( prep_template_id );
