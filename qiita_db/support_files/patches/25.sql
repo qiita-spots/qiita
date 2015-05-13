29b9b3db87d5a90c80fcde377e8fe46b1c990dde
Joshua Shorenstein <squirrelo@gmail.com>
2015-05-13 15:43:49 -0700

    fix patch to use study_id

--- qiita_db/support_files/patches/25.sql
+++ qiita_db/support_files/patches/25.sql
@@ -5,17 +5,17 @@ INSERT INTO qiita.analysis_status (status) VALUES ('altered_data');
 
 -- Add logging tables for template edits
 CREATE TABLE qiita.sample_template_edit (
-	sample_template_id   bigint  NOT NULL,
+	study_id   bigint  NOT NULL,
 	change               varchar  NOT NULL,
 	timestamp            timestamp DEFAULT current_timestamp NOT NULL,
-	CONSTRAINT idx_sample_template_edit PRIMARY KEY ( sample_template_id, change )
+	CONSTRAINT idx_sample_template_edit PRIMARY KEY ( study_id, change )
  );
 
-CREATE INDEX idx_sample_template_edit_0 ON qiita.sample_template_edit ( sample_template_id );
+CREATE INDEX idx_sample_template_edit_0 ON qiita.sample_template_edit ( study_id );
 
 COMMENT ON TABLE qiita.sample_template_edit IS 'Holds information on edits made to sample templates';
 
-ALTER TABLE qiita.sample_template_edit ADD CONSTRAINT fk_sample_template_edit FOREIGN KEY ( sample_template_id ) REFERENCES qiita.study( study_id );
+ALTER TABLE qiita.sample_template_edit ADD CONSTRAINT fk_sample_template_edit FOREIGN KEY ( study_id ) REFERENCES qiita.study( study_id );
 
 CREATE TABLE qiita.prep_template_edit (
 	prep_template_id     bigint  NOT NULL,
