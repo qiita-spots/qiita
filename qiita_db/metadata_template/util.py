2494f33484c5a0635863d8f236253e0d9054ba27
Joshua Shorenstein <squirrelo@gmail.com>
2015-05-20 12:46:17 -0700

    add QiitaDBError as displayed error

--- qiita_db/metadata_template/util.py
+++ qiita_db/metadata_template/util.py
@@ -141,13 +141,15 @@ def load_template_to_dataframe(fn, strip_whitespace=True):
     Raises
     ------
     ValueError
-        Empty file passed, or non UTF-8 characters are found in the file.
+        Empty file passed
     QiitaDBColumnError
         If the sample_name column is not present in the template.
         If there's a value in one of the reserved columns that cannot be cast
         to the needed type.
     QiitaDBWarning
         When columns are dropped because they have no content for any sample.
+    QiitaDBError
+        When non UTF-8 characters are found in the file.
 
     Notes
     -----
@@ -242,8 +244,8 @@ def load_template_to_dataframe(fn, strip_whitespace=True):
                     cell.encode('utf-8')
                 except UnicodeError:
                     errors.append('row %d, header %s' % (row, headers[col]))
-        raise ValueError('Non UTF-8 characters found at ' +
-                         '; '.join(errors))
+        raise QiitaDBError('Non UTF-8 characters found at ' +
+                           '; '.join(errors))
 
     # let pandas infer the dtypes of these columns, if the inference is
     # not correct, then we have to raise an error
