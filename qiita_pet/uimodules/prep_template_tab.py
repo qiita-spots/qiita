8fb00cca22a1bd3624bf0d98172613fedcf86c96
Jose Navas <josenavasmolina@gmail.com>
2015-06-05 12:11:54 -0700

    Showing the raw data of all accessible studies, not only the owned ones

--- qiita_pet/uimodules/prep_template_tab.py
+++ qiita_pet/uimodules/prep_template_tab.py
@@ -39,9 +39,11 @@ def _get_accessible_raw_data(user):
     raw_data
     """
     d = {}
-    for sid in user.user_studies:
+    accessible_studies = user.user_studies.union(user.shared_studies)
+    for sid in accessible_studies:
         for rdid in Study(sid).raw_data():
-            d[int(rdid)] = Study(RawData(rdid).studies[-1]).title
+            if rdid not in d:
+                d[int(rdid)] = Study(RawData(rdid).studies[-1]).title
     return d
 
 
