854de2d0a50805dc4feba06120c8436e00328928
Joshua Shorenstein <squirrelo@gmail.com>
2015-06-16 10:37:29 -0700

    move study portal to its own table

--- qiita_db/support_files/patches/26.sql
+++ qiita_db/support_files/patches/26.sql
@@ -23,3 +23,24 @@ DELETE FROM qiita.portal_type WHERE portal = 'QIIME_EMP';
 UPDATE qiita.portal_type
 SET portal = 'QIITA', portal_description = 'QIITA portal. Access to all data stored in database.'
 WHERE portal_type_id = 1;
+
+-- Remove study portal info to it's own table, as one study can be in multiple portals
+CREATE TABLE qiita.study_portal (
+	study_id             bigint  NOT NULL,
+	portal_type_id       bigint  NOT NULL,
+	CONSTRAINT pk_study_portal PRIMARY KEY ( study_id, portal_type_id )
+ );
+
+CREATE INDEX idx_study_portal ON qiita.study_portal ( study_id );
+
+CREATE INDEX idx_study_portal_0 ON qiita.study_portal ( portal_type_id );
+
+COMMENT ON TABLE qiita.study_portal IS 'Controls what studies are visible on what portals';
+
+ALTER TABLE qiita.study_portal ADD CONSTRAINT fk_study_portal FOREIGN KEY ( study_id ) REFERENCES qiita.study( study_id );
+
+ALTER TABLE qiita.study_portal ADD CONSTRAINT fk_study_portal_0 FOREIGN KEY ( portal_type_id ) REFERENCES qiita.portal_type( portal_type_id );
+
+INSERT INTO qiita.study_portal (study_id, portal_type_id) SELECT s.study_id, s.portal_type_id FROM qiita.study s;
+
+ALTER TABLE qiita.study DROP COLUMN portal_type_id;
--- qiita_db/support_files/populate_test_db.sql
+++ qiita_db/support_files/populate_test_db.sql
@@ -30,14 +30,17 @@ INSERT INTO qiita.study_person (name, email, affiliation, address, phone) VALUES
 INSERT INTO qiita.study (email, emp_person_id, first_contact,
     funding, timeseries_type_id, lab_person_id, metadata_complete,
     mixs_compliant, most_recent_contact, number_samples_collected,
-    number_samples_promised, portal_type_id, principal_investigator_id, reprocess,
+    number_samples_promised, principal_investigator_id, reprocess,
     spatial_series, study_title, study_alias, study_description,
     study_abstract, vamps_id) VALUES
     ('test@foo.bar', 2, '2014-05-19 16:10', NULL, 1, 1, TRUE, TRUE,
-    '2014-05-19 16:11', 27, 27, 2, 3, FALSE, FALSE,
+    '2014-05-19 16:11', 27, 27, 3, FALSE, FALSE,
     'Identification of the Microbiomes for Cannabis Soils', 'Cannabis Soils', 'Analysis of the Cannabis Plant Microbiome',
     'This is a preliminary study to examine the microbiota associated with the Cannabis plant. Soils samples from the bulk soil, soil associated with the roots, and the rhizosphere were extracted and the DNA sequenced. Roots from three independent plants of different strains were examined. These roots were obtained November 11, 2011 from plants that had been harvested in the summer. Future studies will attempt to analyze the soils and rhizospheres from the same location at different time points in the plant lifecycle.',
     NULL);
+
+-- Add portal to the study
+INSERT INTO qiita.study_portal (study_id, portal_type_id) VALUES (1, 2);
 
 -- Add some environmental packages to the study
 INSERT INTO qiita.study_environmental_package (study_id, environmental_package_name) VALUES (1, 'soil'), (1, 'plant-associated');
