aa9745e4d332daaa73ac6e7a262a248b27ce7c9a
Joshua Shorenstein <squirrelo@gmail.com>
2015-06-17 14:29:11 -0700

    add analysis_portal table

--- qiita_db/support_files/patches/26.sql
+++ qiita_db/support_files/patches/26.sql
@@ -1,20 +1,6 @@
 -- June 11,, 2015
 -- Adds portal type to analyses
 
--- Temporarily have default value for non-portaled analyses
-ALTER TABLE qiita.analysis
-ADD portal_type_id bigint NOT NULL DEFAULT 1;
-
--- Add FK to portal_type_id
-ALTER TABLE qiita.analysis
-ADD CONSTRAINT fk_analysis_portal
-FOREIGN KEY (portal_type_id)
-REFERENCES qiita.portal_type(portal_type_id);
-
--- Drop defaut value constraint so it is now required to give portal type
-ALTER TABLE qiita.analysis
-ALTER portal_type_id DROP DEFAULT;
-
 -- Remove existing portals and replace with more relevant ones
 UPDATE qiita.study SET portal_type_id = 1 WHERE portal_type_id = 3;
 
@@ -23,6 +9,26 @@ DELETE FROM qiita.portal_type WHERE portal = 'QIIME_EMP';
 UPDATE qiita.portal_type
 SET portal = 'QIITA', portal_description = 'QIITA portal. Access to all data stored in database.'
 WHERE portal_type_id = 1;
+
+-- Add analysis portal info to it's own table, as one analysis can be in two portals
+CREATE TABLE qiita.analysis_portal (
+	analysis_id             bigint  NOT NULL,
+	portal_type_id       bigint  NOT NULL,
+	CONSTRAINT pk_analysis_portal PRIMARY KEY ( analysis_id, portal_type_id )
+ );
+
+CREATE INDEX idx_analysis_portal ON qiita.analysis_portal ( analysis_id );
+
+CREATE INDEX idx_analysis_portal_0 ON qiita.analysis_portal ( portal_type_id );
+
+ALTER TABLE qiita.analysis_portal ADD CONSTRAINT fk_analysis_portal FOREIGN KEY ( analysis_id ) REFERENCES qiita.analysis( analysis_id );
+
+ALTER TABLE qiita.analysis_portal ADD CONSTRAINT fk_analysis_portal_0 FOREIGN KEY ( portal_type_id ) REFERENCES qiita.portal_type( portal_type_id );
+
+COMMENT ON TABLE qiita.analysis_portal IS 'Controls what analyses are visible on what portals';
+
+-- Attach all existing analyses to the qiita portal
+INSERT INTO qiita.analysis_portal (analysis_id, portal_type_id) SELECT analysis_id, 1 FROM qiita.analysis;
 
 -- Remove study portal info to it's own table, as one study can be in multiple portals
 CREATE TABLE qiita.study_portal (
