81b3303fd817534d0245b73ae7199cdba03cfa48
Joshua Shorenstein <squirrelo@gmail.com>
2015-06-30 14:11:46 -0700

    add initial portal object

--- qiita_db/test/test_portal.py
+++ qiita_db/test/test_portal.py
@@ -1,38 +1,105 @@
 from unittest import TestCase, main
 
 from qiita_core.util import qiita_test_checker
-from qiita_db.portal import (
-    add_study_to_portal, remove_study_from_portal, get_studies_by_portal)
+from qiita_core.exceptions import IncompetentQiitaDeveloperError
+from qiita_db.portal import Portal
 from qiita_db.study import Study
+from qiita_db.analysis import Analysis
+from qiita_db.exceptions import QiitaDBError, QiitaDBDuplicateError
 from qiita_core.qiita_settings import qiita_config
 
-# Only test if functions available
-if qiita_config.portal == "QIITA":
-    @qiita_test_checker()
-    class TestPortal(TestCase):
-        def setUp(self):
-            self.study = Study(1)
 
-        def test_get_by_portal(self):
-            obs = get_studies_by_portal('EMP')
-            self.assertEqual(obs, set())
+@qiita_test_checker()
+class TestPortal(TestCase):
+    def setUp(self):
+        self.study = Study(1)
+        self.analysis = Analysis(1)
+        self.qiita_portal = Portal('QIITA')
+        self.emp_portal = Portal('EMP')
 
-            obs = get_studies_by_portal('QIITA')
-            self.assertEqual(obs, {1})
+    def tearDown(self):
+        qiita_config.portal = 'QIITA'
 
-        def test_add_portal(self):
-            add_study_to_portal(self.study, 'EMP')
-            obs = self.study._portals
-            self.assertEqual(obs, ['EMP', 'QIITA'])
+    def test_add_portal(self):
+        Portal.create("NEWPORTAL", "SOMEDESC")
+        obs = self.conn_handler.execute_fetchall(
+            "SELECT * FROM qiita.portal_type")
+        exp = [[1, 'QIITA', 'QIITA portal. Access to all data stored '
+                'in database.'],
+               [2, 'EMP', 'EMP portal'],
+               [4, 'NEWPORTAL', 'SOMEDESC']]
+        self.assertItemsEqual(obs, exp)
 
-        def test_remove_portal(self):
-            with self.assertRaises(ValueError):
-                remove_study_from_portal(self.study, 'QIITA')
+        with self.assertRaises(QiitaDBDuplicateError):
+            Portal.create("EMP", "DOESNTMATTERFORDESC")
 
-            add_study_to_portal(self.study, 'EMP')
-            remove_study_from_portal(self.study, 'EMP')
-            obs = self.study._portals
-            self.assertEqual(obs, ['QIITA'])
+    def test_remove_portal(self):
+        Portal.create("NEWPORTAL", "SOMEDESC")
+        Portal.delete("NEWPORTAL")
+        obs = self.conn_handler.execute_fetchall(
+            "SELECT * FROM qiita.portal_type")
+        exp = [[1, 'QIITA', 'QIITA portal. Access to all data stored '
+                'in database.'],
+               [2, 'EMP', 'EMP portal']]
+        self.assertItemsEqual(obs, exp)
+
+        with self.assertRaises(IncompetentQiitaDeveloperError):
+            Portal.delete("NOEXISTPORTAL")
+        with self.assertRaises(QiitaDBError):
+            Portal.delete("QIITA")
+
+    def test_check_studies(self):
+        with self.assertRaises(QiitaDBError):
+            self.qiita_portal._check_studies([2000000000000, 122222222222222])
+
+    def test_check_analyses(self):
+        with self.assertRaises(QiitaDBError):
+            self.qiita_portal._check_analyses([2000000000000, 122222222222222])
+
+        with self.assertRaises(QiitaDBError):
+            self.qiita_portal._check_analyses([8, 9])
+
+    def test_get_studies_by_portal(self):
+        obs = self.emp_portal.get_studies()
+        self.assertEqual(obs, set())
+
+        obs = self.qiita_portal.get_studies()
+        self.assertEqual(obs, {1})
+
+    def test_add_study_portals(self):
+        self.emp_portal.add_studies([self.study.id])
+        obs = self.study._portals
+        self.assertEqual(obs, ['EMP', 'QIITA'])
+
+    def test_remove_study_portals(self):
+        with self.assertRaises(ValueError):
+            self.qiita_portal.remove_studies([self.study.id])
+
+        self.emp_portal.add_studies([self.study.id])
+        self.emp_portal.remove_studies([self.study.id])
+        obs = self.study._portals
+        self.assertEqual(obs, ['QIITA'])
+
+    def test_get_analyses_by_portal(self):
+        obs = self.emp_portal.get_analyses()
+        self.assertEqual(obs, set())
+
+        obs = self.qiita_portal.get_analyses()
+        self.assertEqual(obs, set(x for x in range(1, 11)))
+
+    def test_add_analysis_portals(self):
+        self.emp_portal.add_analyses([self.analysis.id])
+        obs = self.analysis._portals
+        self.assertEqual(obs, ['EMP', 'QIITA'])
+
+    def test_remove_analysis_portals(self):
+        with self.assertRaises(ValueError):
+            self.qiita_portal.remove_analyses([self.analysis.id])
+
+        self.emp_portal.add_analyses([self.analysis.id])
+        self.emp_portal.remove_analyses([self.analysis.id])
+        obs = self.analysis._portals
+        self.assertEqual(obs, ['QIITA'])
 
 
 if __name__ == '__main__':
