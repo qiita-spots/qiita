e963a76fb6502ea67d63e74962e3e25e9b0faac9
Jose Navas <josenavasmolina@gmail.com>
2015-07-06 20:24:54 -0700

    Fixing reference object

--- qiita_db/reference.py
+++ qiita_db/reference.py
@@ -12,7 +12,7 @@ from .base import QiitaObject
 from .exceptions import QiitaDBDuplicateError
 from .util import (insert_filepaths, convert_to_id,
                    get_mountpoint)
-from .sql_connection import SQLConnectionHandler
+from .sql_connection import TRN
 
 
 class Reference(QiitaObject):
@@ -62,40 +62,42 @@ class Reference(QiitaObject):
             If the reference database with name `name` and version `version`
             already exists on the system
         """
-        if cls.exists(name, version):
-            raise QiitaDBDuplicateError("Reference",
-                                        "Name: %s, Version: %s"
-                                        % (name, version))
+        with TRN:
+            if cls.exists(name, version):
+                raise QiitaDBDuplicateError("Reference",
+                                            "Name: %s, Version: %s"
+                                            % (name, version))
 
-        conn_handler = SQLConnectionHandler()
+            seq_id = insert_filepaths(
+                [(seqs_fp, convert_to_id("reference_seqs", "filepath_type"))],
+                "%s_%s" % (name, version), "reference", "filepath")[0]
 
-        seq_id = insert_filepaths([(seqs_fp, convert_to_id("reference_seqs",
-                                                           "filepath_type"))],
-                                  "%s_%s" % (name, version), "reference",
-                                  "filepath")[0]
+            # Check if the database has taxonomy file
+            tax_id = None
+            if tax_fp:
+                fps = [(tax_fp,
+                        convert_to_id("reference_tax", "filepath_type"))]
+                tax_id = insert_filepaths(fps, "%s_%s" % (name, version),
+                                          "reference", "filepath")[0]
 
-        # Check if the database has taxonomy file
-        tax_id = None
-        if tax_fp:
-            fps = [(tax_fp, convert_to_id("reference_tax", "filepath_type"))]
-            tax_id = insert_filepaths(fps, "%s_%s" % (name, version),
-                                      "reference", "filepath")[0]
+            # Check if the database has tree file
+            tree_id = None
+            if tree_fp:
+                fps = [(tree_fp,
+                        convert_to_id("reference_tree", "filepath_type"))]
+                tree_id = insert_filepaths(fps, "%s_%s" % (name, version),
+                                           "reference", "filepath")[0]
 
-        # Check if the database has tree file
-        tree_id = None
-        if tree_fp:
-            fps = [(tree_fp, convert_to_id("reference_tree", "filepath_type"))]
-            tree_id = insert_filepaths(fps, "%s_%s" % (name, version),
-                                       "reference", "filepath")[0]
+            # Insert the actual object to the db
+            sql = """INSERT INTO qiita.{0}
+                        (reference_name, reference_version, sequence_filepath,
+                         taxonomy_filepath, tree_filepath)
+                     VALUES (%s, %s, %s, %s, %s)
+                     RETURNING reference_id""".format(cls._table)
+            TRN.add(sql, [name, version, seq_id, tax_id, tree_id])
+            id_ = TRN.execute_fetchlast()
 
-        # Insert the actual object to the db
-        ref_id = conn_handler.execute_fetchone(
-            "INSERT INTO qiita.{0} (reference_name, reference_version, "
-            "sequence_filepath, taxonomy_filepath, tree_filepath) VALUES "
-            "(%s, %s, %s, %s, %s) RETURNING reference_id".format(cls._table),
-            (name, version, seq_id, tax_id, tree_id))[0]
-
-        return cls(ref_id)
+            return cls(id_)
 
     @classmethod
     def exists(cls, name, version):
@@ -113,54 +115,62 @@ class Reference(QiitaObject):
         QiitaDBNotImplementedError
             If the method is not overwritten by a subclass
         """
-        conn_handler = SQLConnectionHandler()
-        return conn_handler.execute_fetchone(
-            "SELECT EXISTS(SELECT * FROM qiita.{0} WHERE "
-            "reference_name=%s AND reference_version=%s)".format(cls._table),
-            (name, version))[0]
+        with TRN:
+            sql = """SELECT EXISTS(
+                        SELECT * FROM qiita.{0}
+                        WHERE reference_name=%s
+                            AND reference_version=%s)""".format(cls._table)
+            TRN.add(sql, [name, version])
+            return TRN.execute_fetchlast()
 
     @property
     def name(self):
-        conn_handler = SQLConnectionHandler()
-        return conn_handler.execute_fetchone(
-            "SELECT reference_name FROM qiita.{0} WHERE "
-            "reference_id = %s".format(self._table), (self._id,))[0]
-        _, basefp = get_mountpoint('reference')[0]
+        with TRN:
+            sql = """SELECT reference_name FROM qiita.{0}
+                     WHERE reference_id = %s""".format(self._table)
+            TRN.add(sql, [self._id])
+            return TRN.execute_fetchlast()
 
     @property
     def version(self):
-        conn_handler = SQLConnectionHandler()
-        return conn_handler.execute_fetchone(
-            "SELECT reference_version FROM qiita.{0} WHERE "
-            "reference_id = %s".format(self._table), (self._id,))[0]
-        _, basefp = get_mountpoint('reference')[0]
+        with TRN:
+            sql = """SELECT reference_version FROM qiita.{0}
+                     WHERE reference_id = %s""".format(self._table)
+            TRN.add(sql, [self._id])
+            return TRN.execute_fetchlast()
 
     @property
     def sequence_fp(self):
-        conn_handler = SQLConnectionHandler()
-        rel_path = conn_handler.execute_fetchone(
-            "SELECT f.filepath FROM qiita.filepath f JOIN qiita.{0} r ON "
-            "r.sequence_filepath=f.filepath_id WHERE "
-            "r.reference_id=%s".format(self._table), (self._id,))[0]
-        _, basefp = get_mountpoint('reference')[0]
-        return join(basefp, rel_path)
+        with TRN:
+            sql = """SELECT f.filepath
+                     FROM qiita.filepath f
+                        JOIN qiita.{0} r ON r.sequence_filepath=f.filepath_id
+                     WHERE r.reference_id=%s""".format(self._table)
+            TRN.add(sql, [self._id])
+            rel_path = TRN.execute_fetchlast()
+            _, basefp = get_mountpoint('reference')[0]
+            return join(basefp, rel_path)
 
     @property
     def taxonomy_fp(self):
-        conn_handler = SQLConnectionHandler()
-        rel_path = conn_handler.execute_fetchone(
-            "SELECT f.filepath FROM qiita.filepath f JOIN qiita.{0} r ON "
-            "r.taxonomy_filepath=f.filepath_id WHERE "
-            "r.reference_id=%s".format(self._table), (self._id,))[0]
-        _, basefp = get_mountpoint('reference')[0]
-        return join(basefp, rel_path)
+        with TRN:
+            sql = """SELECT f.filepath
+                     FROM qiita.filepath f
+                        JOIN qiita.{0} r ON r.taxonomy_filepath=f.filepath_id
+                     WHERE r.reference_id=%s""".format(self._table)
+            TRN.add(sql, [self._id])
+            rel_path = TRN.execute_fetchlast()
+            _, basefp = get_mountpoint('reference')[0]
+            return join(basefp, rel_path)
 
     @property
     def tree_fp(self):
-        conn_handler = SQLConnectionHandler()
-        rel_path = conn_handler.execute_fetchone(
-            "SELECT f.filepath FROM qiita.filepath f JOIN qiita.{0} r ON "
-            "r.tree_filepath=f.filepath_id WHERE "
-            "r.reference_id=%s".format(self._table), (self._id,))[0]
-        _, basefp = get_mountpoint('reference')[0]
-        return join(basefp, rel_path)
+        with TRN:
+            sql = """SELECT f.filepath
+                     FROM qiita.filepath f
+                        JOIN qiita.{0} r ON r.tree_filepath=f.filepath_id
+                     WHERE r.reference_id=%s""".format(self._table)
+            TRN.add(sql, [self._id])
+            rel_path = TRN.execute_fetchlast()
+            _, basefp = get_mountpoint('reference')[0]
+            return join(basefp, rel_path)
