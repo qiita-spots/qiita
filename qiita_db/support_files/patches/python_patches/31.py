35d5231e00db1955ad3d2e3be33da39d5900ad59
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-08-25 10:41:12 -0700

    BUG: Fix patch

--- qiita_db/support_files/patches/python_patches/31.py
+++ qiita_db/support_files/patches/python_patches/31.py
@@ -1,6 +1,11 @@
 from os.path import realpath
-from qiita_db.sql_connection import SQLConnectionHandler
+from qiita_db.sql_connection import TRN
 
-conn = SQLConnectionHandler()
-path = conn.execute_fetchone('SELECT base_data_dir FROM settings')[0]
-conn.execute("UPDATE settings SET base_data_dir = %s", (realpath(path),))
+with TRN:
+    TRN.add('SELECT base_data_dir FROM settings')
+    path = TRN.execute_fetchlast()
+
+    # if the path is non-canonical (it contains .. or other redundant symbols)
+    # this will update it, else it will leave as is
+    TRN.add("UPDATE settings SET base_data_dir = %s", (realpath(path),))
+    TRN.execute()
