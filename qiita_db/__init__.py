cfda81f71ebabaafd0d180b1225cec7af9b711cb
Jose Navas <josenavasmolina@gmail.com>
2015-11-09 11:01:33 -0800

    improving __init__.py

--- qiita_db/__init__.py
+++ qiita_db/__init__.py
@@ -7,3 +7,30 @@
 # -----------------------------------------------------------------------------
 
 __version__ = "0.2.0-dev"
+
+import base
+import util
+import sql_connection
+import metadata_template
+import analysis
+import artifact
+import commands
+import environment_manager
+import exceptions
+import investigation
+import job
+import logger
+import meta_util
+import ontology
+import parameters
+import reference
+import search
+import software
+import study
+import user
+
+
+__all__ = ["analysis", "artifact", "base", "commands", "environment_manager",
+           "exceptions", "investigation", "job", "logger", "meta_util",
+           "ontology", "parameters", "reference", "search", "software",
+           "sql_connection", "study", "user", "util", "metadata_template"]
