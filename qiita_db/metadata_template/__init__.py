4a4252a610e0c229232e179ade7794b1e9ae05f6
Jose Navas <josenavasmolina@gmail.com>
2015-11-16 19:17:21 -0800

    Addressing comments

--- qiita_db/metadata_template/__init__.py
+++ qiita_db/metadata_template/__init__.py
@@ -8,21 +8,7 @@
 
 import constants
 import util
-# import base_metadata_template
 import sample_template
 import prep_template
 
 __all__ = ["sample_template", "prep_template", "util", "constants"]
-
-# from .sample_template import SampleTemplate
-# from .prep_template import PrepTemplate
-# from .util import load_template_to_dataframe, looks_like_qiime_mapping_file
-# from .constants import (TARGET_GENE_DATA_TYPES, SAMPLE_TEMPLATE_COLUMNS,
-#                         PREP_TEMPLATE_COLUMNS,
-#                         PREP_TEMPLATE_COLUMNS_TARGET_GENE, CONTROLLED_COLS)
-#
-#
-# __all__ = ['SampleTemplate', 'PrepTemplate', 'load_template_to_dataframe',
-#            'TARGET_GENE_DATA_TYPES', 'SAMPLE_TEMPLATE_COLUMNS',
-#            'PREP_TEMPLATE_COLUMNS', 'PREP_TEMPLATE_COLUMNS_TARGET_GENE',
-#            'CONTROLLED_COLS', 'looks_like_qiime_mapping_file']
