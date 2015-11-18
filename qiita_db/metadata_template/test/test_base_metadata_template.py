ea451e3138d1a18154891e6286448ca43c3d6a1d
Jose Navas <josenavasmolina@gmail.com>
2015-11-18 11:28:54 -0800

    Fixing base template tests

--- qiita_db/metadata_template/test/test_base_metadata_template.py
+++ qiita_db/metadata_template/test/test_base_metadata_template.py
@@ -9,10 +9,7 @@
 from unittest import TestCase, main
 
 from qiita_core.exceptions import IncompetentQiitaDeveloperError
-from qiita_db.study import Study
-from qiita_db.metadata_template.base_metadata_template import (
-    MetadataTemplate, BaseSample)
-from qiita_db.metadata_template.sample_template import SampleTemplate
+import qiita_db as qdb
 
 
 class TestBaseSample(TestCase):
@@ -21,45 +18,54 @@ class TestBaseSample(TestCase):
     def test_init(self):
         """BaseSample init should raise an error (it's a base class)"""
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            BaseSample('SKM7.640188', SampleTemplate(1))
+            qdb.metadata_template.base_metadata_template.BaseSample(
+                'SKM7.640188',
+                qdb.metadata_template.sample_template.SampleTemplate(1))
 
     def test_exists(self):
         """exists should raise an error if called from the base class"""
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            BaseSample.exists('SKM7.640188', SampleTemplate(1))
+            qdb.metadata_template.base_metadata_template.BaseSample.exists(
+                'SKM7.640188',
+                qdb.metadata_template.sample_template.SampleTemplate(1))
 
 
 class TestMetadataTemplateReadOnly(TestCase):
     """Tests the MetadataTemplate base class"""
     def setUp(self):
-        self.study = Study(1)
+        self.study = qdb.study.Study(1)
 
     def test_init(self):
         """Init raises an error because it's not called from a subclass"""
+        MT = qdb.metadata_template.base_metadata_template.MetadataTemplate
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            MetadataTemplate(1)
+            MT(1)
 
     def test_exist(self):
         """Exists raises an error because it's not called from a subclass"""
+        MT = qdb.metadata_template.base_metadata_template.MetadataTemplate
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            MetadataTemplate.exists(self.study)
+            MT.exists(self.study)
 
     def test_table_name(self):
         """table name raises an error because it's not called from a subclass
         """
+        MT = qdb.metadata_template.base_metadata_template.MetadataTemplate
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            MetadataTemplate._table_name(self.study)
+            MT._table_name(self.study)
 
     def test_common_creation_steps(self):
         """common_creation_steps raises an error from base class
         """
+        MT = qdb.metadata_template.base_metadata_template.MetadataTemplate
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            MetadataTemplate._common_creation_steps(None, 1)
+            MT._common_creation_steps(None, 1)
 
     def test_clean_validate_template(self):
         """_clean_validate_template raises an error from base class"""
+        MT = qdb.metadata_template.base_metadata_template.MetadataTemplate
         with self.assertRaises(IncompetentQiitaDeveloperError):
-            MetadataTemplate._clean_validate_template(None, 1, None)
+            MT._clean_validate_template(None, 1, None)
 
 
 if __name__ == '__main__':
