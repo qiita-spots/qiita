2779fa024a74f81c1512c4121e5cf1b3e372956c
Jose Navas <josenavasmolina@gmail.com>
2015-11-25 12:32:10 -0800

    Fix populate

--- qiita_db/support_files/populate_test_db.sql
+++ qiita_db/support_files/populate_test_db.sql
@@ -331,6 +331,18 @@ INSERT INTO qiita.parent_artifact (parent_id, artifact_id)
     VALUES (1, 2), (1, 3),
            (2, 4);
 
+-- Insert the jobs that processed the previous artifacts
+INSERT INTO qiita.processing_job (processing_job_id, email, command_id, command_parameters, processing_job_status_id)
+    VALUES ('6d368e16-2242-4cf8-87b4-a5dc40bb890b', 'test@foo.bar', 1, '{"max_bad_run_length":3,"min_per_read_length_fraction":0.75,"sequence_max_n":0,"rev_comp_barcode":false,"rev_comp_mapping_barcodes":false,"rev_comp":false,"phred_quality_threshold":3,"barcode_type":"golay_12","max_barcode_errors":1.5,"input_data":1}'::json, 3),
+           ('4c7115e8-4c8e-424c-bf25-96c292ca1931', 'test@foo.bar', 1, '{"max_bad_run_length":3,"min_per_read_length_fraction":0.75,"sequence_max_n":0,"rev_comp_barcode":false,"rev_comp_mapping_barcodes":true,"rev_comp":false,"phred_quality_threshold":3,"barcode_type":"golay_12","max_barcode_errors":1.5,"input_data":1}'::json, 3),
+           ('3c9991ab-6c14-4368-a48c-841e8837a79c', 'test@foo.bar', 3, '{"reference":1,"sortmerna_e_value":1,"sortmerna_max_pos":10000,"similarity":0.97,"sortmerna_coverage":0.97,"threads":1,"input_data":2}'::json, 3);
+
+-- Relate the above jobs with the artifacts
+INSERT INTO qiita.artifact_processing_job (artifact_id, processing_job_id)
+    VALUES (1, '6d368e16-2242-4cf8-87b4-a5dc40bb890b'),
+           (1, '4c7115e8-4c8e-424c-bf25-96c292ca1931'),
+           (2, '3c9991ab-6c14-4368-a48c-841e8837a79c');
+
 -- Insert filepaths for the artifacts and reference
 INSERT INTO qiita.filepath (filepath, filepath_type_id, checksum, checksum_algorithm_id, data_directory_id)
     VALUES ('1_s_G1_L001_sequences.fastq.gz', 1, '852952723', 1, 5),
@@ -486,6 +498,12 @@ INSERT INTO qiita.processing_job
            ('bcc7ebcd-39c1-43e4-af2d-822e3589f14d', 'test@foo.bar', 2, '{"min_seq_len":100,"max_seq_len":1000,"trim_seq_length":false,"min_qual_score":25,"max_ambig":6,"max_homopolymer":6,"max_primer_mismatch":0,"barcode_type":"golay_12","max_barcode_errors":1.5,"disable_bc_correction":false,"qual_score_window":0,"disable_primers":false,"reverse_primers":"disable","reverse_primer_mismatches":0,"truncate_ambi_bases":false,"input_data":1}'::json, 2, NULL, 'Sun Nov 22 21:00:00 2015', 'demultiplexing'),
            ('b72369f9-a886-4193-8d3d-f7b504168e75', 'shared@foo.bar', 1, '{"max_bad_run_length":3,"min_per_read_length_fraction":0.75,"sequence_max_n":0,"rev_comp_barcode":false,"rev_comp_mapping_barcodes":true,"rev_comp":false,"phred_quality_threshold":3,"barcode_type":"golay_12","max_barcode_errors":1.5,"input_data":1}'::json, 3, NULL, 'Sun Nov 22 21:15:00 2015', NULL),
            ('d19f76ee-274e-4c1b-b3a2-a12d73507c55', 'shared@foo.bar', 3, '{"reference":1,"sortmerna_e_value":1,"sortmerna_max_pos":10000,"similarity":0.97,"sortmerna_coverage":0.97,"threads":1,"input_data":2}'::json, 4, 1, 'Sun Nov 22 21:30:00 2015', 'generating demux file');
+
+INSERT INTO qiita.artifact_processing_job (artifact_id, processing_job_id)
+    VALUES (1, '063e553b-327c-4818-ab4a-adfe58e49860'),
+           (1, 'bcc7ebcd-39c1-43e4-af2d-822e3589f14d'),
+           (1, 'b72369f9-a886-4193-8d3d-f7b504168e75'),
+           (2, 'd19f76ee-274e-4c1b-b3a2-a12d73507c55');
 
 -- Add a default parameter set for sortemrna
 INSERT INTO qiita.default_parameter_set (command_id, parameter_set_name, parameter_set)
