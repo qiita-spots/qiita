af2fc7e78d52ab99106fac96ebac65355358b810
Jose Navas <josenavasmolina@gmail.com>
2016-02-15 20:05:52 -0800

    fixing flake8 (I think - can't reproduce on my end

--- qiita_pet/handlers/api_proxy/__init__.py
+++ qiita_pet/handlers/api_proxy/__init__.py
@@ -10,7 +10,6 @@
 # The idea is that this proxies the call and response dicts we expect from the
 # Qiita API once we build it. This will be removed and replaced with API calls
 # when the API is complete.
-__version__ = "0.2.0-dev"
 from .sample_template import (
     sample_template_post_req, sample_template_put_req,
     sample_template_summary_get_req, sample_template_delete_req,
@@ -24,10 +23,11 @@ from .prep_template import (
     ena_ontology_get_req, prep_template_samples_get_req)
 from .studies import (
     data_types_get_req, study_get_req, study_prep_get_req, study_delete_req)
-
 from .artifact import (artifact_graph_get_req, artifact_types_get_req,
                        artifact_post_req, artifact_get_req,
                        artifact_status_put_req, artifact_delete_req)
+
+__version__ = "0.2.0-dev"
 
 __all__ = ['prep_template_summary_get_req', 'sample_template_post_req',
            'sample_template_put_req', 'data_types_get_req',
