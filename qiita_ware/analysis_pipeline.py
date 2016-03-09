7e6db47f4c10d4be7c4d2cab19728972ecd7e373
Antonio Gonzalez <antgonza@gmail.com>
2016-03-09 11:56:48 -0700

    addressing @squirrelo comments

--- qiita_ware/analysis_pipeline.py
+++ qiita_ware/analysis_pipeline.py
@@ -4,6 +4,8 @@ from os.path import join
 from sys import stderr
 
 from qiita_db.job import Job
+from qiita_db.reference import Reference
+from qiita_db.software import Command
 from qiita_db.logger import LogEntry
 from qiita_db.util import get_db_files_base_dir
 from qiita_ware.wrapper import ParallelWrapper, system_call_from_job
@@ -136,8 +138,8 @@ class RunAnalysis(ParallelWrapper):
             # These values are going to be changed in the next PR
             # 1: GG
             # 3: pick close reference
-            Job.create(data_type, command, opts, analysis, 1, 3,
-                       return_existing=True)
+            Job.create(data_type, command, opts, analysis, Reference(1),
+                       Command(3), return_existing=True)
 
         # Create the files for the jobs
         files_node_name = "%d_ANALYSISFILES" % analysis.id
