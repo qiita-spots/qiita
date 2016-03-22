809d2859ab7466f1ef477e3a646e32aa5915f89d
Antonio Gonzalez <antgonza@gmail.com>
2016-03-22 08:54:53 -0600

    fix None errors 2

--- qiita_plugins/target_gene/tgp/split_libraries/tests/test_util.py
+++ qiita_plugins/target_gene/tgp/split_libraries/tests/test_util.py
@@ -23,17 +23,19 @@ from tgp.split_libraries.util import (
 
 
 class UtilTests(TestCase):
-    @httpretty.activate
     def setUp(self):
-        httpretty.enable()
-        httpretty.register_uri(
-            httpretty.POST,
-            "https://test_server.com/qiita_db/authenticate/",
-            body='{"access_token": "token", "token_type": "Bearer", '
-                 '"expires_in": "3600"}')
+        @httpretty.activate
+        def init_httpretty(self):
+            httpretty.register_uri(
+                httpretty.POST,
+                "https://test_server.com/qiita_db/authenticate/",
+                body='{"access_token": "token", "token_type": "Bearer", '
+                     '"expires_in": "3600"}')
 
-        self.qclient = QiitaClient("https://test_server.com", 'client_id',
-                                   'client_secret')
+            self.qclient = QiitaClient("https://test_server.com", 'client_id',
+                                       'client_secret')
+
+        init_httpretty(self)
         self._clean_up_files = []
 
     def tearDown(self):
