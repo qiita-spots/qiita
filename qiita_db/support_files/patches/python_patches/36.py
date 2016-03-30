249e4023dc40ead00a20a94e8eb6b88ab05d9529
Antonio Gonzalez <antgonza@gmail.com>
2016-03-30 08:08:55 -0600

    initial commit

--- qiita_db/support_files/patches/python_patches/36.py
+++ qiita_db/support_files/patches/python_patches/36.py
@@ -1,4 +1,5 @@
-
+from random import SystemRandom
+from string import ascii_letters, digits
 from subprocess import Popen, PIPE
 from os.path import exists, join
 
@@ -8,10 +9,26 @@ from qiita_db.util import (insert_filepaths, convert_to_id, get_mountpoint,
                            get_mountpoint_path_by_id)
 
 
+pool = ascii_letters + digits
 tgz_id = convert_to_id("tgz", "filepath_type")
-_, analysis_mp = get_mountpoint('analysis')[0]
-
+_id, analysis_mp = get_mountpoint('analysis')[0]
 with TRN:
+    # 2 and 3 are the ids of the 2 new software rows, the BIOM and
+    # target gene type plugins
+    for i in [2, 3]:
+        client_id = ''.join([SystemRandom().choice(pool) for _ in range(50)])
+        client_secret = ''.join(
+            [SystemRandom().choice(pool) for _ in range(255)])
+
+        sql = """INSERT INTO qiita.oauth_identifiers (client_id, client_secret)
+                VALUES (%s, %s)"""
+        TRN.add(sql, [client_id, client_secret])
+
+        sql = """INSERT INTO qiita.oauth_software (software_id, client_id)
+                 VALUES (%s, %s)"""
+        TRN.add(sql, [i, client_id])
+        TRN.execute()
+
     #
     # Generating compressed files for picking failures -- artifact_type = BIOM
     #
