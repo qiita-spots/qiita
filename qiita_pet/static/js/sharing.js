e3695202d34f3d63d219e0c2554e729f5b178cb8
Joshua Shorenstein <squirrelo@gmail.com>
2016-04-08 13:01:13 -0700

    address comments

--- qiita_pet/static/js/sharing.js
+++ qiita_pet/static/js/sharing.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
     },
     minimumInputLength: 1,
     formatResult: function (data, term) {
-      console.log(data);
       return data;
     }
   });
@@ -32,11 +31,11 @@ var shared_list;
 current_study = study_id;
 $.get('/study/sharing/', {study_id: study_id})
     .done(function(data) {
-      users_links = JSON.parse(data);
-      users = users_links.users;
+      var users_links = JSON.parse(data);
+      var users = users_links.users;
       //empty dropdown and repopulate with new study shared values
       $('#shares-select').html('');
-      for(i=0;i<users.length;i++) {
+      for(var i=0;i<users.length;i++) {
         var shared = new Option(users[i], users[i], true, true);
         $("#shares-select").append(shared).trigger('change');
       }
@@ -51,6 +50,6 @@ function update_share(params) {
     .done(function(data) {
       users_links = JSON.parse(data);
       links = users_links['links'];
-      document.getElementById("shared_html_"+current_study).innerHTML = links;
+      $("#shared_html_"+current_study).innerHTML = links;
     });
 }
