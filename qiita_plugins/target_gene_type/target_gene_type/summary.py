2a60bab1535410996f7b165c8ac69e6021110cfe
Jose Navas <josenavasmolina@gmail.com>
2016-05-25 08:16:12 -0700

    Fixing target gene type plugin

--- qiita_plugins/target_gene_type/target_gene_type/summary.py
+++ qiita_plugins/target_gene_type/target_gene_type/summary.py
@@ -50,24 +50,24 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
         - If the artifact is 'Demultiplexed' but it doesn't have a demux file
     """
     # Step 1: gather file information from qiita using REST api
-    # 1a. getting the file paths
     artifact_id = parameters['input_data']
-    qclient_url = "/qiita_db/artifacts/%s/filepaths/" % artifact_id
-    fps_info = qclient.get(qclient_url)
+    qclient_url = "/qiita_db/artifacts/%s/" % artifact_id
+    a_info = qclient.get(qclient_url)
+    # 1a. getting the file paths
+    fps_info = a_info['files']
     # 1.b get the artifact type_info
-    type_info = qclient.get("/qiita_db/artifacts/%s/type/" % artifact_id)
-    artifact_type = type_info['type']
+    artifact_type = a_info['type']
 
     # we have 2 main cases: Demultiplexed and everything else, splitting on
     # those
     if artifact_type == 'Demultiplexed':
         artifact_information = _summary_demultiplexed(
-            artifact_type, fps_info['filepaths'])
+            artifact_type, fps_info)
         if artifact_information is None:
             raise ValueError("We couldn't find a demux file in your artifact")
     else:
         artifact_information = _summary_not_demultiplexed(
-            artifact_type, fps_info['filepaths'])
+            artifact_type, fps_info)
 
     of_fp = join(out_dir, "artifact_%d.html" % artifact_id)
     with open(of_fp, 'w') as of:
@@ -103,35 +103,37 @@ def _summary_not_demultiplexed(artifact_type, filepaths):
     """
     # loop over each of the fps/fps_type pairs
     artifact_information = []
-    for (fps, fps_type) in filepaths:
-        # Step 2: generate HTML summary
-        # md5, from http://stackoverflow.com/a/3431838
-        with open(fps, "rb") as f:
-            hash_md5 = md5()
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
+    for (fps_type, fps) in sorted(filepaths.items()):
+        for fp in fps:
+            # Step 2: generate HTML summary
+            # md5, from http://stackoverflow.com/a/3431838
+            with open(fp, "rb") as f:
+                hash_md5 = md5()
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
 
-        # getting head of the files
-        header = []
-        if artifact_type not in FILEPATH_TYPE_TO_NOT_SHOW_HEAD:
-            # we need to encapsulate the full for loop because gzip will not
-            # raise an error until you try to read
-            try:
-                with gopen(fps, 'r') as fin:
-                    header = [
-                        next(fin) for x in xrange(LINES_TO_READ_FOR_HEAD)]
-            except IOError:
-                with open(fps, 'r') as fin:
-                    header = [
-                        next(fin) for x in xrange(LINES_TO_READ_FOR_HEAD)]
-        filename = basename(fps)
-        artifact_information.append("<h3>%s (%s)</h3>" % (filename, fps_type))
-        artifact_information.append("<b>MD5:</b>: %s</br>" %
-                                    hash_md5.hexdigest())
-        if header:
-            artifact_information.append(
-                "<p style=\"font-family:'Courier New', Courier, monospace;"
-                "font-size:10;\">%s</p><hr/>" % ("<br/>".join(header)))
+            # getting head of the files
+            header = []
+            if artifact_type not in FILEPATH_TYPE_TO_NOT_SHOW_HEAD:
+                # we need to encapsulate the full for loop because gzip will
+                # not raise an error until you try to read
+                try:
+                    with gopen(fp, 'r') as fin:
+                        header = [
+                            next(fin) for x in xrange(LINES_TO_READ_FOR_HEAD)]
+                except IOError:
+                    with open(fp, 'r') as fin:
+                        header = [
+                            next(fin) for x in xrange(LINES_TO_READ_FOR_HEAD)]
+            filename = basename(fp)
+            artifact_information.append("<h3>%s (%s)</h3>"
+                                        % (filename, fps_type))
+            artifact_information.append("<b>MD5:</b>: %s</br>" %
+                                        hash_md5.hexdigest())
+            if header:
+                artifact_information.append(
+                    "<p style=\"font-family:'Courier New', Courier, monospace;"
+                    "font-size:10;\">%s</p><hr/>" % ("<br/>".join(header)))
 
     return artifact_information
 
@@ -153,13 +155,12 @@ def _summary_demultiplexed(artifact_type, filepaths):
         A list of strings with the html summary
     """
     # loop over each of the fps/fps_type pairs to find the demux_fp
-    demux_fp = None
-    for (fps, fps_type) in filepaths:
-        if fps_type == 'preprocessed_demux':
-            demux_fp = fps
-            break
+    demux_fp = filepaths.get('preprocessed_demux')
     if demux_fp is None:
         return None
+
+    # At this point demux_fp is a list, but we know it only has 1 element
+    demux_fp = demux_fp[0]
 
     # generating html summary
     artifact_information = []
