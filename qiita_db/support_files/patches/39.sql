45797732dedf919f378956f98d413b4da8fbc838
Jose Navas <josenavasmolina@gmail.com>
2016-07-26 07:48:44 -0700

    Modifying DB and adding patch

---
+++ qiita_db/support_files/patches/39.sql
@@ -0,0 +1,7 @@
+-- Jul 26, 2016
+-- Adding active column to the software and software command table to be able
+-- to disallow plugins and/or individual software commands
+
+ALTER TABLE qiita.software ADD active bool DEFAULT 'True' NOT NULL;
+
+ALTER TABLE qiita.software_command ADD active bool DEFAULT 'True' NOT NULL;
