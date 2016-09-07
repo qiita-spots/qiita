53472e545df4c052b45ccaba00fafc9fec22878e
Antonio Gonzalez <antgonza@gmail.com>
2016-09-07 07:44:29 -0600

    initial commit

---
+++ qiita_db/support_files/patches/39.sql
@@ -0,0 +1,15 @@
+-- Sep 7, 2016
+-- Adding a is numeric function to avoid problems with texts
+-- http://stackoverflow.com/a/16206123
+
+CREATE OR REPLACE FUNCTION isnumeric(text) RETURNS BOOLEAN AS $$
+DECLARE x NUMERIC;
+BEGIN
+    x = $1::NUMERIC;
+    RETURN TRUE;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE;
