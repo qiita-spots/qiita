07ee472c052d005dc434154ddfee1dac59eda1b1
Jose Navas <josenavasmolina@gmail.com>
2016-09-26 10:55:15 -0700

    almost finishing all the handlers

--- qiita_db/handlers/plugin.py
+++ qiita_db/handlers/plugin.py
@@ -47,6 +47,28 @@ def _get_plugin(name, version):
 class PluginHandler(OauthBaseHandler):
     @authenticate_oauth
     def get(self, name, version):
+        """Retrieve the plugin information
+
+        Parameters
+        ----------
+        name : str
+            The plugin name
+        version : str
+            The plugin version
+
+        Returns
+        -------
+        dict
+            The plugin information:
+            'name': the plugin name
+            'version': the plugin version
+            'description': the plugin description
+            'commands': list of the plugin commands
+            'publications': list of publications
+            'default_workflows': list of the plugin default workflows
+            'type': the plugin type
+            'active': whether the plugin is active or not
+        """
         with qdb.sql_connection.TRN:
             plugin = _get_plugin(name, version)
             response = {
@@ -63,8 +85,91 @@ class PluginHandler(OauthBaseHandler):
         self.write(response)
 
 
+class CommandListHandler(OauthBaseHandler):
+    @authenticate_oauth
+    def post(self, name, version):
+        with qdb.sql_connection.TRN:
+            cmd_name = self.get_argument('name')
+            cmd_desc = self.get_argument('description')
+            req_params = self.get_argument('required_parameters')
+            opt_params = self.get_argument('optional_parameters')
+            dfl_param_set = self.get_argument('default_parameter_sets')
+
+            # params = opt_params
+            # for pname, vals in
+            print cmd_name, cmd_desc, req_params, opt_params
+        self.finish()
+
+
+def _get_command(plugin_name, plugin_version, cmd_name):
+    """Returns the command with the given name within the given plugin
+
+    Parameters
+    ----------
+    plugin_name : str
+        The name of the plugin
+    plugin_version : str
+        The version of the plugin
+    cmd_name : str
+        The name of the command in the plugin
+
+    Returns
+    -------
+    qiita_db.software.Command
+        The requested command
+
+    Raises
+    ------
+    HTTPError
+        If the command does not exist, with error code 404
+        If there is a problem instantiating the command, with error code 500
+    """
+    plugin = _get_plugin(plugin_name, plugin_version)
+    try:
+        cmd = plugin.get_command(cmd_name)
+    except qdb.exceptions.QiitaDBUnknownIDError:
+        raise HTTPError(404)
+    except Exception as e:
+        raise HTTPError(500, 'Error instantiating cmd %s of plugin %s %s: %s'
+                        % (cmd_name, plugin_name, plugin_version, str(e)))
+
+    return cmd
+
+
 class CommandHandler(OauthBaseHandler):
     @authenticate_oauth
-    def post(self):
-        """Creates a new command in the system"""
-        pass
+    def get(self, plugin_name, plugin_version, cmd_name):
+        """Retrieve the command information
+
+        Parameters
+        ----------
+        plugin_name : str
+            The plugin name
+        plugin_version : str
+            The plugin version
+        cmd_name : str
+            The command name
+
+        Returns
+        -------
+        dict
+            The command information
+            'name': the command name
+            'description': the command description
+            'required_parameters': dict with the required parameters, in the
+                format {parameter_name: [type, [subtypes]]}
+            'optional_parameters': dict with the optional parameters, in the
+                format {parameter_name: [type, default value]}
+            'default_parameter_sets': dict with the default parameter sets, in
+                the format {parameter set name: {parameter_name: value}}
+        """
+        with qdb.sql_connection.TRN:
+            cmd = _get_command(plugin_name, plugin_version, cmd_name)
+            response = {
+                'name': cmd.name,
+                'description': cmd.description,
+                'required_parameters': cmd.required_parameters,
+                'optional_parameters': cmd.optional_parameters,
+                'default_parameter_sets': {
+                    p.name: p.values for p in cmd.default_parameter_sets}}
+        self.write(response)
