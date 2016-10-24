c5edaea8ebf28217baf2f27d53a4fc112dcf5dac
Jose Navas <josenavasmolina@gmail.com>
2016-10-24 09:39:05 -0700

    Adding patch to the PR...

---
+++ qiita_db/support_files/patches/41.sql
@@ -0,0 +1,15 @@
+-- Oct 23, 2016
+-- Adding provenance parameter to validate commands
+
+DO $do$
+DECLARE
+    cmd  RECORD;
+BEGIN
+    FOR cmd IN
+        SELECT command_id FROM qiita.software_command WHERE name = 'Validate'
+    LOOP
+        INSERT INTO qiita.command_parameter (command_id, parameter_name, parameter_type, required, default_value)
+            VALUES (cmd.command_id, 'provenance', 'string', 'False', NULL);
+
+    END LOOP;
+END $do$
