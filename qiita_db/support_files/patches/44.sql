c0792fa3e2e79324fea7ebc078128cbc5e0918b7
Jose Navas <josenavasmolina@gmail.com>
2016-12-14 13:56:29 -0800

    Fixing patch

--- qiita_db/support_files/patches/44.sql
+++ qiita_db/support_files/patches/44.sql
@@ -8,5 +8,5 @@ CREATE TABLE qiita.processing_job_validator (
  ) ;
 CREATE INDEX idx_processing_job_validator_0 ON qiita.processing_job_validator ( processing_job_id ) ;
 CREATE INDEX idx_processing_job_validator_1 ON qiita.processing_job_validator ( validator_id ) ;
-ALTER TABLE qiita.processing_job_validator_p ADD CONSTRAINT fk_processing_job_validator FOREIGN KEY ( processing_job_id ) REFERENCES processing_job( processing_job_id )    ;
-ALTER TABLE qiita.processing_job_validator_c ADD CONSTRAINT fk_processing_job_validator_0 FOREIGN KEY ( validator_id ) REFERENCES processing_job( processing_job_id )    ;
+ALTER TABLE qiita.processing_job_validator ADD CONSTRAINT fk_processing_job_validator_p FOREIGN KEY ( processing_job_id ) REFERENCES qiita.processing_job( processing_job_id )    ;
+ALTER TABLE qiita.processing_job_validator ADD CONSTRAINT fk_processing_job_validator_c FOREIGN KEY ( validator_id ) REFERENCES qiita.processing_job( processing_job_id )    ;
