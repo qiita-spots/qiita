467a599147adcdb9255fa1ae7f1a29ea9f2678a7
Jose Navas <josenavasmolina@gmail.com>
2016-12-14 14:24:48 -0800

    Fixing patch

--- qiita_db/support_files/patches/44.sql
+++ qiita_db/support_files/patches/44.sql
@@ -4,6 +4,7 @@
 CREATE TABLE qiita.processing_job_validator (
 	processing_job_id    UUID  NOT NULL,
 	validator_id         UUID  NOT NULL,
+	artifact_info        json,
 	CONSTRAINT idx_processing_job_validator PRIMARY KEY ( processing_job_id, validator_id )
  ) ;
 CREATE INDEX idx_processing_job_validator_0 ON qiita.processing_job_validator ( processing_job_id ) ;
