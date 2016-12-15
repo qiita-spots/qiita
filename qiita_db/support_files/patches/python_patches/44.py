fb926427ca911dc15bbbdb7486d28ea4b075ad10
Antonio Gonzalez <antgonza@gmail.com>
2016-12-15 15:33:30 -0700

    addresssing @josenavas comments

--- qiita_db/support_files/patches/python_patches/44.py
+++ qiita_db/support_files/patches/python_patches/44.py
@@ -8,27 +8,25 @@ with qdb.sql_connection.TRN:
                LEFT JOIN qiita.publication AS p ON (sp.publication = p.doi)
                WHERE p.doi NOT IN (
                  SELECT publication_doi FROM qiita.software_publication)"""
-    qdb.sql_connection.TRN.add(sql, [])
+    qdb.sql_connection.TRN.add(sql)
 
     pubs = qdb.sql_connection.TRN.execute_fetchindex()
 
-# deleting all references to start from scratch
-with qdb.sql_connection.TRN:
+    # deleting all references to start from scratch
     sql = """DELETE FROM qiita.study_publication"""
-    qdb.sql_connection.TRN.add(sql, [])
+    qdb.sql_connection.TRN.add(sql)
     qdb.sql_connection.TRN.execute()
 
-# reinserting following the new structure
-for doi, pid, sid in pubs:
-    to_insert = []
-    if doi is not None:
-        to_insert.append([doi, True, sid])
-    if pid not in to_insert:
-        to_insert.append([pid, False, sid])
+    # reinserting following the new structure
+    for doi, pid, sid in pubs:
+        to_insert = []
+        if doi is not None:
+            to_insert.append([doi, True, sid])
+        if pid not in to_insert:
+            to_insert.append([pid, False, sid])
 
-    with qdb.sql_connection.TRN:
-        sql = """INSERT INTO qiita.study_publication
-                   (publication, is_doi, study_id)
-                 VALUES (%s, %s, %s)"""
-        qdb.sql_connection.TRN.add(sql, to_insert, many=True)
-        qdb.sql_connection.TRN.execute()
+            sql = """INSERT INTO qiita.study_publication
+                       (publication, is_doi, study_id)
+                     VALUES (%s, %s, %s)"""
+            qdb.sql_connection.TRN.add(sql, to_insert, many=True)
+    qdb.sql_connection.TRN.execute()
