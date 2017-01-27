19889f9aeddf65d5b79927a307840320205c2792
Antonio Gonzalez <antgonza@gmail.com>
2017-01-28 00:15:23 +0400

    Automatic jobs & new stats (#2057)
    * fix #814, fix #1636
    
    * fixing error in test-env
    
    * fixing stats.html call
    
    * adding img
    
    * addressing @josenavas comments
    
    * rm for loops
    
    * addresssing @ElDeveloper comments

--- qiita_db/test/test_meta_util.py
+++ qiita_db/test/test_meta_util.py
@@ -10,6 +10,7 @@ from unittest import TestCase, main
 
 import pandas as pd
 
+from moi import r_client
 from qiita_core.qiita_settings import qiita_config
 from qiita_core.util import qiita_test_checker
 
@@ -180,6 +181,43 @@ class MetaUtilTests(TestCase):
 
         self.assertItemsEqual(obs, exp)
 
+    def test_update_redis_stats(self):
+        qdb.meta_util.update_redis_stats()
+
+        portal = qiita_config.portal
+        vals = [
+            ('number_studies', {'sanbox': '2', 'public': '0',
+                                'private': '1'}, r_client.hgetall),
+            ('number_of_samples', {'sanbox': '1', 'public': '0',
+                                   'private': '27'}, r_client.hgetall),
+            ('num_users', '4', r_client.get),
+            ('lat_longs', EXP_LAT_LONG, r_client.get),
+            ('num_studies_ebi', '3', r_client.get),
+            ('num_samples_ebi', '27', r_client.get),
+            ('number_samples_ebi_prep', '54', r_client.get)
+            # not testing img/time for simplicity
+            # ('img', r_client.get),
+            # ('time', r_client.get)
+            ]
+        for k, exp, f in vals:
+            redis_key = '%s:stats:%s' % (portal, k)
+            self.assertEqual(f(redis_key), exp)
+
+
+EXP_LAT_LONG = (
+    '[[0.291867635913, 68.5945325743], [68.0991287718, 34.8360987059],'
+    ' [10.6655599093, 70.784770579], [40.8623799474, 6.66444220187],'
+    ' [13.089194595, 92.5274472082], [84.0030227585, 66.8954849864],'
+    ' [12.7065957714, 84.9722975792], [78.3634273709, 74.423907894],'
+    ' [82.8302905615, 86.3615778099], [53.5050692395, 31.6056761814],'
+    ' [43.9614715197, 82.8516734159], [29.1499460692, 82.1270418227],'
+    ' [23.1218032799, 42.838497795], [12.6245524972, 96.0693176066],'
+    ' [38.2627021402, 3.48274264219], [74.0894932572, 65.3283470202],'
+    ' [35.2374368957, 68.5041623253], [4.59216095574, 63.5115213108],'
+    ' [95.2060749748, 27.3592668624], [68.51099627, 2.35063674718],'
+    ' [85.4121476399, 15.6526750776], [60.1102854322, 74.7123248382],'
+    ' [3.21190859967, 26.8138925876], [57.571893782, 32.5563076447],'
+    ' [44.9725384282, 66.1920014699], [42.42, 41.41]]')
 
 if __name__ == '__main__':
     main()
