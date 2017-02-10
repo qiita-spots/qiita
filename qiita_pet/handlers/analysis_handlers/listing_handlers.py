a083e2389f8ce9d8779866022ec5a1346c05a9d2
Jose Navas <josenavasmolina@gmail.com>
2017-02-10 11:29:06 -0800

    Analysis refactor GUI part1 (#2076)
    * fix #1505
    
    * improving some GUI stuff
    
    * improving some GUI stuff - missing lines
    
    * addressing all comments
    
    * ready for review
    
    * fix #1987
    
    * initial commit
    
    * requested changes
    
    * fix filter job list
    
    * Fixing server cert (#2051)
    
    * fix get_studies
    
    * flake8
    
    * fix #503
    
    * fix #2010
    
    * fix #1913
    
    * fix errors
    
    * addressing @josenavas comment
    
    * flake8
    
    * fix #1010
    
    * fix #1066 (#2058)
    
    * addressing @josenavas comments
    
    * fix #1961
    
    * fix #1837
    
    * Automatic jobs & new stats (#2057)
    
    * fix #814, fix #1636
    
    * fixing error in test-env
    
    * fixing stats.html call
    
    * adding img
    
    * addressing @josenavas comments
    
    * rm for loops
    
    * addresssing @ElDeveloper comments
    
    * generalizing this functionality
    
    * fix #1816
    
    * fix #1959
    
    * addressing @josenavas comments
    
    * addressing @josenavas comments
    
    * fixing error
    
    * fixed?
    
    * addressing @josenavas comments
    
    * addressing @wasade comments
    
    * fix flake8
    
    * generate biom and metadata release (#2066)
    
    * initial commit
    
    * adding portal
    
    * addressing @josenavas comments
    
    * pid -> qiita_artifact_id
    
    * addressing @josenavas comments
    
    * addressing @ElDeveloper comments
    
    * rm 50.sql
    
    * database changes to fix 969
    
    * adding delete
    
    * addressing @josenavas comments
    
    * addressing @ElDeveloper comments
    
    * duh!
    
    * fix generate_biom_and_metadata_release (#2072)
    
    * fix generate_biom_and_metadata_release
    
    * addressing @ElDeveloper comment
    
    * Removing qiita ware code that will not be used anymore
    
    * Organizing the handlers and new analysis description page
    
    * Addressing @antgonza's comments

---
+++ qiita_pet/handlers/analysis_handlers/listing_handlers.py
@@ -0,0 +1,135 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from functools import partial
+from json import dumps
+from collections import defaultdict
+from future.utils import viewitems
+
+from tornado.web import authenticated
+
+from qiita_core.qiita_settings import qiita_config
+from qiita_core.util import execute_as_transaction
+from qiita_pet.handlers.base_handlers import BaseHandler
+from qiita_pet.handlers.util import download_link_or_path
+from qiita_pet.handlers.analysis_handlers import check_analysis_access
+from qiita_pet.util import is_localhost
+from qiita_db.util import get_filepath_id
+from qiita_db.analysis import Analysis
+from qiita_db.logger import LogEntry
+from qiita_db.reference import Reference
+from qiita_db.artifact import Artifact
+
+
+class ListAnalysesHandler(BaseHandler):
+    @authenticated
+    @execute_as_transaction
+    def get(self):
+        message = self.get_argument('message', '')
+        level = self.get_argument('level', '')
+        user = self.current_user
+
+        analyses = user.shared_analyses | user.private_analyses
+
+        is_local_request = is_localhost(self.request.headers['host'])
+        gfi = partial(get_filepath_id, 'analysis')
+        dlop = partial(download_link_or_path, is_local_request)
+        mappings = {}
+        bioms = {}
+        tgzs = {}
+        for analysis in analyses:
+            _id = analysis.id
+            # getting mapping file
+            mapping = analysis.mapping_file
+            if mapping is not None:
+                mappings[_id] = dlop(mapping, gfi(mapping), 'mapping file')
+            else:
+                mappings[_id] = ''
+
+            bioms[_id] = ''
+            # getting tgz file
+            tgz = analysis.tgz
+            if tgz is not None:
+                tgzs[_id] = dlop(tgz, gfi(tgz), 'tgz file')
+            else:
+                tgzs[_id] = ''
+
+        self.render("list_analyses.html", analyses=analyses, message=message,
+                    level=level, is_local_request=is_local_request,
+                    mappings=mappings, bioms=bioms, tgzs=tgzs)
+
+    @authenticated
+    @execute_as_transaction
+    def post(self):
+        analysis_id = int(self.get_argument('analysis_id'))
+        analysis = Analysis(analysis_id)
+        analysis_name = analysis.name.decode('utf-8')
+
+        check_analysis_access(self.current_user, analysis)
+
+        try:
+            Analysis.delete(analysis_id)
+            msg = ("Analysis <b><i>%s</i></b> has been deleted." % (
+                analysis_name))
+            level = "success"
+        except Exception as e:
+            e = str(e)
+            msg = ("Couldn't remove <b><i>%s</i></b> analysis: %s" % (
+                analysis_name, e))
+            level = "danger"
+            LogEntry.create('Runtime', "Couldn't remove analysis ID %d: %s" %
+                            (analysis_id, e))
+
+        self.redirect(u"%s/analysis/list/?level=%s&message=%s"
+                      % (qiita_config.portal_dir, level, msg))
+
+
+class AnalysisSummaryAJAX(BaseHandler):
+    @authenticated
+    @execute_as_transaction
+    def get(self):
+        info = self.current_user.default_analysis.summary_data()
+        self.write(dumps(info))
+
+
+class SelectedSamplesHandler(BaseHandler):
+    @authenticated
+    @execute_as_transaction
+    def get(self):
+        # Format sel_data to get study IDs for the processed data
+        sel_data = defaultdict(dict)
+        proc_data_info = {}
+        sel_samps = self.current_user.default_analysis.samples
+        for aid, samples in viewitems(sel_samps):
+            a = Artifact(aid)
+            sel_data[a.study][aid] = samples
+            # Also get processed data info
+            processing_parameters = a.processing_parameters
+            if processing_parameters is None:
+                params = None
+                algorithm = None
+            else:
+                cmd = processing_parameters.command
+                params = processing_parameters.values
+                if 'reference' in params:
+                    ref = Reference(params['reference'])
+                    del params['reference']
+
+                    params['reference_name'] = ref.name
+                    params['reference_version'] = ref.version
+                algorithm = '%s (%s)' % (cmd.software.name, cmd.name)
+
+            proc_data_info[aid] = {
+                'processed_date': str(a.timestamp),
+                'algorithm': algorithm,
+                'data_type': a.data_type,
+                'params': params
+            }
+
+        self.render("analysis_selected.html", sel_data=sel_data,
+                    proc_info=proc_data_info)
