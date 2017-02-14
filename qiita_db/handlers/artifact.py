7542658f9c37dc8ed273175d2a6e02b5f56945f3
Jose Navas <josenavasmolina@gmail.com>
2017-02-14 10:25:42 -0800

    Fixing artifact rest API since not all artifacts have study

--- qiita_db/handlers/artifact.py
+++ qiita_db/handlers/artifact.py
@@ -79,6 +79,7 @@ class ArtifactHandler(OauthBaseHandler):
         """
         with qdb.sql_connection.TRN:
             artifact = _get_artifact(artifact_id)
+            study = artifact.study
             response = {
                 'name': artifact.name,
                 'timestamp': str(artifact.timestamp),
@@ -89,7 +90,7 @@ class ArtifactHandler(OauthBaseHandler):
                 'can_be_submitted_to_vamps':
                     artifact.can_be_submitted_to_vamps,
                 'prep_information': [p.id for p in artifact.prep_templates],
-                'study': artifact.study.id}
+                'study': study.id if study else None}
             params = artifact.processing_parameters
             response['processing_parameters'] = (
                 params.values if params is not None else None)
