5e734464026efc4bbb7514a694ff97dbb7c368b8
Daniel McDonald <mcdonadt@colorado.edu>
2017-03-22 17:57:53 -0700

    ENH: oauth2 forced

--- qiita_pet/handlers/rest/study.py
+++ qiita_pet/handlers/rest/study.py
@@ -7,12 +7,14 @@
 # -----------------------------------------------------------------------------
 from qiita_db.study import Study
 from qiita_db.exceptions import QiitaDBUnknownIDError
+from qiita_db.handlers.oauth2 import authenticate_oauth
 from qiita_pet.handlers.util import to_int
 from qiita_pet.handlers.base_handlers import BaseHandler
 
-class StudyHandler(BaseHandler):
-    def get(self, study_id):
 
+class StudyHandler(BaseHandler):
+    @authenticate_oauth
+    def get(self, study_id):
         study_id = to_int(study_id)
         try:
             Study(study_id)
--- qiita_pet/test/rest/test_study.py
+++ qiita_pet/test/rest/test_study.py
@@ -9,31 +9,41 @@
 from unittest import main
 
 from tornado.escape import json_decode
+from moi import r_client
 
 from qiita_pet.test.tornado_test_base import TestHandlerBase
 
 
 class StudyHandlerTests(TestHandlerBase):
+    def setUp(self):
+        self.client_token = 'SOMEAUTHTESTINGTOKENHERE2122'
+        r_client.hset(self.client_token, 'timestamp', '12/12/12 12:12:00')
+        r_client.hset(self.client_token, 'client_id', 'test123123123')
+        r_client.hset(self.client_token, 'grant_type', 'client')
+        r_client.expire(self.client_token, 5)
+
+        self.headers = {'Authorization': 'Bearer ' + self.client_token}
+        super(StudyHandlerTests, self).setUp()
 
     def test_get_valid(self):
-        response = self.get('/api/v1/study/1')
+        response = self.get('/api/v1/study/1', headers=self.headers)
         self.assertEqual(response.code, 200)
         self.assertEqual(response.body, "")
 
     def test_get_invalid(self):
-        response = self.get('/api/v1/study/0')
+        response = self.get('/api/v1/study/0', headers=self.headers)
         self.assertEqual(response.code, 404)
         self.assertEqual(json_decode(response.body),
                          {'message': 'Study not found'})
 
     def test_get_invalid_negative(self):
-        response = self.get('/api/v1/study/-1')
+        response = self.get('/api/v1/study/-1', headers=self.headers)
         self.assertEqual(response.code, 404)
         # not asserting the body content as this is not a valid URI according
         # to the regex associating the handler to the webserver
 
     def test_get_invalid_namespace(self):
-        response = self.get('/api/v1/study/1.11111')
+        response = self.get('/api/v1/study/1.11111', headers=self.headers)
         self.assertEqual(response.code, 404)
         # not asserting the body content as this is not a valid URI according
 
