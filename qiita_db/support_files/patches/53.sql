fe78f95de78f9c6a85ecf7b4d1a960b0fc3d9787
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2017-04-03 13:59:19 -0700

    API: Add unique constraint

--- qiita_db/support_files/patches/53.sql
+++ qiita_db/support_files/patches/53.sql
@@ -2,7 +2,6 @@
 -- Linking qiita users to the oauth table
 ALTER TABLE qiita_user ADD client_id varchar  ;
 
-CREATE INDEX idx_qiita_user ON qiita_user ( client_id ) ;
+ALTER TABLE qiita_user ADD CONSTRAINT uc_qiita_user_client_id UNIQUE ( client_id ) ;
 
 ALTER TABLE qiita_user ADD CONSTRAINT fk_qiita_user_client_id FOREIGN KEY ( client_id ) REFERENCES oauth_identifiers( client_id )    ;
-
