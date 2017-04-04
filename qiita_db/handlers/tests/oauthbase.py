9c62dad255260c600df694ad2dc80302fdc7802c
Daniel McDonald <mcdonadt@colorado.edu>
2017-04-04 15:12:20 -0700

    BUG: oauth test base wasn't specifying a client id

--- qiita_db/handlers/tests/oauthbase.py
+++ qiita_db/handlers/tests/oauthbase.py
@@ -9,5 +9,6 @@ class OauthTestingBase(TestHandlerBase):
         self.header = {'Authorization': 'Bearer ' + self.token}
         r_client.hset(self.token, 'timestamp', '12/12/12 12:12:00')
         r_client.hset(self.token, 'grant_type', 'client')
+        r_client.hset(self.token, 'client_id', 'foo')
         r_client.expire(self.token, 20)
         super(OauthTestingBase, self).setUp()
