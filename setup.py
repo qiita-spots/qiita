06bc8b2049f3cbe5506e8c293fd86d6ab67f42c3
Antonio Gonzalez <antgonza@gmail.com>
2017-04-26 09:18:05 -0600

    installing redbiom

--- setup.py
+++ setup.py
@@ -114,5 +114,6 @@ setup(name='qiita-spots',
       dependency_links=[
         'https://github.com/qiita-spots/qiita-files/archive/master.zip#'
         'egg=qiita-files-0.1.0-dev'],
+        'https://github.com/wasade/redbiom/archive/master.zip'
       classifiers=classifiers
       )
