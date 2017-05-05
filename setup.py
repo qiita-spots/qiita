0e72f2ad26fb61379152db9c0fba47cecb692993
Antonio Gonzalez <antgonza@gmail.com>
2017-05-04 21:08:10 -0600

    redbiom setup.py

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(name='qiita-spots',
                         'h5py >= 2.3.1', 'biom-format', 'natsort', 'networkx',
                         'scikit-bio >= 0.2.3, < 0.3.0', 'wtforms == 2.0.1',
                         'moi', 'sphinx-bootstrap-theme', 'Sphinx >= 1.2.2',
-                        'gitpython', 'qiita-files'],
+                        'gitpython', 'qiita-files', 'redbiom'],
       dependency_links=[
         'https://github.com/qiita-spots/qiita-files/archive/master.zip#'
         'egg=qiita-files-0.1.0-dev',
