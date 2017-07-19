e2915deefe2711748f460dd94ab6cf2e14f5a558
Antonio Gonzalez <antgonza@gmail.com>
2017-07-19 14:52:50 -0600

    addressing @ElDeveloper comments

--- qiita_pet/handlers/study_handlers/tests/test_prep_template.py
+++ qiita_pet/handlers/study_handlers/tests/test_prep_template.py
@@ -60,7 +60,6 @@ class TestPrepTemplateGraphAJAX(TestHandlerBase):
 
 class TestPrepTemplateAJAXReadOnly(TestHandlerBase):
     def test_get(self):
-        sleep(1)
         response = self.get('/study/description/prep_template/',
                             {'prep_id': 1, 'study_id': 1})
         self.assertEqual(response.code, 200)
