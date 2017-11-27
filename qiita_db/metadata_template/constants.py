f461a9905b1de2b4caf80dc2d063a965fa019334
Antonio Gonzalez <antgonza@gmail.com>
2017-11-26 20:32:41 -0700

    fix #1510

--- qiita_db/metadata_template/constants.py
+++ qiita_db/metadata_template/constants.py
@@ -51,8 +51,7 @@ TARGET_GENE_DATA_TYPES = ['16S', '18S', 'ITS']
 PREP_TEMPLATE_COLUMNS_TARGET_GENE = {
     # The following columns are required by QIIME to execute split libraries
     'demultiplex': Restriction(
-        columns={'barcode': str,
-                 'primer': str},
+        columns={'barcode': str},
         error_msg="Demultiplexing disabled."),
     # The following columns are required by Qiita to know how to execute split
     # libraries using QIIME over a study with multiple illumina lanes
