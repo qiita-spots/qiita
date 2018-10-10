519834b1b720650630c566470b15885f33cfe7b1
Charles Cowart <ccowart@BigMac.local>
2018-10-09 17:22:07 -0700

    Post Processing Merged BIOMs extended
    analysis.build_files will now run an arbitrary number of post-processes defined
    in qiita.software_command.post_processing_cmd column. It's assumed that each
    post-process is a python script, with a defined Conda environment. It's also
    currently assumed that each post-process overwrites the merged BIOM file. A
    step removed from calling plugins, but getting closer.

--- qiita_db/support_files/patches/67.sql
+++ qiita_db/support_files/patches/67.sql
@@ -1,6 +1,6 @@
 -- October 6, 2018
--- add addtl_processing_cmd column to record additional information required to merge some BIOMS.
+-- add post_processing_cmd column to record additional information required to merge some BIOMS.
 
-ALTER TABLE qiita.software_command ADD addtl_processing_cmd varchar;
+ALTER TABLE qiita.software_command ADD post_processing_cmd varchar;
 
-COMMENT ON COLUMN qiita.software_command.addtl_processing_cmd IS 'Store information on additional processes to merge BIOMs, if any.';
+COMMENT ON COLUMN qiita.software_command.post_processing_cmd IS 'Store information on additional post-processing steps for merged BIOMs, if any.';
