4b1ee2c8ebf3069d163ccebac6cb4810c12710b1
Charles Cowart <ccowart@BigMac.local>
2018-10-31 16:01:23 -0700

    Added analysis id to fp_archive.json

--- qiita_db/analysis.py
+++ qiita_db/analysis.py
@@ -1007,7 +1007,8 @@ class Analysis(qdb.base.QiitaObject):
                     # suitable for other files as well.
                     output_dir = base_fp
 
-                    fp_archive = join(output_dir, 'archive.json')
+                    fp_archive = join(output_dir,
+                                      'archive_%d.json' % (self._id))
 
                     with open(fp_archive, 'w') as out_file:
                         dump(archives, out_file)
