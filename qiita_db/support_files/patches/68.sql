60b60c2da1aa58f45341b37e76a5d58fcec14676
charles-cowart <42684307+charles-cowart@users.noreply.github.com>
2018-12-21 15:13:00 -0800

    Update 68.sql

--- qiita_db/support_files/patches/68.sql
+++ qiita_db/support_files/patches/68.sql
@@ -1,6 +1,6 @@
 -- December 21, 2018
 -- Strip non-printable-ASCII characters from study_person.name
-UPDATE study_person SET name = regexp_replace(name, '[^\x20-\x7E]+', '', 'g');
+UPDATE qiita.study_person SET name = regexp_replace(name, '[^\x20-\x7E]+', '', 'g');
 
 -- November 21, 2018
 -- moving sample and prep info files to jsonb
