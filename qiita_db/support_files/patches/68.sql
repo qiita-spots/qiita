0283704262df7e5c93e24857adfd20948bd3b56e
Charles Cowart <ccowart@BigMac.local>
2019-01-04 17:12:10 -0800

    Minor fix

--- qiita_db/support_files/patches/68.sql
+++ qiita_db/support_files/patches/68.sql
@@ -1,4 +1,4 @@
 -- January 4, 2019
 -- add external_job_id column to record mapping of Torque Job IDs to Qiita Job IDs.
-ALTER TABLE qiita.processing_job ADD external_job_id varchar;
+ALTER TABLE qiita.processing_job ADD COLUMN external_job_id varchar;
 COMMENT ON COLUMN qiita.processing_job IS 'Store an external job ID (e.g. Torque job ID) associated this Qiita job.';
