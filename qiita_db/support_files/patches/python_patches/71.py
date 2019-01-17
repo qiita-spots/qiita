78d4394a4d99a05d5d0894bdda3d3b51c313a2a8
Antonio Gonzalez <antgonza@gmail.com>
2019-01-17 14:38:24 -0700

    forcing commit

--- qiita_db/support_files/patches/python_patches/71.py
+++ qiita_db/support_files/patches/python_patches/71.py
@@ -1,6 +1,5 @@
 from qiita_db.sql_connection import TRN
 
-
 with TRN:
     sql = """SELECT DISTINCT table_name
              FROM information_schema.columns
@@ -8,9 +7,11 @@ with TRN:
     TRN.add(sql)
     tables = ['qiita.%s' % t for t in TRN.execute_fetchflatten()]
 
-chunk_size = 500
+chunk_size = 200
 for i in range(0, len(tables), chunk_size):
     chunk = tables[i:chunk_size+i]
     sql = "DROP TABLE %s" % ', '.join(chunk)
-    TRN.add(sql)
-    TRN.execute()
+    with TRN:
+        TRN.add(sql)
+        TRN.execute()
+        TRN.commit()
