36b3ccc8fc97d06ed8ccdeebe01cbe669022f4ae
Antonio Gonzalez <antgonza@gmail.com>
2019-01-24 07:43:40 -0700

    config_test.cfg.enc -> qiita_core/support_files/config_test.cfg.enc

--- travis_ebi_setup.sh
+++ travis_ebi_setup.sh
@@ -1,3 +1,3 @@
 set -e
 echo "** Testing EBI **"
-openssl aes-256-cbc -K $encrypted_079e536628f1_key -iv $encrypted_079e536628f1_iv -in config_test.cfg.enc -out qiita_core/support_files/config_test.cfg -d
+openssl aes-256-cbc -K $encrypted_079e536628f1_key -iv $encrypted_079e536628f1_iv -in qiita_core/support_files/config_test.cfg.enc -out qiita_core/support_files/config_test.cfg -d
