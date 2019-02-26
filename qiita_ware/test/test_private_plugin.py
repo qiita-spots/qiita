a9990160430179bfa6fce0fce0ddb09ddfcea419
Antonio Gonzalez <antgonza@gmail.com>
2019-02-26 12:18:10 -0700

    fix #2734

--- qiita_ware/test/test_private_plugin.py
+++ qiita_ware/test/test_private_plugin.py
@@ -438,6 +438,8 @@ class TestPrivatePluginDeleteStudy(BaseTestPrivatePlugin):
         self.assertEqual(job.status, 'error')
         self.assertIn("Cannot delete artifact 2: Artifact 2 has been "
                       "submitted to EBI", job.log.msg)
+        # makingn sure the analysis, first thing to delete, still exists
+        self.assertTrue(Analysis.exists(1))
 
         # delete everything from the EBI submissions and the processing job so
         # we can try again: test success (with tags)
