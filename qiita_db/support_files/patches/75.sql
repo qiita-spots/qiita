0681530fcf1da489f5d2626cac69fa2d49be45ec
Isaac Jiang <isaacjiang@gmail.com>
2019-07-15 10:35:15 -0700

    fix issue #304

--- qiita_db/support_files/patches/75.sql
+++ qiita_db/support_files/patches/75.sql
@@ -1,25 +1,36 @@
--- Jun 17 2019
+	-- Jun 17 2019
 -- Drop the study_person table and connect tables related to study_person with qiita_user
 
 -- Drop the PK in qiita_user
-ALTER TABLE qiita_user DROP CONSTRAINT qiita_user_pk CASCADE;
+ALTER TABLE qiita.qiita_user DROP CONSTRAINT pk_user CASCADE;
 
 -- Add new PK qiita_user_id in qiita_user
-ALTER TABLE qiita_user ADD COLUMN qiita_user_id BIGSERIAL PRIMARY KEY;
+ALTER TABLE qiita.qiita_user ADD COLUMN qiita_user_id BIGSERIAL PRIMARY KEY;
 
 -- Reconnect tables with qiita _user_id
-ALTER TABLE analysis ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE analysis_users ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE study_tags ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE processing_job ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE study ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE study_users ADD COLUMN user_id BIGINT REFERENCES qiita_user(qiita_user_id);
+ALTER TABLE qiita.analysis ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.analysis SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.analysis.email = qiita.qiita_user.email;
+ALTER TABLE qiita.analysis DROP COLUMN email CASCADE;
+
+ALTER TABLE qiita.analysis_users ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.analysis_users SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.analysis_users.email = qiita.qiita_user.email;
+ALTER TABLE qiita.analysis_users DROP COLUMN email CASCADE;
+
+ALTER TABLE qiita.study_tags ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.study_tags SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.study_tags.email = qiita.qiita_user.email;
+ALTER TABLE qiita.study_tags DROP COLUMN email CASCADE;
+
+ALTER TABLE qiita.processing_job_workflow ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.processing_job_workflow SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.processing_job_workflow.email = qiita.qiita_user.email;
+ALTER TABLE qiita.processing_job_workflow DROP COLUMN email CASCADE;
+
+ALTER TABLE qiita.study ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.study SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.study.email = qiita.qiita_user.email;
+ALTER TABLE qiita.study DROP COLUMN email CASCADE;
+
+ALTER TABLE qiita.study_users ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
+UPDATE qiita.study_users SET user_id = qiita.qiita_user.qiita_user_id FROM qiita.qiita_user WHERE qiita.study_users.email = qiita.qiita_user.email;
+ALTER TABLE qiita.study_users DROP COLUMN email CASCADE;
 
 -- Drop table study_person
-DROP TABLE study_person CASCADE;
-
--- Connect to qiita_user
-ALTER TABLE study ADD CONSTRAINT fk_study FOREIGN KEY(principal_investigator_id) REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE study ADD CONSTRAINT fk_study FOREIGN KEY(lab_person_id) REFERENCES qiita_user(qiita_user_id);
-ALTER TABLE investigation ADD CONSTRAINT fk_investigation FOREIGN KEY(contact_person_id) REFERENCES qiita_user(qiita_user_id);
-
+DROP TABLE qiita.study_person CASCADE;
