027807de24614a25c7a890461e1d712e8303e034
Isaac Jiang <isaacjiang@gmail.com>
2019-07-17 14:42:48 -0700

    fix #304

--- qiita_db/support_files/patches/76.sql
+++ qiita_db/support_files/patches/76.sql
@@ -1,11 +1,13 @@
 -- Jun 17 2019
 -- Drop the study_person table and connect tables related to study_person with qiita_user
 
--- Drop the PK in qiita_user
-ALTER TABLE qiita.qiita_user DROP CONSTRAINT pk_user CASCADE;
+ALTER TABLE qiita.qiita_user ADD COLUMN test BIGSERIAL;
 
--- Add new PK qiita_user_id in qiita_user
-ALTER TABLE qiita.qiita_user ADD COLUMN qiita_user_id BIGSERIAL PRIMARY KEY;
+-- -- Drop the PK in qiita_user
+-- ALTER TABLE qiita.qiita_user DROP CONSTRAINT pk_user CASCADE;
+
+-- -- Add new PK qiita_user_id in qiita_user
+-- ALTER TABLE qiita.qiita_user ADD COLUMN qiita_user_id BIGSERIAL PRIMARY KEY;
 
 -- -- Reconnect tables with qiita _user_id
 -- ALTER TABLE qiita.analysis ADD COLUMN user_id BIGINT REFERENCES qiita.qiita_user(qiita_user_id);
