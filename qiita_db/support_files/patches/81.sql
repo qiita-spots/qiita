6107d2faac8a3d17228716b0a56ced7e97114deb
Antonio Gonzalez <antgonza@gmail.com>
2021-01-19 09:38:54 -0700

    Delete 81.sql

--- qiita_db/support_files/patches/81.sql
+++
@@ -1,5 +0,0 @@
--- Jan 8, 2021
--- Update 
-Add a flag to the studies to see if the study was submitted by Qiita or downloaded by EBI
-
-ALTER TABLE qiita.study ADD autoloaded BOOL NOT NULL DEFAULT false;
