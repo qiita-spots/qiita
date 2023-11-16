73fac6664924f85e8a3f38c7c97c6e325fac9723
Antonio Gonzalez <antgonza@gmail.com>
2023-11-16 11:36:23 -0700

    db changes

---
+++ qiita_db/support_files/patches/89.sql
@@ -0,0 +1,4 @@
+-- Nov 1, 2023
+-- add creation_job_id to qiita.prep_template
+ALTER TABLE qiita.analysis slurm_reservation VARCHAR DEFAULT '' NOT NULL;
+ALTER TABLE qiita.user_level ADD slurm_parameters VARCHAR DEFAULT '' NOT NULL;
