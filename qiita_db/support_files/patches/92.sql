8660043591c627d5b24d96ef24183a91fd67a7c4
Antonio Gonzalez <antgonza@gmail.com>
2024-04-16 13:34:09 -0600

    qiita.slurm_resource_allocations (#3391)
    * Update CHANGELOG.md
    
    * add db changes including fix #3368

---
+++ qiita_db/support_files/patches/92.sql
@@ -0,0 +1,20 @@
+-- Apr 16, 2024
+-- Adding a new table to contain the basic slurm information to minimize
+-- the number of times we need to retrieve this information
+
+CREATE  TABLE qiita.slurm_resource_allocations (
+	processing_job_id    uuid  NOT NULL,
+	samples              integer,
+	columns              integer,
+	input_size           bigint,
+	extra_info           varchar DEFAULT NULL,
+	memory_used          bigint,
+	walltime_used        integer,
+	CONSTRAINT pk_slurm_resource_allocations_processing_job_id PRIMARY KEY (
+    processing_job_id )
+ );
+
+ALTER TABLE qiita.slurm_resource_allocations
+  ADD CONSTRAINT fk_slurm_resource_allocations
+  FOREIGN KEY ( processing_job_id )
+  REFERENCES qiita.processing_job ( processing_job_id );
