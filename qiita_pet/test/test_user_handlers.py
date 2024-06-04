54772cf1567409e989d2b4548ce5a0e907fcb547
sjanssen2 <stefan.m.janssen@gmail.com>
2024-06-04 21:27:05 +0200

    add tests for validator functions for WTform StringFields

--- qiita_pet/test/test_user_handlers.py
+++ qiita_pet/test/test_user_handlers.py
@@ -7,8 +7,11 @@
 # -----------------------------------------------------------------------------
 
 from unittest import main
+from wtforms.validators import ValidationError
+from wtforms import StringField
 
 from qiita_pet.test.tornado_test_base import TestHandlerBase
+from qiita_pet.handlers.user_handlers import UserProfile
 
 
 class TestUserProfile(TestHandlerBase):
@@ -17,7 +20,6 @@ class TestUserProfile(TestHandlerBase):
 
 
 class TestUserProfileHandler(TestHandlerBase):
-
     def test_get(self):
         response = self.get('/profile/')
         self.assertEqual(response.code, 200)
@@ -43,6 +45,77 @@ class TestUserProfileHandler(TestHandlerBase):
             'social_researchgate': ['']}
         response = self.post('/profile/', post_args)
         self.assertEqual(response.code, 200)
+
+    def test_validators_social(self):
+        # None or empty should be valid
+        obs = UserProfile.validate_general(None, "", "")
+        self.assertEqual(obs, None)
+        obs = UserProfile.validate_general("", "", "")
+        self.assertEqual(obs, None)
+
+        # having white spaces should raise errors
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validate_general(" infix", "", "")
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validate_general("infix ", "", "")
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validate_general(" infix ", "", "")
+        obs = UserProfile.validate_general("infix", "", "")
+        self.assertEqual(obs, 'infix')
+
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validate_general(
+                "http://kurt.com/id1234", "msg", r"http://kurt.\w{1,3}/")
+
+    def test_validator_orcid_id(self):
+        field = StringField("testfield")
+
+        field.data = "0000-0002-0975-9019"
+        obs = UserProfile.validator_orcid_id(None, field)
+        self.assertEqual(obs, None)
+
+        field.data = "https://orcid.org/0000-0002-0975-9019"
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_orcid_id(None, field)
+
+        field.data = "wrong"
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_orcid_id(None, field)
+
+    def test_validator_gscholar_id(self):
+        field = StringField("testfield")
+
+        field.data = "_e3QL94AAAAJ"
+        obs = UserProfile.validator_gscholar_id(None, field)
+        self.assertEqual(obs, None)
+
+        field.data = ('https://scholar.google.com/citations?user=_e3QL94AAAAJ&'
+                      'hl=en')
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_gscholar_id(None, field)
+
+        field.data = 'user=_e3QL94AAAAJ&hl=en'
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_gscholar_id(None, field)
+
+        field.data = 'user=_e3QL94AAAAJ'
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_gscholar_id(None, field)
+
+        field.data = '=_e3QL94AAAAJ'
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_gscholar_id(None, field)
+
+    def test_validator_rgate_id(self):
+        field = StringField("testfield")
+
+        field.data = "Rob-Knight"
+        obs = UserProfile.validator_rgate_id(None, field)
+        self.assertEqual(obs, None)
+
+        field.data = 'https://www.researchgate.net/profile/Rob-Knight'
+        with self.assertRaises(ValidationError):
+            obs = UserProfile.validator_rgate_id(None, field)
 
 
 class TestUserJobsHandler(TestHandlerBase):
