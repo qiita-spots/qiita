dd0ee7407bd92cfd9a479f265682cb335ee3dd4f
sjanssen2 <stefan.m.janssen@gmail.com>
2024-06-19 22:41:44 +0200

    moving changes to patch 92

--- qiita_db/support_files/patches/92.sql
+++ qiita_db/support_files/patches/92.sql
@@ -27,3 +27,12 @@ ALTER TABLE qiita.prep_template ADD current_human_filtering boolean DEFAULT Fals
 -- Adding a new column: reprocess_job_id to qiita.prep_template to keep track of
 -- the job that reprocessed this prep
 ALTER TABLE qiita.prep_template ADD reprocess_job_id uuid DEFAULT NULL;
+
+-- Jun 19, 2024
+-- Adding a new column to the user table that logs when this account was created
+-- Usefull e.g. to prune non-verified=inactive user or to plot user growth
+
+ALTER TABLE qiita.qiita_user
+  ADD creation_timestamp timestamp without time zone DEFAULT NOW();
+
+COMMENT ON COLUMN qiita.qiita_user.creation_timestamp IS 'The date the user account was created';
--- qiita_db/support_files/patches/93.sql
+++
@@ -1,8 +0,0 @@
--- Jun 19, 2024
--- Adding a new column to the user table that logs when this account was created
--- Usefull e.g. to prune non-verified=inactive user or to plot user growth
-
-ALTER TABLE qiita.qiita_user
-  ADD creation_timestamp timestamp without time zone DEFAULT NOW();
-
-COMMENT ON COLUMN qiita.qiita_user.creation_timestamp IS 'The date the user account was created';
