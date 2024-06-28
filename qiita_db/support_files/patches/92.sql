7d42b8418d78cb665afc550194b75aafaee85f18
Antonio Gonzalez <antgonza@gmail.com>
2024-06-28 09:32:30 -0600

    clean db files + add human_reads_filter_method

--- qiita_db/support_files/patches/92.sql
+++ qiita_db/support_files/patches/92.sql
@@ -39,3 +39,28 @@ ALTER TABLE qiita.qiita_user
   ADD creation_timestamp timestamp without time zone DEFAULT NOW();
 
 COMMENT ON COLUMN qiita.qiita_user.creation_timestamp IS 'The date the user account was created';
+
+-- Jun 28, 2024
+-- These columns were added by mistake to qiita-db-unpatched.sql in PR:
+-- https://github.com/qiita-spots/qiita/pull/3412 so adding here now
+
+ALTER TABLE qiita.qiita_user ADD social_orcid character varying DEFAULT NULL;
+ALTER TABLE qiita.qiita_user ADD social_researchgate character varying DEFAULT NULL;
+ALTER TABLE qiita.qiita_user ADD social_googlescholar character varying DEFAULT NULL;
+
+-- Add human_reads_filter_method so we can keep track of the available methods
+-- and link them to the preparations
+
+CREATE  TABLE qiita.human_reads_filter_method (
+	human_reads_filter_method_id    bigint  NOT NULL,
+	human_reads_filter_method_method character varying NOT NULL;
+	CONSTRAINT pk_human_reads_filter_method_id PRIMARY KEY (
+    human_reads_filter_method_id )
+ );
+
+ALTER TABLE qiita.prep_template
+  ADD human_reads_filter_method_id bigint DEFAULT NULL;
+ALTER TABLE qiita.prep_template
+  ADD CONSTRAINT fk_human_reads_filter_method
+  FOREIGN KEY ( human_reads_filter_method_id )
+  REFERENCES qiita.human_reads_filter_method ( human_reads_filter_method_id );
