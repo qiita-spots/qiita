be74ad5714b1455ca6aaae1a3228df44bca6933a
Antonio Gonzalez <antgonza@gmail.com>
2024-12-11 06:57:09 -0700

    fix trace

--- qiita_db/test/test_analysis.py
+++ qiita_db/test/test_analysis.py
@@ -688,7 +688,7 @@ class TestAnalysis(TestCase):
     def test_slurm_reservation(self):
         analysis = qdb.analysis.Analysis(1)
         self.assertIsNone(analysis.slurm_reservation)
-        text = 'thisisatest!'
+        text = 'thisisatest'
         analysis.slurm_reservation = text
         self.assertEqual(analysis._slurm_reservation(), [text])
         self.assertIsNone(analysis.slurm_reservation)
