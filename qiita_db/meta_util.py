9717adb469625ae6d86944ad71e56d3ef94d55ef
gossty <stivengolzari@gmail.com>
2025-02-11 12:23:10 -0800

    Update description of software retrieval function

--- qiita_db/meta_util.py
+++ qiita_db/meta_util.py
@@ -559,9 +559,8 @@ def _build_software_commands_col_names_object(active):
     '''
     Helper function for update_resource_allocation_redis
 
-    Gets all the available software with the corresponding software versions,
-    with the corresponding commands, with the corresponding col_names, with
-    column names from db
+    Constructs a nested dictionary containing software commands and associated 
+    column names from the database.
     '''
 
     col_name_list = []
