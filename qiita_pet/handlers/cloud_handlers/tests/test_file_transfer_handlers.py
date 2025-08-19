3fce3125e44e7feeeec322d8ce03579fb2102811
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-08-19 15:51:13 +0200

    remove debug and fix codestyle

--- qiita_pet/handlers/cloud_handlers/tests/test_file_transfer_handlers.py
+++ qiita_pet/handlers/cloud_handlers/tests/test_file_transfer_handlers.py
@@ -1,7 +1,6 @@
 from unittest import main
 from os.path import exists, basename
 from os import remove
-import sys
 import filecmp
 
 from qiita_db.handlers.tests.oauthbase import OauthTestingBase
@@ -25,14 +24,9 @@ class FetchFileFromCentralHandlerTests(OauthTestingBase):
         self.assertEqual(obs.status_code, 403)
         self.assertIn('The requested file is not present', obs.reason)
 
-        print("STEFAN",
-              endpoint + base_data_dir[1:] +
-              '/raw_data/FASTA_QUAL_preprocessing.fna')
         obs = self.get_authed(
             endpoint + base_data_dir[1:] +
             '/raw_data/FASTA_QUAL_preprocessing.fna')
-        print("STEFAN2", obs.reason)
-        print("STFAN3", obs.__dict__)
         self.assertEqual(obs.status_code, 200)
         self.assertIn('FLP3FBN01ELBSX length=250 xy=1766_01', str(obs.content))
 
