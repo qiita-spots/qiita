54695332dd709eb27a0a280c5fd13d812a387c87
Daniel McDonald <damcdonald@ucsd.edu>
2025-11-06 16:20:28 -0700

    API: add /api/v1/study/<id>/associations to retrieve comprehensive id, path, processing information for a study

--- qiita_pet/test/rest/test_study_associations.py
+++ qiita_pet/test/rest/test_study_associations.py
@@ -16,14 +16,17 @@ from qiita_pet.test.rest.test_base import RESTHandlerTestCase
 
 class StudyAssociationTests(RESTHandlerTestCase):
     def test_get_valid(self):
-        IGNORE = IGNORE
-        exp = {'study': 1,
+        IGNORE = 'IGNORE'
+        exp = {'study_id': 1,
+               'study_sample_metadata_filepath': IGNORE,
                'prep_templates': [{'prep_id': 1,
-                   'prep_filepath': IGNORE,
-                   'prep_datatype': '18S',
+                   'prep_status': 'private',
+                   'prep_sample_metadata_filepath': IGNORE,
+                   'prep_data_type': '18S',
                    'prep_human_filtering': 'The greatest human filtering method',
                    'prep_artifacts': [{'artifact_id': 1,
-                     'artifact_parent_ids': [1],
+                     'artifact_status': 'private',
+                     'artifact_parent_ids': None,
                      'artifact_basal_id': 1,
                      'artifact_processing_id': None,
                      'artifact_processing_name': None,
@@ -35,7 +38,8 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath': IGNORE,
                        'artifact_filepath_type': 'raw_barcodes'}]},
                     {'artifact_id': 2,
-                     'artifact_parent_ids': None,
+                     'artifact_status': 'private',
+                     'artifact_parent_ids': [1],
                      'artifact_basal_id': 1,
                      'artifact_processing_id': 1,
                      'artifact_processing_name': 'Split libraries FASTQ',
@@ -60,6 +64,7 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath_id': 5,
                        'artifact_filepath_type': 'preprocessed_demux'}]},
                     {'artifact_id': 3,
+                     'artifact_status': 'private',
                      'artifact_parent_ids': [1],
                      'artifact_basal_id': 1,
                      'artifact_processing_id': 1,
@@ -69,14 +74,15 @@ class StudyAssociationTests(RESTHandlerTestCase):
                       'min_per_read_length_fraction': '0.75',
                       'sequence_max_n': '0',
                       'rev_comp_barcode': 'False',
-                      'rev_comp_mapping_barcodes': 'False',
+                      'rev_comp_mapping_barcodes': 'True',
                       'rev_comp': 'False',
                       'phred_quality_threshold': '3',
                       'barcode_type': 'golay_12',
                       'max_barcode_errors': '1.5',
                       'phred_offset': 'auto'},
-                     'artifact_filepaths': []},
+                     'artifact_filepaths': None},
                     {'artifact_id': 4,
+                     'artifact_status': 'private',
                      'artifact_parent_ids': [2],
                      'artifact_basal_id': 1,
                      'artifact_processing_id': 3,
@@ -92,6 +98,7 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath': IGNORE,
                        'artifact_filepath_type': 'biom'}]},
                     {'artifact_id': 5,
+                     'artifact_status': 'private',
                      'artifact_parent_ids': [2],
                      'artifact_basal_id': 1,
                      'artifact_processing_id': 3,
@@ -107,6 +114,7 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath': IGNORE,
                        'artifact_filepath_type': 'biom'}]},
                     {'artifact_id': 6,
+                     'artifact_status': 'private',
                      'artifact_parent_ids': [2],
                      'artifact_basal_id': 1,
                      'artifact_processing_id': 3,
@@ -122,12 +130,14 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath': IGNORE,
                        'artifact_filepath_type': 'biom'}]}]},
                   {'prep_id': 2,
-                   'prep_filepath': IGNORE,
-                   'prep_datatype': '18S',
+                   'prep_status': 'private',
+                   'prep_sample_metadata_filepath': IGNORE,
+                   'prep_data_type': '18S',
                    'prep_human_filtering': None,
                    'prep_artifacts': [{'artifact_id': 7,
-                     'artifact_parent_ids': [],
+                     'artifact_parent_ids': None,
                      'artifact_basal_id': 7,
+                     'artifact_status': 'private',
                      'artifact_processing_id': None,
                      'artifact_processing_name': None,
                      'artifact_processing_arguments': None,
@@ -135,16 +145,37 @@ class StudyAssociationTests(RESTHandlerTestCase):
                        'artifact_filepath': IGNORE,
                        'artifact_filepath_type': 'biom'}]}]}]}
 
-        response = self.get('/api/v1/study-association/1', headers=self.headers)
+        response = self.get('/api/v1/study/1/associations', headers=self.headers)
         self.assertEqual(response.code, 200)
         obs = json_decode(response.body)
+
+        def _process_dict(d):
+            return [(d, k) for k in d]
+
+        def _process_list(l):
+            if l is None:
+                return []
+
+            return [dk for d in l
+                    for dk in _process_dict(d)]
+
+        stack = _process_dict(obs)
+        while stack:
+            (d, k) = stack.pop()
+            if k.endswith('filepath'):
+                d[k] = IGNORE
+            elif k.endswith('filepaths'):
+                stack.extend(_process_list(d[k]))
+            elif k.endswith('templates'):
+                stack.extend(_process_list(d[k]))
+            elif k.endswith('artifacts'):
+                stack.extend(_process_list(d[k]))
+
         self.assertEqual(obs, exp)
 
     def test_get_invalid(self):
-        response = self.get('/api/v1/study-association/0', headers=self.headers)
+        response = self.get('/api/v1/study/0/associations', headers=self.headers)
         self.assertEqual(response.code, 404)
-        self.assertEqual(json_decode(response.body),
-                         {'message': 'Study not found'})
 
 
 if __name__ == '__main__':
