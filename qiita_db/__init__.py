e5372679e57dfc898ef4706b0a47bf1e68b48069
Antonio Gonzalez <antgonza@gmail.com>
2025-12-05 12:48:36 -0700

    . import

--- qiita_db/__init__.py
+++ qiita_db/__init__.py
@@ -6,28 +6,27 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from . import (
-    analysis,
-    archive,
-    artifact,
-    base,
-    commands,
-    environment_manager,
-    exceptions,
-    investigation,
-    logger,
-    meta_util,
-    metadata_template,
-    ontology,
-    portal,
-    processing_job,
-    reference,
-    software,
-    sql_connection,
-    study,
-    user,
-    util,
-)
+from . import base  # noqa
+from . import util  # noqa
+from . import sql_connection  # noqa
+from . import metadata_template  # noqa
+from . import analysis  # noqa
+from . import artifact  # noqa
+from . import archive  # noqa
+from . import commands  # noqa
+from . import environment_manager  # noqa
+from . import exceptions  # noqa
+from . import investigation  # noqa
+from . import logger  # noqa
+from . import meta_util  # noqa
+from . import ontology  # noqa
+from . import portal  # noqa
+from . import reference  # noqa
+from . import software  # noqa
+from . import study  # noqa
+from . import user  # noqa
+from . import processing_job  # noqa
+
 
 __version__ = "2025.11"
 
