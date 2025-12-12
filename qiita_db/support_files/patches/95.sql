c16ef720fa72b8169d3d2c61a30c924918910855
Daniel McDonald <damcdonald@ucsd.edu>
2025-12-12 15:17:20 -0700

    API: passing PrepTemplate.unique_ids

--- qiita_db/support_files/patches/95.sql
+++ qiita_db/support_files/patches/95.sql
@@ -16,7 +16,7 @@ CREATE TABLE map_prep_sample_idx (
     prep_sample_idx BIGINT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('sequence_prep_sample_idx'),
     prep_idx BIGINT NOT NULL,
     sample_idx BIGINT NOT NULL,
-    UNIQUE (prep_idx, prep_sample_idx),
+    CONSTRAINT uc_prep_sample UNIQUE(prep_idx, sample_idx),
     CONSTRAINT fk_prep_template FOREIGN KEY (prep_idx) REFERENCES qiita.prep_template (prep_template_id)
 );
 
@@ -25,6 +25,6 @@ CREATE TABLE map_artifact_sample_idx (
     artifact_sample_idx BIGINT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('sequence_artifact_sample_idx'),
     artifact_idx BIGINT NOT NULL,
     prep_sample_idx BIGINT NOT NULL,
-    UNIQUE (artifact_idx, artifact_sample_idx),
+    CONSTRAINT uc_artifact_sample UNIQUE(artifact_idx, prep_sample_idx),
     CONSTRAINT fk_artifact FOREIGN KEY (artifact_idx) REFERENCES qiita.artifact (artifact_id)
 );
