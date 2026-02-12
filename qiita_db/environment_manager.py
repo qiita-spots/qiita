92b4e8e3d253a353443f7737662c457fa4da8823
Antonio Gonzalez <antgonza@gmail.com>
2026-02-12 06:20:48 -0700

    autoremove files created during testing (#3502)
    * autoremove files created during testing
    
    * artifact.id <= 9

--- qiita_db/environment_manager.py
+++ qiita_db/environment_manager.py
@@ -366,6 +366,19 @@ def drop_and_rebuild_tst_database(drop_labcontrol=False):
     """
     with qdb.sql_connection.TRN:
         r_client.flushdb()
+
+        # before we delete the full database we want to check if there are any artifact files
+        # created in the test environment - normally from plugins - and delete them; first we will
+        # find them and then we will delete them as a transaction
+        # NOTE: default testing artifact ids are <= 9 so not changing them
+        for artifact in qdb.artifact.Artifact.iter():
+            if artifact.id <= 9:
+                continue
+            for fp in artifact.filepaths:
+                fp = fp["fp"]
+                if exists(fp):
+                    qdb.util._rm_files(qdb.sql_connection.TRN, fp)
+
         # Drop the schema, note that we are also going to drop labman because
         # if not it will raise an error if you have both systems on your
         # computer due to foreing keys
